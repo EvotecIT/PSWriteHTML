e7e1b99d1368f2c2939ecd25ba3ed6319390a809
Matt Quickenden <matt.quickenden@avanade.com>
2016-08-11 10:26:33 -0700

    Initial code load

---
+++ Examples/Report-AzureVMsExamples_Part1WithComments.ps1
@@ -0,0 +1,195 @@
+﻿param (
+ $ReportOutputPath
+)
+ 
+Import-Module ReportHtml
+Get-Command -Module ReportHtml
+ 
+$ReportName = "Azure VMs Report"
+ 
+if (!$ReportOutputPath) 
+{
+ $ReportOutputPath = Split-Path -Path $MyInvocation.MyCommand.Definition -Parent
+} 
+ 
+# see if we already have a session. If we don't don't re-authN
+if (!$AzureRMAccount.Context.Tenant) {
+    $AzureRMAccount = Add-AzureRmAccount
+}
+
+# Get arrary of VMs from ARM
+$RMVMs = get-azurermvm
+ 
+$RMVMArray = @() ; $TotalVMs = $RMVMs.Count; $i =1 
+ 
+# Loop through VMs
+foreach ($vm in $RMVMs)
+{  
+# Tracking progress
+  Write-Progress -PercentComplete ($i / $TotalVMs * 100) -Activity "Building VM array" -CurrentOperation  ($vm.Name + " in resource group " + $vm.ResourceGroupName)
+     
+# Get VM Status (for Power State)
+  $vmStatus = Get-AzurermVM -Name $vm.Name -ResourceGroupName $vm.ResourceGroupName -Status
+ 
+  # Generate Array
+  $RMVMArray += New-Object PSObject -Property @{`
+ 
+    # Collect Properties
+    ResourceGroup = $vm.ResourceGroupName
+    Name = $vm.Name;
+    PowerState = (get-culture).TextInfo.ToTitleCase(($vmStatus.statuses)[1].code.split("/")[1]);
+    Location = $vm.Location;
+    Tags = $vm.Tags
+    Size = $vm.HardwareProfile.VmSize;
+    ImageSKU = $vm.StorageProfile.ImageReference.Sku;
+    OSType = $vm.StorageProfile.OsDisk.OsType;
+    OSDiskSizeGB = $vm.StorageProfile.OsDisk.DiskSizeGB;
+    DataDiskCount = $vm.StorageProfile.DataDisks.Count;
+    DataDisks = $vm.StorageProfile.DataDisks;
+    }
+    $I++
+}
+
+Function Test-Report
+{
+    param ($TestName)
+    $rptFile = join-path $ReportOutputPath ($ReportName.replace(" ","") + "-$TestName" + ".mht")
+    $rpt | Set-Content -Path $rptFile -Force
+    Invoke-Item $rptFile
+    sleep 1
+}
+
+####### Example 1 #######
+# Create an empty array for HTML strings
+$rpt = @()
+ 
+# note from here on we always append to the $rpt array variable.
+# First, let's add the HTML header information including report title
+$rpt += Get-HtmlOpen -TitleText $ReportName
+ 
+#  This content open function add a section header
+$rpt += Get-HtmlContentOpen -HeaderText "Virtual Machines"
+ 
+# This creates an HTML table of whatever array you pass into the function
+$rpt += Get-HtmlContentTable $RMVMArray
+ 
+#  This content close function closes the section 
+$rpt += Get-HtmlContentClose
+ 
+#  This HTML close adds HTML footer 
+$rpt += Get-HtmlClose
+ 
+# Now let's test what we have
+Test-Report -TestName Example1
+
+
+####### Example 2 ########
+$rpt = @()
+$rpt += Get-HtmlOpen -TitleText $ReportName
+$rpt += Get-HtmlContentOpen -HeaderText "Virtual Machines"
+ 
+# here we are going to filter the recordset, reorder the columns and group the results by location.  
+# The value you group by must be first in the select statement 
+$rpt += Get-HtmlContentTable ($RMVMArray | select Location, ResourceGroup, Name, Size,PowerState,DataDiskCount, ImageSKU ) -GroupBy Location
+$rpt += Get-HtmlContentClose
+$rpt += Get-HtmlClose
+ 
+Test-Report -TestName Example2
+
+####### Example 3 ########
+$rpt = @()
+$rpt += Get-HtmlOpen -TitleText $ReportName
+ 
+# adding the summary section 
+$rpt += Get-HtmlContentOpen -HeaderText "Summary Information"
+$rpt += Get-HtmlContenttext -Heading "Total VMs" -Detail ( $RMVMArray.Count)
+$rpt += Get-HtmlContenttext -Heading "VM Power State" -Detail ("Running " + ($RMVMArray | ? {$_.PowerState -eq 'Running'} | measure ).count + " / Deallocated " + ($RMVMArray | ? {$_.PowerState -eq 'Deallocated'} | measure ).count)
+$rpt += Get-HtmlContenttext -Heading "Total Data Disks" -Detail $RMVMArray.datadisks.count
+$rpt += Get-HtmlContentClose
+ 
+# adding the VM size section.  Note the -IsHidden switch
+$rpt += Get-HtmlContentOpen -HeaderText "VM Size Summary" -IsHidden
+$rpt += Get-HtmlContenttable ($RMVMArray | group size | select Name, Count | sort count -Descending ) -Fixed
+$rpt += Get-HtmlContentClose
+ 
+# Note I have also added the -IsHidden Switch here
+$rpt += Get-HtmlContentOpen -HeaderText "Virtual Machines" -IsHidden
+$rpt += Get-HtmlContentTable ($RMVMArray | select Location, ResourceGroup, Name, Size,PowerState, DataDiskCount, ImageSKU ) -GroupBy Location
+$rpt += Get-HtmlContentClose
+$rpt += Get-HtmlClose
+ 
+Test-Report -TestName Example3
+
+####### Example 4 ########
+$rpt = @()
+$rpt += Get-HtmlOpen -TitleText $ReportName
+$rpt += Get-HtmlContentOpen -HeaderText "Summary Information"
+$rpt += Get-HtmlContenttext -Heading "Total VMs" -Detail ( $RMVMArray.Count)
+$rpt += Get-HtmlContenttext -Heading "VM Power State" -Detail ("Running " + ($RMVMArray | ? {$_.PowerState -eq 'Running'} | measure ).count + " / Deallocated " + ($RMVMArray | ? {$_.PowerState -eq 'Deallocated'} | measure ).count)
+$rpt += Get-HtmlContenttext -Heading "Total Data Disks" -Detail $RMVMArray.datadisks.count
+$rpt += Get-HtmlContentClose
+$rpt += Get-HtmlContentOpen -HeaderText "VM Size Summary" -IsHidden
+$rpt += Get-HtmlContenttable ($RMVMArray | group size | select Name, Count | sort count -Descending ) -Fixed
+$rpt += Get-HtmlContentClose
+ 
+# We are introducing -BackgroundShade 2 so that we can clearly see the sections.  
+# This helps with larger reports and many when there are many levels to the sections
+$rpt += Get-HtmlContentOpen -HeaderText "Virtual Machines by location" -IsHidden
+ 
+# adding the foreach loop for the group recordset.   
+foreach ($Group in ($RMVMArray | select Location, ResourceGroup, Name, Size,PowerState,  DataDiskCount, ImageSKU | group location ) ) {
+ 
+#for every group that exists for a location we will create an HTML section.  I have also specified the -BackgroupShade to 1
+    $rpt += Get-HtmlContentOpen -HeaderText ("Virtual Machines for location '" + $group.Name +"'") -IsHidden -BackgroundShade 1
+ 
+# Each recordset may have different data in the columns and therefore create different width in the table columns.
+# We would like it to look the same. We can use the -Fixed switch to produce evenly space columns for the table
+    $rpt += Get-HtmlContentTable ($Group.Group | select ResourceGroup, Name, Size,PowerState,  DataDiskCount, ImageSKU ) -Fixed
+    $rpt += Get-HtmlContentClose
+}
+$rpt += Get-HtmlContentClose
+$rpt += Get-HtmlClose
+ 
+Test-Report -TestName Example4
+
+####### Example 5 ########
+$rpt = @()
+$rpt += Get-HtmlOpen -TitleText ($ReportName + "Example 5")
+$rpt += Get-HtmlContentOpen -HeaderText "Summary Information" -BackgroundShade 1
+$rpt += Get-HtmlContenttext -Heading "Total VMs" -Detail ( $RMVMArray.Count)
+$rpt += Get-HtmlContenttext -Heading "VM Power State" -Detail ("Running " + ($RMVMArray | ? {$_.PowerState -eq 'Running'} | measure ).count + " / Deallocated " + ($RMVMArray | ? {$_.PowerState -eq 'Deallocated'} | measure ).count)
+$rpt += Get-HtmlContenttext -Heading "Total Data Disks" -Detail $RMVMArray.datadisks.count
+$rpt += Get-HtmlContentClose
+$rpt += Get-HtmlContentOpen -HeaderText "VM Size Summary" -IsHidden -BackgroundShade 1
+$rpt += Get-HtmlContenttable ($RMVMArray | group size | select Name, Count | sort count -Descending ) -Fixed
+$rpt += Get-HtmlContentClose
+$rpt += Get-HtmlContentOpen -HeaderText "Virtual Machines by location" -BackgroundShade 3
+foreach ($Group in ($RMVMArray | select Location, ResourceGroup, Name, Size,PowerState,  DataDiskCount, ImageSKU | group location ) ) {
+ 
+# Here we are creating a group to use for the IF condition, so we can create sections for VMs by powerstate, Running or Deallocated 
+    $PowerState = $Group.Group | group PowerState 
+    $rpt += Get-HtmlContentOpen -HeaderText ("Virtual Machines for location '" + $group.Name +"' - "  + $Group.Group.Count + " VMs") -IsHidden -BackgroundShade 2
+ 
+# If there are VMs in the running group, continue and create a section for them 
+        if (($PowerState | ? {$_.name -eq 'running'})) 
+                {
+            $rpt += Get-HtmlContentOpen -HeaderText ("Running Virtual Machines") -BackgroundShade 1
+            $rpt += Get-HtmlContentTable ($Group.Group | where {$_.PowerState -eq "Running"} | select ResourceGroup, Name, Size,  DataDiskCount, ImageSKU  ) -Fixed
+            $rpt += Get-HtmlContentClose
+        }
+ 
+# If there are VMs in the running group, continue and create a section for them
+        if (($PowerState | ? {$_.name -eq 'Deallocated'})) 
+                {
+            $rpt += Get-HtmlContentOpen -HeaderText ("Deallocated") -BackgroundShade 1 -IsHidden
+            $rpt += Get-HtmlContentTable ($Group.Group  | where {$_.PowerState -eq "Deallocated"} | select ResourceGroup, Name, Size,  DataDiskCount, ImageSKU)-Fixed 
+            $rpt += Get-HtmlContentClose
+        }
+    $rpt += Get-HtmlContentClose
+}
+$rpt += Get-HtmlContentClose
+$rpt += Get-HtmlClose
+ 
+Test-Report -TestName Example5
+
+Invoke-Item $ReportOutputPath
---
+++ ReportHTML/ReportHTML.psd1
@@ -0,0 +1,105 @@
+﻿<#	
+	===========================================================================
+	 Created with: 	SAPIEN Technologies, Inc., PowerShell Studio 2016 v5.2.127
+	 Created on:   	8/3/2016 1:46 PM
+	 Created by:   	matt.quickenden
+	 Organization: 	
+	 Filename:     	ReportHTML.psd1
+	 -------------------------------------------------------------------------
+	 Module Manifest
+	-------------------------------------------------------------------------
+	 Module Name: ReportHTML
+	===========================================================================
+#>
+
+@{
+
+# Script module or binary module file associated with this manifest
+ModuleToProcess = 'ReportHTML.psm1'
+
+# Version number of this module.
+ModuleVersion = '1.0.0.0'
+
+# ID used to uniquely identify this module
+GUID = '13c25942-f436-44e1-b236-66e3ae11a8a0'
+
+# Author of this module
+Author = 'matt.quickenden'
+
+# Company or vendor of this module
+CompanyName = ''
+
+# Copyright statement for this module
+Copyright = '(c) 2016. All rights reserved.'
+
+# Description of the functionality provided by this module
+Description = 'Module description'
+
+# Minimum version of the Windows PowerShell engine required by this module
+PowerShellVersion = '2.0'
+
+# Name of the Windows PowerShell host required by this module
+PowerShellHostName = ''
+
+# Minimum version of the Windows PowerShell host required by this module
+PowerShellHostVersion = ''
+
+# Minimum version of the .NET Framework required by this module
+DotNetFrameworkVersion = '2.0'
+
+# Minimum version of the common language runtime (CLR) required by this module
+CLRVersion = '2.0.50727'
+
+# Processor architecture (None, X86, Amd64, IA64) required by this module
+ProcessorArchitecture = 'None'
+
+# Modules that must be imported into the global environment prior to importing
+# this module
+RequiredModules = @()
+
+# Assemblies that must be loaded prior to importing this module
+RequiredAssemblies = @()
+
+# Script files (.ps1) that are run in the caller's environment prior to
+# importing this module
+ScriptsToProcess = @()
+
+# Type files (.ps1xml) to be loaded when importing this module
+TypesToProcess = @()
+
+# Format files (.ps1xml) to be loaded when importing this module
+FormatsToProcess = @()
+
+# Modules to import as nested modules of the module specified in
+# ModuleToProcess
+NestedModules = @()
+
+# Functions to export from this module
+FunctionsToExport = '*'
+
+# Cmdlets to export from this module
+CmdletsToExport = '*'
+
+# Variables to export from this module
+VariablesToExport = '*'
+
+# Aliases to export from this module
+AliasesToExport = '*'
+
+# List of all modules packaged with this module
+ModuleList = @()
+
+# List of all files packaged with this module
+FileList = @()
+
+# Private data to pass to the module specified in ModuleToProcess
+PrivateData = ''
+
+}
+
+
+
+
+
+
+
