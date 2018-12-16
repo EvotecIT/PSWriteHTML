0cd524b9e764760fa74a07e36291bac59f02a192
PrzemyslawKlys <pklys@evotec.pl>
2018-12-16 20:19:09 +0100

    Update

--- Public/Get-HTMLContentOpen.ps1
+++ Public/Get-HTMLContentOpen.ps1
@@ -1,6 +1,5 @@
-Function Get-HTMLContentOpen
-{
-<#
+Function Get-HTMLContentOpen {
+    <#
 	.SYNOPSIS
 		Creates a section in HTML
 	    .PARAMETER HeaderText
@@ -13,21 +12,20 @@ Function Get-HTMLContentOpen
     [CmdletBinding()]
     Param
     (
-	    [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory = $false)]
         [string]$HeaderText,
-	    [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory = $false)]
         [switch]$IsHidden,
-	    [Parameter(Mandatory=$false)]
+        [Parameter(Mandatory = $false)]
         [string]$Anchor,
-	    [Parameter(Mandatory=$false)]
-        [validateset(1,2,3,4,5,6)]
+        [Parameter(Mandatory = $false)]
+        [validateset(1, 2, 3, 4, 5, 6)]
         [int]$BackgroundShade
     )
 
-    switch ($BackgroundShade)
-    {
+    switch ($BackgroundShade) {
         1 { $bgColorCode = "#F8F8F8" }
-	    2 { $bgColorCode = "#D0D0D0" }
+        2 { $bgColorCode = "#D0D0D0" }
         3 { $bgColorCode = "#A8A8A8" }
         4 { $bgColorCode = "#888888" }
         5 { $bgColorCode = "#585858" }
@@ -36,27 +34,26 @@ Function Get-HTMLContentOpen
     }
 
     if ([String]::IsNullOrEmpty($Anchor)) {
-	    $InsertAnchor = 'name="' + $Anchor +'"'
+        $InsertAnchor = 'name="' + $Anchor + '"'
     }
 
-if ($IsHidden) {
-	$RandomNumber = Get-Random
-	$Report = @"
+    if ($IsHidden) {
+        $RandomNumber = Get-Random
+        $Report = @"
 <div class="section">
 <div class="header">
     <a name="$($HeaderText)">$($HeaderText)</a> (<a id="show_$RandomNumber" href="#" onclick="show('$RandomNumber');" style="color: #ffffff;">Show</a><a id="hide_$RandomNumber" href="#" onclick="hide('$RandomNumber');" style="color: #ffffff; display:none;">Hide</a>)
 </div>
 <div class="content" id="$RandomNumber" style="display:none;background-color:$($bgColorCode);">
 "@
-}
-else {
-	$Report = @"
+    } else {
+        $Report = @"
 <div class="section">
 <div class="header">
     <a name="$($HeaderText)">$($HeaderText)</a>
 </div>
 <div class="content" style="background-color:$($bgColorCode);">
 "@
-}
-	Return $Report
+    }
+    Return $Report
 }
