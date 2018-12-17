d763c8e015fef66a74807ddf712cd66090382d55
PrzemyslawKlys <pklys@evotec.pl>
2018-12-17 18:04:02 +0100

    Upgraded datatable to higher version.

--- Public/Get-HTMLOpenPage.ps1
+++ Public/Get-HTMLOpenPage.ps1
@@ -85,10 +85,11 @@ Function Get-HTMLOpenPage {
 
         Write-Verbose "Logos: $($Options.Logos.Keys -join ',')"
         foreach ($Logo in $Options.Logos.Keys) {
-            $Search = "../images/$Logo.png"
+            $Search = "../images/$Logo.png", "DataTables-1.10.18/images/$Logo.png"
             $Replace = $Options.Logos[$Logo]
-
-            $Options.StyleContent = ($Options.StyleContent).Replace($Search, $Replace)
+            foreach ($S in $Search) {
+                $Options.StyleContent = ($Options.StyleContent).Replace($S, $Replace)
+            }
         }
     }
 
