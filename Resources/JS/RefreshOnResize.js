7ad92443fa3f4013261fc35c9b57e9a5edfe1d80
PrzemyslawKlys <pklys@evotec.pl>
2018-12-18 00:07:41 +0100

    Added RefreshOnResize to prevent row problems

---
+++ Resources/JS/RefreshOnResize.js
@@ -0,0 +1,9 @@
+//refresh page on browser resize
+$(window).bind('resize', function(e)
+{
+  if (window.RT) clearTimeout(window.RT);
+  window.RT = setTimeout(function()
+  {
+    this.location.reload(false); /* false to get page from cache */
+  }, 200);
+});
