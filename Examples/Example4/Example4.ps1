571d7229718b4f15d40704723e2b9f7c8303dcf2
PrzemyslawKlys <pklys@evotec.pl>
2019-01-12 14:15:22 +0100

    Update

--- Examples/Example4/Example4.ps1
+++ Examples/Example4/Example4.ps1
@@ -10,22 +10,17 @@ $DomainAdminTable = Get-ADForest | Select-Object ForestMode, Name, RootDomain, S
 $EnterpriseAdminTable = Get-ADuser -Filter * | Select-Object Name, Surname, Enabled, DisplayName
 $Allusers = Get-AdUser -Filter *
 
-$Report = New-GenericList
-
 $TabNames = 'Dashboard', 'Something'
 
-$Report.Add($(New-HTML -Open -TitleText $ReportTitle -HideLogos -AddAuthor -HideDate -UseCssLinks -UseStyleLinks -Verbose))
-$Report.Add($(Get-HTMLTabHeader -TabNames $TabNames))
-$Report.Add($(Get-HTMLTab -Open -TabName 'Dashboard'))
-$Report.Add($(Get-HTMLContent -Open -HeaderText "Groups"))
-$Report.Add($(Get-HTMLContent -Open -BackgroundShade 1 -HeaderText 'Domain Administrators' -CanCollapse ))
-$Report.Add($(Get-HTMLContentTable $DomainAdminTable -Verbose))
-$Report.Add($(Get-HTMLContent -Close))
-$Report.Add($(Get-HTMLContent -Close))
-$Report.Add($(Get-HTMLTab -Close))
-$Report.Add($(New-HTML -Close))
-
+$Report = New-HTMLPage -TitleText $ReportTitle -HideLogos -AddAuthor -HideDate -UseCssLinks -UseStyleLinks -Verbose {
+    New-HTMLTabHeader -TabNames $TabNames
+    New-HTMLTab -TabName 'Dashboard' {
+        New-HTMLContent -HeaderText "Groups" {
+            New-HTMLContent -BackgroundShade 1 -HeaderText 'Domain Administrators' -CanCollapse {
+                Get-HTMLContentTable -DataTable $DomainAdminTable -Verbose
+            }
+        }
+    }
+}
 Save-HTML -HTML $Report -FilePath $ReportPath -ShowHTML
-
-
 Stop-TimeLog -Time $Time -Option OneLiner
--- Public/New-HTMLPage.ps1
+++ Public/New-HTMLPage.ps1
@@ -1,17 +1,35 @@
 Function New-HtmlPage {
     [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $true, Position = 0)][string] $Name,
-        [Parameter(Position = 1)][ValidateNotNull()][ScriptBlock] $ServiceData = $(Throw "No test script block is provided. (Have you put the open curly brace on the next line?)"),
+        [Parameter(Position = 0)][ValidateNotNull()][ScriptBlock] $HtmlData = $(Throw "Have you put the open curly brace on the next line?"),
         [switch] $UseCssLinks,
-        [switch] $UseStyleLinks
+        [switch] $UseStyleLinks,
+        [String] $TitleText,
+        [String] $CSSPath,
+        [String] $CSSName = "default",
+        [String] $ScriptPath,
+        [String] $ColorSchemePath,
+        [String] $LogoPath,
+        [string] $LeftLogoName = "Sample",
+        [string] $RightLogoName = "Alternate",
+        [string] $LeftLogoString,
+        [string] $RightLogoString,
+        [switch] $HideLogos,
+        [switch] $HideTitle,
+        [switch] $NoScript,
+        [string] $PrimaryColorHex,
+        [switch] $AddAuthor,
+        [string] $Author,
+        [switch] $HideDate,
+        [string] $DateFormat = 'yyyy-MM-dd HH:mm:ss',
+        [String] $FooterText
     )
     Begin {}
     Process {
         $HTML = "<HTML>"
-        $HTML += Add-HTMLHead -UseStyleLinks:$UseStyleLinks -UseCssLinks:$UseCssLinks
+        $HTML += Add-HTMLHead -UseStyleLinks:$UseStyleLinks -UseCssLinks:$UseCssLinks -TitleText $TitleText -AddAuthor $AddAuthor -HideTitle:$HideTitle
         $HTML += "<BODY>"
-        $HTML += Invoke-Command -ScriptBlock $ServiceData
+        $HTML += Invoke-Command -ScriptBlock $HtmlData
         $HTML += Get-JavaScript -FilePath "$PSScriptRoot\..\Resources\JS\Additional\LoadEnlighter.js"
         $HTML += "</BODY>"
     }
