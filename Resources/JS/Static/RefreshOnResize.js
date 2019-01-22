bf53afb4c0832a88e8c5dc36989a15fccb899536
PrzemyslawKlys <pklys@evotec.pl>
2019-01-22 09:30:47 +0100

    Removed refresh, fixing it other way

--- Resources/JS/Static/RefreshOnResize.js
+++
@@ -1,7 +0,0 @@
-//refresh page on browser resize
-$(window).bind("resize", function(e) {
-    if (window.RT) clearTimeout(window.RT);
-    window.RT = setTimeout(function() {
-        this.location.reload(false); /* false to get page from cache */
-    }, 200);
-});
