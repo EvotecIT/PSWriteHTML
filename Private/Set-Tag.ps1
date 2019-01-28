ed60dea81d676f6cc55c44a2f064c0a8a4b47b96
PrzemyslawKlys <pklys@evotec.pl>
2019-01-28 21:59:48 +0100

    More support

--- Private/Set-Tag.ps1
+++ Private/Set-Tag.ps1
@@ -9,20 +9,27 @@ function Set-Tag {
         $PropertyValue = $HtmlObject.Attributes[$Property]
         # skip adding properties that are empty
         if ($PropertyValue -ne '') {
-            $HTML.Add("$Property = `"$PropertyValue`"")
+            $HTML.Add("$Property=`"$PropertyValue`"")
         }
     }
     if (($null -ne $HtmlObject.Value) -and ($HtmlObject.Value -ne '')) {
         $HTML.Add(">")
-        if ($HtmlObject.Value -is [System.Collections.IDictionary]) {
-            [string] $NewObject = Set-Tag -HtmlObject ($HtmlObject.Value)
-            $HTML.Add($NewObject)
-        } else {
-            $HTML.Add($HtmlObject.Value)
+        foreach ($Entry in $HtmlObject.Value) {
+            if ($Entry -is [System.Collections.IDictionary]) {
+                [string] $NewObject = Set-Tag -HtmlObject ($Entry)
+                $HTML.Add($NewObject)
+            } else {
+                $HTML.Add($Entry)
+            }
+
         }
         $HTML.Add("</$($HtmlObject.Tag)>")
     } else {
-        $HTML.Add("/>")
+        if ($HtmlObject.SelfClosing) {
+            $HTML.Add("/>")
+        } else {
+            $HTML.Add("></$($HtmlObject.Tag)>")
+        }
     }
     $HTML
 }
