5bfd92e093cf384169dcd683b7adc342a919318f
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-04-04 19:35:57 +0200

    Update

--- Examples/NotWorking/Example17-Tables/Example17.ps1
+++ Examples/NotWorking/Example17-Tables/Example17.ps1
@@ -1,13 +1,16 @@
 Import-Module .\PSWriteHTML.psd1 -Force
 
-$Process = Get-Process | Select-Object -First 30
+$Process = Get-Process | Select-Object -First 5
 
-New-HTML -TitleText 'My title' -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\Example17.html {
-    New-HTMLContent -HeaderText 'Test' {
-        New-HTMLTable -DataTable $Process
+New-HTML -TitleText 'My title' -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\Example18.html {
+    New-HTMLTab -Name 'Test 1' {
+        New-HTMLContent -HeaderText 'Test 3' {
+            New-HTMLTable -DataTable $Process
+        }
     }
-    New-HTMLContent -HeaderText 'Test' {
-        New-HTMLTable -DataTable $Process -DefaultSortIndex 3
+    New-HTMLTab -Name 'Test2' {
+        New-HTMLContent -HeaderText 'Test 4' {
+            New-HTMLTable -DataTable $Process
+        }
     }
-    New-HTMLTable -DataTable $Process
 }
