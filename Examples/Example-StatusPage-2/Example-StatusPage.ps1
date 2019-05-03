b40c25c0102531fbe709091d2346cf164de23c25
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-05-03 14:48:29 +0200

    Update

---
+++ Examples/Example-StatusPage-2/Example-StatusPage.ps1
@@ -0,0 +1,89 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Time = Start-TimeLog
+$DynamicHTML = New-HTML -TitleText 'Services Status' -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\StatusPage.html {
+    New-HTMLSection -Invisible {
+        New-HTMLPanel -Invisible {
+            New-HTMLToast -Icon Information -Color Blue -TextHeader 'Information' -Text 'Everything is running smoothly!' -Type ''
+            New-HTMLToast -Icon Information -Color Orange -TextHeader 'Helloo' -Text 'Everything is running smoothly!' -Type ''
+            New-HTMLToast -Icon Information -Color Blue -TextHeader 'Information' -Text 'Everything is running smoothly!' -Type ''
+        }
+        New-HTMLPanel -Invisible {
+            New-HTMLTimeline {
+                New-HTMLTimelineItem -HeadingText 'Azure AD is down' -Text "We have huge problems" -Date (Get-Date).AddDays(-1)
+                New-HTMLTimelineItem -HeadingText 'Azure AD ...' -Text "We have huge problems" -Date (Get-Date).AddDays(-2)
+                New-HTMLTimelineItem -HeadingText 'AD just died' -Text "We have huge problems" -Date (Get-Date).AddDays(-3)
+            }
+        }
+    }
+}
+
+return
+
+$DynamicHTML = New-HTML -TitleText 'Services Status' -UseCssLinks:$true -UseJavaScriptLinks:$true {
+
+    New-HTMLPanel -Invisible {
+        New-HTMLToast -Icon Information -Color Blue -TextHeader 'Information' -Text 'Everything is running smoothly!'
+    }
+
+    New-HTMLHeading -Heading h1 -HeadingText 'Current Status' -Type 'central'
+
+    New-HTMLPanel -Invisible {
+        New-HTMLStatus {
+            New-HTMLStatusItem -ServiceName 'Active Directory' -ServiceStatus 'Operational' -Icon Good -Percentage '100%'
+            New-HTMLStatusItem -ServiceName 'Github' -ServiceStatus 'Operational' -Icon Good -Percentage '100%'
+            New-HTMLStatusItem -ServiceName 'Hyper-V' -ServiceStatus 'Non-functional' -Icon Dead -Percentage '0%'
+            New-HTMLStatusItem -ServiceName 'Active Directory' -ServiceStatus 'Operational' -Icon Good -Percentage '100%'
+            New-HTMLStatusItem -ServiceName 'Azure' -ServiceStatus 'Working on it' -Icon Bad -Percentage '70%'
+            New-HTMLStatusItem -ServiceName 'Twitter' -ServiceStatus 'Who knows?!' -Icon Bad -Percentage '30%'
+        }
+    }
+
+    New-HTMLHeading -Heading h1 -HeadingText 'Scheduled Maintenance' -Type 'central'
+
+    New-HTMLPanel -Invisible {
+        New-HTMLToast -Icon Information -Color Orange -TextHeader 'Maintenance' -Text "We've planned maintenance on 24th of January 2020. It will last 30 hours."
+    }
+
+
+    New-HTMLHeading -Heading h1 -HeadingText 'Incidents per day' -Type 'central'
+
+    New-HTMLPanel -Invisible {
+        $Data = foreach ($Element in 30..0) {
+            Get-Random -Minimum 0 -Maximum 5
+        }
+        $DataName = "Incidents"
+        $DataCategories = foreach ($Element in 30..0) {
+            (Get-Date).AddDays(-$Element).ToShortDateString()
+        }
+
+        New-HTMLChart `
+            -Data $Data `
+            -DataNames $DataName `
+            -DataCategories $DataCategories `
+            -Type 'line' `
+            -TitleText '' `
+            -TitleAlignment 'left' `
+            -LineColor 'Blue' -Horizontal $false -Positioning central
+    }
+
+
+    New-HTMLHeading -Heading h1 -HeadingText 'Past Incidents' -Type 'central'
+
+    New-HTMLPanel -Invisible {
+        New-HTMLTimeline {
+            New-HTMLTimelineItem -HeadingText 'Azure AD is down' -Text "We have huge problems" -Date (Get-Date).AddDays(-1)
+            New-HTMLTimelineItem -HeadingText 'Azure AD ...' -Text "We have huge problems" -Date (Get-Date).AddDays(-2)
+            New-HTMLTimelineItem -HeadingText 'AD just died' -Text "We have huge problems" -Date (Get-Date).AddDays(-3)
+            New-HTMLTimelineItem -HeadingText 'Twitter' -Text "We're very soorrry! It just won't work!" -Date (Get-Date).AddDays(-4)
+            New-HTMLTimelineItem -HeadingText 'Twitter' -Text "We're very soorrry! It just won't work!" -Date (Get-Date).AddDays(-4)
+            New-HTMLTimelineItem -HeadingText 'Twitter' -Text "We're very soorrry! It just won't work!" -Date (Get-Date).AddDays(-4)
+            New-HTMLTimelineItem -HeadingText 'Twitter' -Text "We're very soorrry! It just won't work!" -Date (Get-Date).AddDays(-5)
+            New-HTMLTimelineItem -HeadingText 'Twitter' -Text "We're very soorrry! It just won't work!" -Date (Get-Date).AddDays(-6)
+            New-HTMLTimelineItem -HeadingText 'Twitter' -Text "We're very soorrry! It just won't work!" -Date (Get-Date).AddDays(-7)
+        }
+    }
+}
+
+[string] $DynamicHTMLPath = Save-HTML -HTML $DynamicHTML -FilePath 'C:\Support\GitHub\PSWriteHTML\Examples\Example-StatusPage\Example-StatusPage.html'
+Stop-TimeLog -Time $Time
---
+++ Examples/Example20-TableConditionsWithSomeText/Example20.ps1
@@ -0,0 +1,45 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Process = Get-Process | Select-Object -First 70
+
+New-HTML -TitleText 'My title' -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\Example20.html {
+    New-HTMLContent -HeaderText 'Test' {
+        New-HTMLTable -DataTable $Process -PagingOptions @(50, 100, 150, 200) -Find 'chrome' {
+            New-HTMLText -Text 'Test test' -Alignment center -Color Red -FontSize 20
+            New-HTMLTableCondition -Name 'HandleCount' -Type number -Operator gt -Value 300 -BackgroundColor Yellow
+            New-HTMLTableCondition -Name 'ID' -Type number -Operator gt -Value 16000 -BackgroundColor Green
+            New-HTMLTableCondition -Name 'Name' -Type string -Operator eq -Value 'browser_broker' -BackgroundColor Gold -Row
+        }
+        New-HTMLTable -DataTable $Process -PagingOptions @(50, 100, 150, 200) -DefaultSortColumn 'PriorityClass' {
+            New-HTMLText -Text 'Test test' -Alignment center -Color Red -FontSize 20
+            New-HTMLText -Text 'Test test' -Alignment center -Color Red -FontSize 20
+            New-HTMLTableCondition -Name 'HandleCount' -Type number -Operator gt -Value 300 -BackgroundColor Yellow
+            New-HTMLTableCondition -Name 'ID' -Type number -Operator gt -Value 16000 -BackgroundColor Green
+            New-HTMLTableCondition -Name 'Name' -Type string -Operator eq -Value 'browser_broker' -BackgroundColor Gold -Row
+        }
+    }
+    New-HTMLContent -HeaderText 'Test' {
+        New-HTMLContainer {
+            New-HTMLText -Text 'Test test' -Alignment center -Color Red -FontSize 20
+            New-HTMLTable -DataTable $Process -PagingOptions @(50, 100, 150, 200) -DefaultSortColumn 'PriorityClass' {
+                New-HTMLTableCondition -Name 'HandleCount' -Type number -Operator gt -Value 300 -BackgroundColor Yellow
+                New-HTMLTableCondition -Name 'ID' -Type number -Operator gt -Value 16000 -BackgroundColor Green
+                New-HTMLTableCondition -Name 'Name' -Type string -Operator eq -Value 'browser_broker' -BackgroundColor Gold -Row
+            }
+        }
+        New-HTMLContainer {
+            New-HTMLText -Text 'Test test1' -Alignment center -Color Red -FontSize 20
+            New-HTMLTable -DataTable $Process -PagingOptions @(50, 100, 150, 200) -DefaultSortColumn 'PriorityClass' {
+                New-HTMLTableCondition -Name 'HandleCount' -Type number -Operator gt -Value 300 -BackgroundColor Yellow
+                New-HTMLTableCondition -Name 'ID' -Type number -Operator gt -Value 16000 -BackgroundColor Green
+                New-HTMLTableCondition -Name 'Name' -Type string -Operator eq -Value 'browser_broker' -BackgroundColor Gold -Row
+            }
+            New-HTMLText -Text 'Test test2' -Alignment center -Color Red -FontSize 20
+        }
+    }
+    New-HTMLTable -DataTable $Process -DefaultSortIndex 10 -OrderMulti {
+        New-HTMLTableCondition -Name 'HandleCount' -Type number -Operator gt -Value 300 -BackgroundColor Yellow
+        New-HTMLTableCondition -Name 'ID' -Type number -Operator gt -Value 16000 -BackgroundColor Green
+        New-HTMLTableCondition -Name 'Name' -Type string -Operator eq -Value 'browser_broker' -BackgroundColor Gold -Row
+    }
+}
