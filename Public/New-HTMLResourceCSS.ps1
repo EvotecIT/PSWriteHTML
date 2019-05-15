ce866a2b252864c707c19008194164f8ef75518d
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-05-15 19:56:17 +0200

    Fix for multiple CSS

--- Public/New-HTMLResourceCSS.ps1
+++ Public/New-HTMLResourceCSS.ps1
@@ -5,24 +5,24 @@ function New-HTMLResourceCSS {
         [alias('ScriptContent')][Parameter(Mandatory = $false, Position = 0)][ValidateNotNull()][ScriptBlock] $Content,
         [string] $Link,
         [string] $ResourceComment,
-        [string] $FilePath
-    
+        [string[]] $FilePath
+
     )
     $Output = @(
         "<!-- CSS $ResourceComment START -->"
         foreach ($File in $FilePath) {
             if ($File -ne '') {
-                if (Test-Path -LiteralPath $File) {                
-                    New-HTMLTag -Tag 'style' -Attributes @{ type = 'text/css' } {    
-                        Write-Verbose "New-HTMLResourceCSS - Reading file from $File"        
-                        Get-Content -LiteralPath $File           
+                if (Test-Path -LiteralPath $File) {
+                    New-HTMLTag -Tag 'style' -Attributes @{ type = 'text/css' } {
+                        Write-Verbose "New-HTMLResourceCSS - Reading file from $File"
+                        Get-Content -LiteralPath $File
                     }
-                } 
+                }
             }
         }
-        foreach ($L in $Link) {   
+        foreach ($L in $Link) {
             if ($L -ne '') {
-                Write-Verbose "New-HTMLResourceCSS - Adding link $L"   
+                Write-Verbose "New-HTMLResourceCSS - Adding link $L"
                 New-HTMlTag -Tag 'link' -Attributes @{ rel = "stylesheet"; type = "text/css"; href = $L } -SelfClosing
             }
         }
@@ -31,5 +31,5 @@ function New-HTMLResourceCSS {
     if ($Output.Count -gt 2) {
         # Outputs only if more than comments
         $Output
-    } 
+    }
 }
