ab288b9118677583f86a3ba1c416fb901bc18915
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-06-20 15:14:20 +0200

    Added example

---
+++ Examples/Example01-TableColumnSort/Example01.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Process = Get-Process | Select-Object -First 30
+
+New-HTML -Name 'Dashimo Test' -FilePath $PSScriptRoot\Example01.html -AutoRefresh 15 -Show {
+    New-HTMLTable -DataTable $Process -DefaultSortIndex 4 -ScrollCollapse -HideFooter -Buttons @()
+
+    New-HTMLTable -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -HideFooter -Buttons @()
+
+    New-HTMLTable -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -DefaultSortOrder Descending -HideFooter -Buttons @()
+}
