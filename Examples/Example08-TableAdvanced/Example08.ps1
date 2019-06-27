34342c8591e9ca22286feff797af10c5383d1647
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-06-28 00:47:18 +0200

    Examples for new features

---
+++ Examples/Example08-TableAdvanced/Example08.ps1
@@ -0,0 +1,95 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+
+$Properties = @(
+    'Name'
+    'Id'
+    'PriorityClass'
+    'FileVersion'
+    'HandleCount'
+    'WorkingSet'
+    'PagedMemorySize'
+    'PrivateMemorySize'
+    'VirtualMemorySize'
+    'TotalProcessorTime'
+    <#
+    'SI'
+    'Handles'
+    'VM'
+    'WS'
+    'PM'
+    'NPM'
+    #>
+)
+$ProcessesAll = Get-Process | Select-Object -First 30
+$Processes = $ProcessesAll | Select-Object -First 30 -Property $Properties
+
+New-HTML -TitleText 'Title' -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\Example08.html -ShowHTML {
+    New-HTMLContent -HeaderText '0 section' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $Processes -HideFooter -ScrollCollapse {
+                New-HTMLTableHeader -Names 'Name', 'ID' -Title 'Process Information' -Color Red -FontWeight lighter -Alignment left -BackGroundColor LightBlue
+                New-HTMLTableHeader -Names 'PagedMemorySize', 'PrivateMemorySize', 'VirtualMemorySize' -Title 'Memory' -Color White -BackGroundColor Blue
+                New-HTMLTableHeader -Names 'Name' -BackGroundColor Red -Color WhiteSmoke
+                New-HTMLTableHeader -Names 'Id' -BackGroundColor Blue -Color White
+                New-HTMLTableHeader -Names 'PriorityClass', 'FileVersion', 'HandleCount' -BackGroundColor Gold -Color White
+                New-HTMLTableHeader -BackGroundColor Green -Color White -AddRow -Title 'Full Title'
+                New-HTMLTableCondition -Name 'HandleCount' -ComparisonType number -Operator gt 500 -BackgroundColor Gray -Color White -Row
+            }
+        }
+    }
+
+    New-HTMLContent -HeaderText '1 section' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $Processes {
+                New-HTMLTableHeader -Names 'Name', 'ID' -Title 'Process Information'
+                New-HTMLTableHeader -Names 'PriorityClass', 'FileVersion', 'HandleCount' -BackGroundColor Gold -Color White
+                New-HTMLTableHeader -Names 'PriorityClass', 'FileVersion', 'HandleCount', 'WorkingSet' -Title 'Merge' -BackGroundColor Orchid
+                New-HTMLTableHeader -Names 'PagedMemorySize', 'PrivateMemorySize', 'VirtualMemorySize' -Title 'Memory'
+                New-HTMLTableHeader -BackGroundColor Green -Color White -AddRow -Title 'Half Title' -ColumnCount 3
+                New-HTMLTableHeader -BackGroundColor Red -Color White -AddRow -Title 'Other Half Title'
+            } -Filtering
+        }
+    }
+    New-HTMLContent -HeaderText '2 section' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $ProcessesAll -HideFooter -DisableResponsiveTable {
+                New-HTMLTableHeader -Names 'Name', 'ID' -Title 'Process Information'
+                New-HTMLTableHeader -Names 'PagedMemorySize', 'PrivateMemorySize', 'VirtualMemorySize' -Title 'Memory'
+            }
+        }
+    }
+
+    New-HTMLContent -HeaderText '3 section' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $Processes  {
+                New-HTMLTableHeader -Names 'PriorityClass', 'FileVersion', 'HandleCount' -BackGroundColor Gold -Color White
+                New-HTMLTableHeader -BackGroundColor Red -Color White -AddRow -Title '1 Half Title' -ColumnCount 3
+                New-HTMLTableHeader -BackGroundColor Yellow -Color Black -AddRow -Title '2 Half Title' -ColumnCount 3
+                New-HTMLTableHeader -BackGroundColor Green -Color White -AddRow -Title '3 Half Title'
+            } -HideFooter
+        }
+    }
+    New-HTMLContent -HeaderText '4 section' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $ProcessesAll -DisableResponsiveTable
+        }
+    }
+    New-HTMLContent -HeaderText 'Demo for ScrollX - disables responsive table automatically' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $ProcessesAll -HideFooter -ScrollX
+        }
+    }
+    New-HTMLContent -HeaderText 'Demo for ScrollX - disables responsive table automatically' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $ProcessesAll -HideFooter -ScrollX -FreezeColumnsRight 2 -FreezeColumnsLeft 2
+        }
+    }
+    New-HTMLContent -HeaderText 'Demo for ScrollY' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $ProcessesAll -HideFooter -ScrollY -DisablePaging
+        }
+    }
+
+}
+
---
+++ Examples/Example12-TableAdvancedFreeze/Example12.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$ProcessesAll = Get-Process
+
+New-HTML -TitleText 'Title' -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\Example12.html -ShowHTML {
+    New-HTMLContent -HeaderText 'Demo for ScrollY' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $ProcessesAll -DisablePaging -FixedHeader -FixedFooter
+        }
+    }
+}
