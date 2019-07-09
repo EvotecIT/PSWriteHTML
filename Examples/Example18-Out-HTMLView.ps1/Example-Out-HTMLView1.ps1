71b7fb35f2b47f16c8f9aa1c87509d7d9897bfa3
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-07-09 21:45:47 +0200

    Updated examples for Out-HtmlView

--- Examples/Example18-Out-HTMLView.ps1/Example-Out-HTMLView1.ps1
+++ Examples/Example18-Out-HTMLView.ps1/Example-Out-HTMLView1.ps1
@@ -2,4 +2,6 @@
 
 Get-Process | Select-Object -First 5 | Out-HtmlView -PriorityProperties PM, NPM, Company -DefaultSortColumn Name, NPM -Filtering -FilteringLocation Both -FilePath $PSScriptRoot\Test.html
 
-#Get-Process | Select-Object -First 5 | Out-HtmlView -DefaultSortIndex 5, 2
+Get-Process | Select-Object -First 5 | Out-HtmlView -DefaultSortIndex 5, 2 -HideFooter
+
+Get-Process | Select-Object -First 5 | Out-HtmlView -DefaultSortIndex 5, 2 -HideFooter -ScrollX
