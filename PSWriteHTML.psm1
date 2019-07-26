ad6955fbd0ae6de24a5be5490466f0417c16f2f5
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-07-26 21:37:33 +0200

    Update

--- PSWriteHTML.psm1
+++ PSWriteHTML.psm1
@@ -11,15 +11,3 @@ Foreach ($import in @($Public + $Private)) {
     }
 }
 Export-ModuleMember -Function '*' -Alias '*'
-
-[string] $ManifestFile = '{0}.psd1' -f (Get-Item $PSCommandPath).BaseName;
-$ManifestPathAndFile = Join-Path -Path $PSScriptRoot -ChildPath $ManifestFile;
-if ( Test-Path -Path $ManifestPathAndFile) {
-    $Manifest = (Get-Content -raw $ManifestPathAndFile) | Invoke-Expression;
-    foreach ( $ScriptToProcess in $Manifest.ScriptsToProcess) {
-        $ModuleToRemove = (Get-Item (Join-Path -Path $PSScriptRoot -ChildPath $ScriptToProcess)).BaseName;
-        if (Get-Module $ModuleToRemove) {
-            Remove-Module $ModuleToRemove;
-        }
-    }
-}
