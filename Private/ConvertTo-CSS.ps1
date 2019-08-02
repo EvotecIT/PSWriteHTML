fc86ae84184b0acc793973201df90369cb9e612c
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-02 22:10:10 +0200

    Rebuilt some CSS code building

---
+++ Private/ConvertTo-CSS.ps1
@@ -0,0 +1,19 @@
+﻿function ConvertTo-CSS {
+    [CmdletBinding()]
+    param(
+        [string] $ClassName,
+        [System.Collections.IDictionary] $Attributes
+    )
+
+    $Css = @(
+        ".$ClassName {"
+
+        foreach ($_ in $Attributes.Keys) {
+            "$($_): $($Attributes[$_]);"
+        }
+
+        '}'
+    ) -join "`n"
+    $CSS
+
+}
