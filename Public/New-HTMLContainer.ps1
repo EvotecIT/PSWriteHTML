0c3b1a5e34734a98699d32c9ad3c68e8f4364397
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-02 22:10:27 +0200

    Support for flex configuration in Container

--- Public/New-HTMLContainer.ps1
+++ Public/New-HTMLContainer.ps1
@@ -1,9 +1,25 @@
 ﻿function New-HTMLContainer {
+    [CmdletBinding()]
     param(
-        [Parameter(Mandatory = $false, Position = 0)][ScriptBlock] $HTML
+        [Parameter(Mandatory = $false, Position = 0)][ScriptBlock] $HTML,
+        [string] $Width,
+        [string] $Margin
     )
 
-    New-HTMLTag -Tag 'div' -Attributes @{ class = 'flexElement' } {
+    if ($Width -or $Margin) {
+        [string] $ClassName = "flexElement$(Get-RandomStringName -Size 8 -LettersOnly)"
+        $Attributes = @{
+            'flex-basis' = if ($Width) { $Width } else { '100%' }
+            'margin'     = if ($Margin) { $Margin }
+        }
+        $Css = ConvertTo-CSS -ClassName $ClassName -Attributes $Attributes
+
+        $Script:HTMLSchema.CustomCSS.Add($Css)
+        [string] $Class = "$ClassName overflowHidden"
+    } else {
+        [string] $Class = 'flexElement overflowHidden'
+    }
+    New-HTMLTag -Tag 'div' -Attributes @{ class = $Class } {
         if ($HTML) {
             Invoke-Command -ScriptBlock $HTML
         }
