828a8b3722bc3883efe3d70072c18e757468eef7
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-08-24 08:13:01 +0200

    Update

---
+++ Examples/Example13-Table/Example-13.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Processes = Get-Process | Select-Object -First 5 -Property Name, ID, HandleCount, WorkingSet
+$Title = 'My title'
+
+New-HTML -TitleText $Title -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\Example13.html {
+    New-HTMLContent -HeaderText '1 section' {
+        New-HTMLTable -ArrayOfObjects $Processes -HideFooter -ImmediatelyShowHiddenDetails
+    }
+    New-HTMLContent -HeaderText '2 section' {
+        New-HTMLTable -ArrayOfObjects $Processes -HideFooter -ImmediatelyShowHiddenDetails -HideShowButton
+    }
+} -ShowHTML
--- Examples/Example13-Table/Example13.ps1
+++ Examples/Example13-Table/Example13.ps1
@@ -4,26 +4,6 @@ $Processes = Get-Process | Select-Object -First 2 -Property Name, ID, HandleCoun
 $Title = 'My title'
 
 New-HTML -TitleText $Title -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\Example13.html {
-
-    <#
-    New-HTMLContent -HeaderText '0 section' {
-        New-HTMLPanel {
-            New-HTMLTable -ArrayOfObjects $Processes -HideFooter #-ImmediatelyShowHiddenDetails
-        }
-        New-HTMLPanel {
-            New-HTMLTable -ArrayOfObjects $Processes -HideFooter
-        }
-        New-HTMLPanel {
-            New-HTMLTable -ArrayOfObjects $Processes -HideFooter
-        }
-        New-HTMLPanel {
-            New-HTMLTable -ArrayOfObjects $Processes -HideFooter
-        }
-        New-HTMLPanel {
-            New-HTMLTable -ArrayOfObjects $Processes -HideFooter
-        }
-    }
-    #>
     New-HTMLContent -HeaderText '1 section' {
         New-HTMLTable -ArrayOfObjects $Processes -HideFooter {
             New-HTMLTableHeader -Names 'ID', 'HandleCount' -ResponsiveOperations none
@@ -34,24 +14,14 @@ New-HTML -TitleText $Title -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePat
             New-HTMLTableHeader -Names 'ID', 'HandleCount' -ResponsiveOperations none
         } -ImmediatelyShowHiddenDetails -HideShowButton
     }
-    New-HTMLContent -HeaderText '2 section' {
+    New-HTMLContent -HeaderText '3 section' {
         New-HTMLTable -ArrayOfObjects $Processes -HideFooter {
             New-HTMLTableHeader -Names 'ID', 'HandleCount' -ResponsiveOperations none
         } -ImmediatelyShowHiddenDetails -HideShowButton -DisableResponsiveTable
     }
-    <#
-    New-HTMLContent -Invisible {
-        New-HTMLPanel {
-            New-HTMLTable -ArrayOfObjects $Processes -HideFooter
-        }
-        New-HTMLPanel {
-            New-HTMLTable -ArrayOfObjects $Processes -HideFooter
-
+    New-HTMLContent -HeaderText '4 section' {
+        New-HTMLTable -ArrayOfObjects $Processes -HideFooter {
+            New-HTMLTableHeader -Names 'ID', 'HandleCount', 'WorkingSet' -ResponsiveOperations none
         }
     }
-    New-HTMLPanel {
-        New-HTMLTable -ArrayOfObjects $Processes -HideFooter
-
-    }
-    #>
 } -ShowHTML
