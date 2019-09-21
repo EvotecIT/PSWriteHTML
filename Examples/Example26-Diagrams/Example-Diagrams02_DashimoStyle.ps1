bed1be9bf2edee385fbaa04709014a446d9010e3
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-22 00:13:47 +0200

    Diagrams examples

---
+++ Examples/Example26-Diagrams/Example-Diagrams02_DashimoStyle.ps1
@@ -0,0 +1,15 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTML -TitleText 'My charts' -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\Example-Diagrams02_DashimoStyle.html {
+    Diagram {
+        DiagramOptionsInteraction -Hover $true
+        DiagramNode -Label '1 test' -To '3', '6', '7' -Background Bisque
+        DiagramNode -Label '2' -To '5', '1 test'
+        DiagramNode -Label '3'
+        DiagramNode -Label '4'
+        DiagramNode -Label '5'
+        DiagramNode -Label '6' -To '5' -Border Aquamarine -Background Yellow
+        DiagramNode -Label '7' -To '5' -HighlightBackground Red
+        DiagramNode -Label '8' -To '5' -HoverBorder Yellow
+    }
+} -ShowHTML
