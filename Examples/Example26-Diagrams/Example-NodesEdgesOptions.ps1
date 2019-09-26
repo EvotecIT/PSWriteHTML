2626349c4a5d8b167d7069e4605210b8eae483e8
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-09-26 17:03:22 +0200

    Add files via upload

--- Examples/Example26-Diagrams/Example-NodesEdgesOptions.ps1
+++ Examples/Example26-Diagrams/Example-NodesEdgesOptions.ps1
@@ -6,20 +6,21 @@ New-HTML -TitleText 'My Ubiquiti Network' -UseCssLinks:$true -UseJavaScriptLinks
             New-HTMLDiagram {
                 New-DiagramOptionsInteraction -Hover $true
                 New-DiagramOptionsPhysics
-                New-DiagramOptionsLayout -RandomSeed 500  
-                New-DiagramOptionsEdges -ArrowsToEnabled $true -Color Red            
-                New-DiagramNode -Label 'USG Pro' -To 'Unifi Switch'
-                New-DiagramNode -Label 'Unifi Switch' -To 'Unifi AP', 'EvoWin'
-                New-DiagramNode -Label 'Unifi AP' -To 'EvoMac', 'EvoWin' -Shape hexagon
-                New-DiagramNode -Label 'EvoMac' -Shape ellipse
-                New-DiagramNode -Label 'EvoWin' -To 'Exch1','Exch2','AD1','AD2','AD3','DC1','DC2' -Shape database
+                #New-DiagramOptionsLayout -RandomSeed 500
+                New-DiagramOptionsEdges -ArrowsToEnabled $true -Color BlueViolet -ArrowsToType arrow -ArrowsFromEnabled $false
+                New-DiagramOptionsNodes -BorderWidth 1 -FontColor Brown -Size 20 -FontMulti true
+                New-DiagramNode -Label 'USG Pro' -To 'Unifi Switch' -IconSolid cloud
+                New-DiagramNode -Label 'Unifi Switch' -To 'Unifi AP', "EvoWin" -IconSolid network-wired
+                New-DiagramNode -Label 'Unifi AP' -To 'EvoMac', "EvoWin" -IconSolid wifi
+                New-DiagramNode -Label 'EvoMac' -IconSolid laptop
+                New-DiagramNode -Id 'EvoWin' -Label "EvoWin`r`n192.168.20.15"  -To 'Exch1','Exch2','AD1','AD2','AD3','DC1','DC2' -IconSolid desktop
                 New-DiagramNode -Label 'Exch1' -Shape diamond
-                New-DiagramNode -Label 'Exch2' -Shape box
-                New-DiagramNode -Label 'AD1'
-                New-DiagramNode -Label 'AD2' -IconBrands apple
-                New-DiagramNode -Label 'AD3' -IconRegular address-card
-                New-DiagramNode -Label 'DC1' -IconSolid address-book
-                New-DiagramNode -Label 'DC2' -IconSolid address-card
+                New-DiagramNode -Label 'Exch2' -IconBrands stack-exchange
+                New-DiagramNode -Label 'AD1' -Border Black -Color Red -Image 'https://cdn.imgbin.com/18/1/14/imgbin-active-directory-microsoft-powershell-lightweight-directory-access-protocol-colossus-YaNhkKVhzC7F48Yd8YiNy2YKw.jpg'
+                New-DiagramNode -Label 'AD2' -IconSolid ad
+                New-DiagramNode -Label 'AD3' -IconSolid ad
+                New-DiagramNode -Label 'DC1' -IconSolid ad
+                New-DiagramNode -Label 'DC2' -IconSolid ad
            }
         }
     }
