1305f81d3f9e6778980a72e0f2e6b7b9a7f1ccfc
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-10-05 23:09:48 +0200

    Update

--- Examples/Example22-Tabs/Example-Tabs3.ps1
+++ Examples/Example22-Tabs/Example-Tabs3.ps1
@@ -7,20 +7,37 @@ New-HTML -TitleText 'My title' -UseCssLinks:$true -UseJavaScriptLinks:$true -Fil
     #New-HTMLTabOptions -SlimTabs -Transition -LinearGradient -SelectorColor Gold -SelectorColorTarget AliceBlue
     New-HTMLTab -Name 'First Level Tab - Test 1' -IconBrands acquisitions-incorporated {
         New-HTMLTab -Name '2nd Level Tab - Test 1/1' -IconRegular address-card {
-            New-HTMLTable -ArrayOfObjects $Test -HideFooter
+            New-HTMLTable -ArrayOfObjects $Test1 -HideFooter
         }
         New-HTMLTab -Name '2nd Level Tab - Test 1/2' -IconRegular address-card {
+            New-HTMLTable -ArrayOfObjects $Test1 -HideFooter
+        }
+        New-HTMLTab -Name '2nd Level Tab - Test 1/3' -IconRegular address-card {
+            New-HTMLTable -ArrayOfObjects $Test1 -HideFooter
+        }
+        New-HTMLTab -Name '2nd Level Tab - Test 1/4' -IconRegular address-card {
             New-HTMLTable -ArrayOfObjects $Test1 -HideFooter
         }
     }
 
     New-HTMLTab -Name 'First Level Tab - Test 4' -IconSolid bezier-curve {
         New-HTMLTab -Name '2nd Level Tab - Test 4/1' -IconBrands app-store {
-            New-HTMLTable -ArrayOfObjects $Test -HideFooter {
+            New-HTMLTable -ArrayOfObjects $Test1 -HideFooter {
                 New-HTMLTableHeader -Names 'ID', 'HandleCount' -ResponsiveOperations none
             }
         }
         New-HTMLTab -Name '2nd Level Tab - Test 4/2' {
+            New-HTMLTable -ArrayOfObjects $Test1 -HideFooter {
+                New-HTMLTableHeader -Names 'ID', 'HandleCount' -ResponsiveOperations none
+            }
+        }
+    }
+    New-HTMLTab -Name 'First Level Tab - Test 4' -IconSolid bezier-curve {
+        New-HTMLSection -Invisible {
+            New-HTMLTable -ArrayOfObjects $Test1 -HideFooter {
+                New-HTMLTableHeader -Names 'ID', 'HandleCount' -ResponsiveOperations none
+            }
+
             New-HTMLTable -ArrayOfObjects $Test1 -HideFooter {
                 New-HTMLTableHeader -Names 'ID', 'HandleCount' -ResponsiveOperations none
             }
