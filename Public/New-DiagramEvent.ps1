dedd62e2adc9c202ffb318ff8c19bb22de3adc1e
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-10-12 16:49:34 +0200

    Update

--- Public/New-DiagramEvent.ps1
+++ Public/New-DiagramEvent.ps1
@@ -1,63 +1,6 @@
-﻿function New-DiagramInternalEvent {
+﻿function New-DiagramEvent {
     param(
-        #[switch] $OnClick,
-        [string] $ID,
         #[switch] $FadeSearch,
-        [int] $ColumnID = -1
-    )
-
-
-    if ($FadeSearch) {
-        $Event = @"
-        var table = `$('#$ID').DataTable();
-        //table.search(params.nodes).draw();
-        table.rows(':visible').every(function (rowIdx, tableLoop, rowLoop) {
-            var present = true;
-            if (params.nodes) {
-                present = table.row(rowIdx).data().some(function (v) {
-                        return v.match(new RegExp(params.nodes, 'i')) != null;
-                    });
-            }
-            `$(table.row(rowIdx).node()).toggleClass('notMatched', !present);
-        });
-
-"@
-
-    } else {
-        if ($ColumnID -ne -1) {
-            $Event = @"
-        var table = `$('#$ID').DataTable();
-        if (findValue != '') {
-            table.columns(1).search("^" + findValue + "$", true, false, true).draw();
-        } else {
-            table.columns(1).search('').draw();
-        }
-        if (table.page.info().recordsDisplay == 0) {
-            table.columns(1).search('').draw();
-        }
-"@
-        } else {
-            $Event = @"
-        var table = `$('#$ID').DataTable();
-        if (findValue != '') {
-            table.search("^" + findValue + "$", true, false, true).draw();
-        } else {
-            table.search('').draw();
-        }
-        if (table.page.info().recordsDisplay == 0) {
-            table.search('').draw();
-        }
-"@
-        }
-    }
-    $Event
-
-}
-
-
-function New-DiagramEvent {
-    param(
-        [switch] $FadeSearch,
         [string] $ID,
         [int] $ColumnID
     )
@@ -67,11 +10,9 @@ function New-DiagramEvent {
         Settings = @{
             # OnClick = $OnClick.IsPresent
             ID         = $ID
-            FadeSearch = $FadeSearch.IsPresent
+           # FadeSearch = $FadeSearch.IsPresent
             ColumnID   = $ColumnID
         }
     }
     $Object
 }
-
-#New-DiagramEvent -ID 'test'
