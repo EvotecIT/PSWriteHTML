7d33cade24198a0a7e8e4ea71bd774684b88463d
Przemysław Klys <JZ5Q@eurofins.com>
2019-10-16 15:24:53 +0200

    Update

--- Examples/Example13-Table/Example13.ps1
+++ Examples/Example13-Table/Example13.ps1
@@ -3,7 +3,42 @@ Import-Module .\PSWriteHTML.psd1 -Force
 $Processes = Get-Process | Select-Object -First 2 -Property Name, ID, HandleCount, WorkingSet
 $Title = 'My title'
 
+$Object = @(
+    [PSCustomObject] @{
+        Test = 5
+        Ello = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test = 5
+        Ello = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test = 5
+        Ello = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test = 5
+        Ello = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test = 5
+        Ello = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test = 3
+        Ello = 'Lotto'
+        Hello = 'Totto'
+    }
+)
+
 New-HTML -TitleText $Title -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\Example13.html {
+    New-HTMLTable -ArrayOfObjects $Processes
+    <#
     New-HTMLContent -HeaderText '1 section' {
         New-HTMLTable -ArrayOfObjects $Processes -HideFooter {
             New-HTMLTableHeader -Names 'ID', 'HandleCount' -ResponsiveOperations none
@@ -24,4 +59,5 @@ New-HTML -TitleText $Title -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePat
             New-HTMLTableHeader -Names 'ID', 'HandleCount', 'WorkingSet' -ResponsiveOperations none
         }
     }
+    #>
 } -ShowHTML
