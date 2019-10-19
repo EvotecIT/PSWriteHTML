11e994a1c3a5885f5d9776c95a0d2834dc654229
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-10-19 19:14:29 +0200

    IE tests

---
+++ Examples/Example-IE/Example.ps1
@@ -0,0 +1,8 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Processes = Get-Process | Select-Object -First 3
+
+New-HTML -TitleText 'My Title' -UseCssLinks:$true -UseJavaScriptLinks:$true -FilePath $PSScriptRoot\TestIE.html -ShowHTML {
+    New-HTMLTable -DataTable $Processes -PagingOptions @(2,4)
+
+}
