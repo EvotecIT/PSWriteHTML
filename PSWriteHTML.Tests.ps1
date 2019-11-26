9b7b216fa932ef439cd1f90d8d770daae6015046
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-11-26 11:18:27 +0100

    Created some basic tests

---
+++ PSWriteHTML.Tests.ps1
@@ -0,0 +1,22 @@
+﻿$PSVersionTable.PSVersion
+
+$ModuleName = (Get-ChildItem $PSScriptRoot\*.psd1).BaseName
+$RequiredModules = @(
+    'Pester'
+    'PSSharedGoods'
+    'PSParseHTML'
+)
+foreach ($_ in $RequiredModules) {
+    if ($null -eq (Get-Module -ListAvailable $_)) {
+        Write-Warning "$ModuleName - Downloading $_ from PSGallery"
+        Install-Module -Name $_ -Repository PSGallery -Force -SkipPublisherCheck
+    }
+    Import-Module $_ -Force
+}
+Import-Module $PSScriptRoot\PSWriteHTML.psd1 -Force
+
+$result = Invoke-Pester -Script $PSScriptRoot\Tests -Verbose -EnableExit
+
+if ($result.FailedCount -gt 0) {
+    throw "$($result.FailedCount) tests failed."
+}
---
+++ Tests/New-HTML.Tests.ps1
@@ -0,0 +1,35 @@
+﻿Describe 'New-HTML / Save-HTML' {
+    It 'Given New-HTML it should create HTML file' {
+        $FilePath = "$PSScriptRoot\TemporaryTest.html"
+        New-HTML {
+
+        } -FilePath $FilePath
+        $PathExists = Test-Path -LiteralPath $FilePath
+        $PathExists | Should -Be $True
+        if ($PathExists) {
+            Remove-Item -LiteralPath $FilePath
+        }
+    }
+    It 'Given New-HTML with wrong path it should create HTML file in temporary location' {
+        $FilePath = "C:\TemporaryTest.html"
+        New-HTML {
+
+        } -FilePath $FilePath -WarningVariable Warnings -WarningAction SilentlyContinue
+        $PathExists = Test-Path -LiteralPath $FilePath
+        $PathExists | Should -Be $false
+        if ($PathExists) {
+            Remove-Item -LiteralPath $FilePath
+        }
+        #Regex pattern to extract Path from Warning
+        $firstString = "Saving HTML to file "
+        $secondString = "$"
+        $pattern = "$firstString(.*?)$secondString"
+        $result = [regex]::Match($Warnings[1], $pattern).Groups[1].Value
+        Write-host $result
+        $PathExists = ($result -and (Test-Path -LiteralPath $result))
+        $PathExists | Should -Be $True
+        if ($PathExists) {
+            Remove-Item -LiteralPath $result
+        }
+    }
+}
