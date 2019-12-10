5fd70b2232b73996a942dbb0376c9f1e42e49914
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2019-12-10 21:09:36 +0100

    Update Status example

---
+++ Examples/Example-StatusPage/Example-StatusPage06.ps1
@@ -0,0 +1,52 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Services = @{
+    'Active Directory' = @{
+        'Texas'  = @{
+            ServiceStatus     = 'Operational'
+            Icon       = 'Good'
+            Percentage = '100%'
+        }
+        'Boston' = @{
+            ServiceStatus     = 'Operational'
+            Icon       = 'Good'
+            Percentage = '100%'
+        }
+    }
+    'Hyper-V'          = @{
+        'California' = @{
+            ServiceStatus     = 'Operational'
+            Icon       = 'Bad'
+            Percentage = '30%'
+        }
+        'Warsaw'     = @{
+            ServiceStatus     = 'Operational'
+            Icon       = 'Dead'
+            Percentage = '70%'
+        }
+    }
+}
+
+
+New-HTML -TitleText 'Services Status' -UseCssLinks -UseJavaScriptLinks {
+    New-HTMLSection -Invisible {
+        New-HTMLContainer -Width '900px' -Margin 'auto' {
+
+            foreach ($_ in $Services.Keys) {
+
+                New-HTMLHeading -Heading h1 -HeadingText "Current Status $_"
+
+                New-HTMLPanel -Invisible {
+                    New-HTMLStatus {
+
+                        foreach ($SubService in $Services["$_"].Keys) {
+                            $Splat = $Services["$_"][$SubService]
+                            New-HTMLStatusItem -ServiceName $SubService @Splat
+                        }
+
+                    }
+                }
+            }
+        }
+    }
+} -FilePath $PSScriptRoot\StatusPage06.html -ShowHTML
