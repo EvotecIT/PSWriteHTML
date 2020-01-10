013d8e41c2a22b3cb7a1b2d5fcb0d9b37da6abc5
Przemysław Kłys <przemyslaw.klys@evotec.pl>
2020-01-10 16:13:51 +0100

    Small example update

--- Examples/Example35-FavIcon/Example35_01.ps1
+++ Examples/Example35-FavIcon/Example35_01.ps1
@@ -1,7 +1,7 @@
-Import-Module -Name PSWriteHTML
+Import-Module -Name "$PSScriptRoot\..\..\PSWriteHTML.psd1" -Force
 
 New-HTML -TitleText "Example35-FavIcon-FromFile" -FavIcon "$PSScriptRoot\favicon.png" -FilePath "$PSScriptRoot\Example35_01.html" {
     New-HTMLSection -HeaderText "Computer Process" -HeaderTextAlignment center -Content {
         New-HTMLTable -DataTable $(Get-Process | Select-Object -First 10)
     }
-}
+} -ShowHTML
