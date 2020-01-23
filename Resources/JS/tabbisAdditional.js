f20e04da9c763145ae3f9f74aa6579685ea8c5c7
opustecnica <jcrisafulli@gmail.com>
2020-01-23 12:13:38 -0500

    Update tabbisAdditional.js

--- Resources/JS/tabbisAdditional.js
+++ Resources/JS/tabbisAdditional.js
@@ -4,10 +4,10 @@ var tabs = tabbis.init({
     tabActive: "active",
     paneActive: "active",
     callback: function (tab, pane) {
-        console.log("TAB id:" + tab.id);
+        // console.log("TAB id:" + tab.id);
         // console.log(pane.id);
         tableid = document.getElementById(tab.id + "-Content").querySelector('table[id^="DT-"]').id;
-        console.log(tableid);
+        // console.log(tableid);
         // this makes sure to refresh tables on tab change to make sure they have buttons and everything
         // it's a bit heavy as it touches all tables, may require some improvements in future to consider
         // which tab has which table
