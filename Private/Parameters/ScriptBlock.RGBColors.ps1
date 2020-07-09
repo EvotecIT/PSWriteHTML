35394526540c3ef1eef32c4f155f98c84eb547a1
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-07-09 22:45:12 +0200

    Better color searching

--- Private/Parameters/ScriptBlock.RGBColors.ps1
+++ Private/Parameters/ScriptBlock.RGBColors.ps1
@@ -1,4 +1,4 @@
 ﻿$Script:ScriptBlockColors = {
     param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
-    $Script:RGBColors.Keys | Where-Object { $_ -like "$wordToComplete*" }
+    $Script:RGBColors.Keys | Sort-Object | Where-Object { $_ -like "*$wordToComplete*" }
 }
