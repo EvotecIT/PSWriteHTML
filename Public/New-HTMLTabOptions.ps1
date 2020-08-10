53a335e3d8f8fa945c4b1d0e9800c61c4fcc1f1f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-10 12:35:48 +0200

    #128

--- Public/New-HTMLTabOptions.ps1
+++ Public/New-HTMLTabOptions.ps1
@@ -7,7 +7,7 @@
         [string] $SelectorColorTarget,
         [switch] $Transition,
         [switch] $LinearGradient,
-        [ValidateSet('0px','10px','15px','25px')][string] $BorderRadius = '0px',
+        [ValidateSet('0px', '10px', '15px', '25px')][string] $BorderRadius = '0px',
         [string] $BorderBackgroundColor
 
     )
@@ -15,29 +15,76 @@
         Write-Warning 'New-HTMLTabOptions - Creation of HTML aborted. Most likely New-HTML is missing.'
         Exit
     }
+    # We need to set defaults if it's not set
+    if (-not $SelectorColor) {
+        $SelectorColor = "DodgerBlue"
+    }
+    if (-not $SelectorColorTarget) {
+        $SelectorColorTarget = "MediumSlateBlue"
+    }
+
     #$Script:HTMLSchema.TabOptions = @{ }
     $Script:HTMLSchema.TabOptions.SlimTabs = $SlimTabs.IsPresent
     if ($SelectorColor) {
-        # $Script:HTMLSchema.TabOptions.SelectorColor = ConvertFrom-Color -Color $SelectorColor
-        $Script:Configuration.Features.Tabbis.CustomActionsReplace.ColorSelector = ConvertFrom-Color -Color $SelectorColor
-        $Script:Configuration.Features.TabbisGradient.CustomActionsReplace.ColorSelector = ConvertFrom-Color -Color $SelectorColor
-        # $Script:Configuration.Features.TabsTransition.CustomActionsReplace.ColorSelector = ConvertFrom-Color -Color $SelectorColor
-    }
-    if ($SelectorColorTarget) {
-        $Script:Configuration.Features.Tabbis.CustomActionsReplace.ColorTarget = ConvertFrom-Color -Color $SelectorColorTarget
-        $Script:Configuration.Features.TabbisGradient.CustomActionsReplace.ColorTarget = ConvertFrom-Color -Color $SelectorColorTarget
-    }
-    $Script:HTMLSchema.Features.TabbisGradient = $LinearGradient.IsPresent
-    $Script:HTMLSchema.Features.TabbisTransition = $Transition.IsPresent
 
+        $Tabbis = @"
+[data-tabs] .active {
+    background: ColorSelector;
+    color: #fff;
+    border-radius: 4px;
+}
+"@
+        $Tabbis = $Tabbis.Replace('ColorSelector', (ConvertFrom-Color -Color $SelectorColor))
+        $Script:HTMLSchema.CustomHeaderCSS.Add($Tabbis)
+    }
+    # This adds styling in new New-HTMLTabHead for all tabs
     $Script:BorderStyle = @{
-        'border-radius' = "$BorderRadius";
+        'border-radius'    = "$BorderRadius";
         'background-color' = ""
     }
 
-    if($BorderBackgroundColor){
+    if ($BorderBackgroundColor) {
         $Script:BorderStyle.'background-color' = ConvertFrom-Color -Color $BorderBackgroundColor
     }
+
+    # This adds Gradient
+    if ($LinearGradient.IsPresent) {
+        $TabbisGradient = @"
+[data-tabs] .active {
+    background: -moz-linear-gradient(45deg, ColorSelector 0%, ColorTarget 100%);
+    background: -webkit-linear-gradient(45deg, ColorSelector 0%, ColorTarget 100%);
+    background: linear-gradient(45deg, ColorSelector 0%, ColorTarget 100%);
+    filter: progid:DXImageTransform.Microsoft.gradient(startColorstr='ColorSelector', endColorstr='ColorTarget', GradientType=1);
+}
+"@
+        $TabbisGradient = $TabbisGradient.Replace('ColorSelector', (ConvertFrom-Color -Color $SelectorColor))
+        $TabbisGradient = $TabbisGradient.Replace('ColorTarget', (ConvertFrom-Color -Color $SelectorColorTarget))
+        $Script:HTMLSchema.CustomHeaderCSS.Add($TabbisGradient)
+    }
+
+    # This adds Transition
+    if ($Transition.IsPresent) {
+        $TabbisTransition = @"
+[data-tabs] .active {
+    transition-duration: 0.6s;
+    transition-timing-function: cubic-bezier(0.68, -0.55, 0.265, 1.55);
+}
+"@
+        $Script:HTMLSchema.CustomHeaderCSS.Add($TabbisTransition)
+    }
+
+    <#
+    [string] $ClassName = "flexPanel$(Get-RandomStringName -Size 8 -LettersOnly)"
+    $Attributes = @{
+        'flex-basis' = if ($Width) { $Width } else { '100%' }
+        'margin'     = if ($Margin) { $Margin }
+    }
+    $Css = ConvertTo-CSS -ClassName $ClassName -Attributes $Attributes
+
+    $Script:HTMLSchema.CustomHeaderCSS.Add($Css)
+    #>
+
+
 }
 
 Register-ArgumentCompleter -CommandName New-HTMLTabOptions -ParameterName SelectorColor -ScriptBlock $Script:ScriptBlockColors
