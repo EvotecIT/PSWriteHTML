76f3b0a3e3d93f065798e268d125a5c02f50de1a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-10 14:22:27 +0200

    Removed tabs dead code

--- Private/New-HTMLTabHead.ps1
+++ Private/New-HTMLTabHead.ps1
@@ -3,35 +3,6 @@ function New-HTMLTabHead {
     Param (
         [Array] $TabsCollection
     )
-
-    if ($Script:HTMLSchema.TabOptions.SlimTabs) {
-        $Style = 'display: inline-block;' # makes tabs wrapperr slim/small
-    } else {
-        $Style = '' # makes it full-width
-    }
-    <#
-    New-HTMLTag -Tag 'div' -Attributes @{ class = 'tabsWrapper' } {
-        New-HTMLTag -Tag 'div' -Attributes @{ class = 'tabs' ; style = $Style } {
-            New-HTMLTag -Tag 'div' -Attributes @{ class = 'selector' }
-            foreach ($Tab in $Script:HTMLSchema.TabsHeaders) {
-                $AttributesA = @{
-                    'href'    = 'javascript:void(0)'
-                    'data-id' = "$($Tab.Id)"
-                }
-                if ($Tab.Active) {
-                    $AttributesA.class = 'active'
-                } else {
-                    $AttributesA.class = ''
-                }
-                New-HTMLTag -Tag 'a' -Attributes $AttributesA {
-                    New-HTMLTag -Tag 'div' -Attributes @{ class = $($Tab.Icon); style = $($Tab.StyleIcon) }
-                    New-HTMLTag -Tag 'span' -Attributes @{ style = $($Tab.StyleText ) } -Value { $Tab.Name }
-                }
-            }
-        }
-    }
-    #>
-
     if ($TabsCollection.Count -gt 0) {
         $Tabs = $TabsCollection
     } else {
