7cfc241bbb3af3e4a75071f0461ff465ec43ce7f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-10 18:40:24 +0200

    Updates to CSS way handling

--- Private/Parameters.Configuration.ps1
+++ Private/Parameters.Configuration.ps1
@@ -341,7 +341,44 @@ $Script:Configuration = [ordered] @{
         Tabbis               = @{
             Comment      = 'Elastic Tabbis'
             HeaderAlways = @{
-                Css = "$PSScriptRoot\..\Resources\CSS\tabbis.css"
+                #Css       = "$PSScriptRoot\..\Resources\CSS\tabbis.css"
+                CssInline = [ordered] @{
+                    ".tabsWrapper"         = [ordered]@{
+                        'text-align'     = 'center'
+                        'margin'         = "10px auto"
+                        'font-family'    = "'Roboto', sans-serif !important"
+                        'text-transform' = 'uppercase'
+                        'font-size'      = '15px'
+                    }
+                    '[data-tabs]'          = [ordered]@{
+                        'display'         = 'flex'
+                        'margin-top'      = '10px'
+                        # /*font-size =  15px;*/
+                        'padding'         = '0px'
+                        'box-shadow'      = '0px 5px 20px rgba(0, 0, 0, 0.1)'
+                        'border-radius'   = '4px'
+                        'justify-content' = 'center'
+                    }
+
+                    '[data-tabs]>*'        = [ordered]@{
+                        'cursor'  = 'pointer'
+                        'padding' = '10px 20px'
+                    }
+
+                    '[data-tabs] .active'  = [ordered]@{
+                        'background'    = '#1e90ff' # DodgerBlue
+                        'color'         = '#fff'
+                        'border-radius' = '4px'
+                    }
+
+                    '[data-panes]>*'       = [ordered]@{
+                        'display' = 'none'
+                    }
+
+                    '[data-panes]>.active' = [ordered]@{
+                        'display' = 'block'
+                    }
+                }
             }
             FooterAlways = @{
                 JS = @(
---
+++ Public/ConvertTo-CascadingStyleSheets.ps1
@@ -0,0 +1,27 @@
+﻿function ConvertTo-CascadingStyleSheets {
+    [cmdletBinding()]
+    param(
+        [Parameter(ValueFromPipeline, ValueFromPipelineByPropertyName)][System.Collections.IDictionary] $Css
+    )
+    Process {
+        if ($Css) {
+            Remove-EmptyValue -Hashtable $Css -Recursive
+        }
+        if ($Css.Count -eq 0) {
+            # Means empty value after we removed all empty values
+            return
+        }
+        foreach ($Key in $Css.Keys) {
+            "$Key {"
+            foreach ($_ in $Css[$Key].Keys) {
+                if ($null -ne $Css[$Key][$_]) {
+                    # we remove empty chars because sometimes there cab be multiple lines similar to each other
+                    $Property = $_.Replace(' ', '')
+                    "    $Property`: $($Css[$Key][$_]);"
+                }
+            }
+            "}"
+            ''
+        }
+    }
+}
