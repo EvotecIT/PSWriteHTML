736e4a257872ddf0b9084608893bd74f711d4090
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-16 10:49:26 +0200

    Renamed Renamed New-HTMLSectionOptions

--- Public/New-HTMLSectionOption.ps1
+++ Public/New-HTMLSectionOption.ps1
@@ -1,21 +1,30 @@
-﻿function New-HTMLSectionOptions {
+﻿function New-HTMLSectionOption {
+    [alias("New-HTMLSectionOption", 'SectionOption')]
     [cmdletBinding()]
     param(
         [Parameter(ParameterSetName = 'Manual')][ValidateSet('0px', '5px', '10px', '15px', '20px', '25px')][string] $BorderRadius,
+
         [Parameter(ParameterSetName = 'Manual')][switch] $RemoveShadow,
+
         [Parameter(ParameterSetName = 'Manual')][alias('TextColor')][string]$HeaderTextColor,
         [Parameter(ParameterSetName = 'Manual')][alias('TextAlignment')][string][ValidateSet('center', 'left', 'right', 'justify')] $HeaderTextAlignment,
         [Parameter(ParameterSetName = 'Manual')][alias('TextBackGroundColor')][string]$HeaderBackGroundColor,
-
-        [Parameter(ParameterSetName = 'Manual')][alias('BackgroundShade')][string]$BackgroundColor,
+        [Parameter(ParameterSetName = 'Manual')][switch] $HeaderRemovePadding,
 
 
         [Parameter(ParameterSetName = 'Manual')][string][ValidateSet('wrap', 'nowrap', 'wrap-reverse')] $Wrap,
         [Parameter(ParameterSetName = 'Manual')][string][ValidateSet('row', 'row-reverse', 'column', 'column-reverse')] $Direction,
-        [Parameter(ParameterSetName = 'Manual')][string][ValidateSet('flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'stretch')] $AlignContent,
+        [Parameter(ParameterSetName = 'Manual')][string][ValidateSet('flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'stretch')] $Align,
         [Parameter(ParameterSetName = 'Manual')][string][ValidateSet('stretch', 'flex-start', 'flex-end', 'center', 'baseline')] $AlignItems,
-        [Parameter(ParameterSetName = 'Manual')][string][ValidateSet('flex-start', 'flex-end', 'center')] $JustifyContent,
+        [Parameter(ParameterSetName = 'Manual')][string][ValidateSet('flex-start', 'flex-end', 'center')] $Justify,
         [Parameter(ParameterSetName = 'Manual')][string][ValidateSet('-180deg', '-90deg', '90deg', '180deg')] $Rotate,
+
+        [Parameter(ParameterSetName = 'Manual')][alias('BackgroundShade')][string]$BackgroundColorContent,
+        [string][ValidateSet('wrap', 'nowrap', 'wrap-reverse')] $WrapContent,
+        [string][ValidateSet('row', 'row-reverse', 'column', 'column-reverse')] $DirectionContent,
+        [string][ValidateSet('flex-start', 'flex-end', 'center', 'space-between', 'space-around', 'stretch')] $AlignContent,
+        [string][ValidateSet('stretch', 'flex-start', 'flex-end', 'center', 'baseline')] $AlignItemsContent,
+        [string][ValidateSet('flex-start', 'flex-end', 'center')] $JustifyContent,
 
         [Parameter(ParameterSetName = 'Manual')][ValidateSet('vertical-rl', 'vertical-lr', 'horizontal-tb')][string] $WritingMode,
         [Parameter(ParameterSetName = 'Manual')][ValidateSet('mixed', 'upright')][string] $TextOrientation,
@@ -26,12 +35,21 @@
     # this is read from $Script:Configuration (or more precise $Script:CurrentConfiguration which is a copy on New-HTML start)
     $CssConfiguration = Get-ConfigurationCss -Feature 'Default' -Type 'HeaderAlways'
     $StyleSheetsConfiguration = [ordered] @{
-        Section     = "defaultSection"
-        SectionText = "defaultSectionText"
-        SectionHead = "defaultSectionHead"
+        Section        = "defaultSection"
+        SectionText    = "defaultSectionText"
+        SectionHead    = "defaultSectionHead"
+        SectionContent = 'defaultSectionContent'
     }
 
     if ($RequestConfiguration) {
+        # This copies current CSS that we need (in this case defaultSection,defaultSectionText, defaultSectionHead)
+        # Inserts it into $Script:CurrentConfiguration
+        # Which then will be inserted into HTML
+        # finally it returns new names for sections above
+        $RequestedConfiguration = New-RequestCssConfiguration -Pair $StyleSheetsConfiguration -CSSConfiguration $CssConfiguration -Feature 'Inject' -Type 'HeaderAlways'
+        $StyleSheetsConfiguration = $RequestedConfiguration.StyleSheetConfiguration
+        $CssConfiguration = $RequestedConfiguration.CSSConfiguration
+        <#
         # We define how our CSS classes will be called - this can be per Section so we need to have it random
         $Name = $(Get-RandomStringName -Size 7)
         $ExpectedStyleSheetsConfiguration = [ordered] @{
@@ -64,10 +82,11 @@
             SectionText = $ExpectedStyleSheetsConfiguration.SectionText
             SectionHead = $ExpectedStyleSheetsConfiguration.SectionHead
         }
+        #>
     }
 
     # Manage SECTION
-    if ($Wrap -or $Direction -or $AlignContent -or $AlignItems) {
+    if ($Wrap -or $Direction -or $Align -or $AlignItems) {
         # This makes sure we can control placement of HEAD within SECTION
         Add-ConfigurationCSS -CSS $CssConfiguration -Name $StyleSheetsConfiguration.Section -Inject @{ display = 'flex' }
     }
@@ -75,18 +94,13 @@
     # keep in mind that also empty elements will be removed from this
     # so if background color is empty it will be removed from "overwritting"
     $SectionStyle = @{
-        'flex-wrap'        = $Wrap
-        'flex-direction'   = $Direction
-        'align-content'    = $AlignContent
-        'align-items'      = $AlignItems
-        'justify-content'  = $JustifyContent
-        "background-color" = ConvertFrom-Color -Color $BackgroundColor
+        "background-color" = ConvertFrom-Color -Color $BackgroundColorContent
         'border-radius'    = $BorderRadius
     }
     Add-ConfigurationCSS -CSS $CssConfiguration -Name $StyleSheetsConfiguration.Section -Inject $SectionStyle
 
     # Removing elements needs to be defined separatly
-    if ($BackgroundColor -eq 'none') {
+    if ($BackgroundColorContent -eq 'none') {
         Remove-ConfigurationCSS -CSS $CssConfiguration -Name $StyleSheetsConfiguration.Section -Property 'background-color'
     }
     if ($RemoveShadow) {
@@ -94,10 +108,23 @@
     }
 
     # Manage SECTION HEAD behaviour
+    <#
     $SectionHeadStyle = @{
         "text-align"       = $HeaderTextAlignment
         "background-color" = ConvertFrom-Color -Color $HeaderBackGroundColor
         'align-content'    = $AlignContent
+    }
+    #>
+
+    $SectionHeadStyle = @{
+        'flex-wrap'        = $Wrap
+        'flex-direction'   = $Direction
+        'align-content'    = $Align
+        'align-items'      = $AlignItems
+        'justify-content'  = $Justify
+        "background-color" = ConvertFrom-Color -Color $HeaderBackGroundColor
+        'border-radius'    = $BorderRadius
+        "text-align"       = $HeaderTextAlignment
         "transform"        = ConvertFrom-Rotate -Rotate $Rotate
     }
 
@@ -108,6 +135,9 @@
     # so if user requests removal of background color we need to remove it
     if ($HeaderBackGroundColor -eq 'none') {
         Remove-ConfigurationCSS -CSS $CssConfiguration -Name $StyleSheetsConfiguration.SectionHead -Property 'background-color'
+    }
+    if ($HeaderRemovePadding) {
+        Remove-ConfigurationCSS -CSS $CssConfiguration -Name $StyleSheetsConfiguration.SectionHead -Property 'padding'
     }
 
     # Manage SECTION TEXT behavior
@@ -121,6 +151,22 @@
     if ($HeaderTextColor -eq 'none') {
         Remove-ConfigurationCSS -CSS $CssConfiguration -Name $StyleSheetsConfiguration.SectionText -Property 'color'
     }
+
+
+    if ($WrapContent -or $DirectionContent -or $AlignContent -or $AlignItemsContent -or $JustifyContent) {
+        # This makes sure we can control placement of HEAD within SECTION
+        Add-ConfigurationCSS -CSS $CssConfiguration -Name $StyleSheetsConfiguration.SectionContent -Inject @{ display = 'flex' }
+    }
+
+    $SectionContentStyle = @{
+        'flex-wrap'       = $WrapContent
+        'flex-direction'  = $DirectionContent
+        'align-content'   = $AlignContent
+        'align-items'     = $AlignItemsContent
+        'justify-content' = $JustifyContent
+    }
+    Add-ConfigurationCSS -CSS $CssConfiguration -Name $StyleSheetsConfiguration.SectionContent -Inject $SectionContentStyle
+
 
     if ($RequestConfiguration) {
         # We only return this when requesting configuration
