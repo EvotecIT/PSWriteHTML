8868ebca239d45fece6e5521a95e8876a7bb50be
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-16 10:49:13 +0200

    Renamed New-HTMLPanelOptions

--- Public/New-HTMLPanelOption.ps1
+++ Public/New-HTMLPanelOption.ps1
@@ -1,4 +1,5 @@
-﻿function New-HTMLPanelOptions {
+﻿function New-HTMLPanelOption {
+    [alias('New-HTMLPanelOptions', 'New-PanelOption', "PanelOption")]
     [cmdletBinding()]
     param(
         [Parameter(ParameterSetName = 'Manual')][ValidateSet('0px', '5px', '10px', '15px', '20px', '25px')][string] $BorderRadius,
