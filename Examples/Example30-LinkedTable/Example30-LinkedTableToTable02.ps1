d75649b338c1c30aa3c4dc5f6162a998f673ae9a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-17 12:32:23 +0200

    #144

---
+++ Examples/Example30-LinkedTable/Example30-LinkedTableToTable02.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Processes = Get-Process | Select-Object -First 3 -Property Name, ProcessName, Id, FileVersion, WorkingSet
+
+New-HTML -TitleText 'My Title' -Online -FilePath $PSScriptRoot\Example30-LinkedTableToTable02.html -ShowHTML {
+    New-HTMLSection -Invisible {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $Processes -DataTableID 'RandomID1' {
+                New-TableEvent -ID 'RandomID2' -ColumnID 0
+                New-TableEvent -ID 'RandomID3' -ColumnID 0
+            }
+        }
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $Processes -DataTableID 'RandomID2'
+        }
+    }
+    New-HTMLPanel {
+        New-HTMLTable -DataTable $Processes -DataTableID 'RandomID3'
+    }
+}
