193c7a478a9be2a00cf1f7975db4e8f0bca24366
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-08-22 19:46:10 +0200

    Update

---
+++ Examples/Example42-TableSearchPanes/Example42-TableSearchPanes.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Table = Get-Process | Select-Object -First 4
+
+New-HTML -TitleText "Testing Search Pane" -FilePath "$PSScriptRoot\Example42-TableSearchPanes.html" {
+    # Hide Buttons
+    New-HTMLSection -HeaderText "Search Pane Button" -Content {
+        New-HTMLTable -DataTable $Table -Filtering {
+
+        } -Buttons @('copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5', 'pageLength', 'searchPanes')
+    }
+    New-HTMLSection -HeaderText "Search Pane Built-in" -Content {
+        New-HTMLTable -DataTable $Table -Filtering {
+
+        } -SearchPane
+    }
+} -ShowHTML -Online
