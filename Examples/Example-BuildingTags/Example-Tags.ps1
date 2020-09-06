8a791de3fdfe867ecc89c159a27ae282b5e096cc
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-06 09:39:05 +0200

    Example tags

---
+++ Examples/Example-BuildingTags/Example-Tags.ps1
@@ -0,0 +1,6 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTMLTag -Tag 'meta' -Attributes @{ charset = "utf-8" } -NoClosing
+#New-HTMLTag -Tag 'meta' -Attributes @{ 'http-equiv' = 'X-UA-Compatible'; content = 'IE=8' } -SelfClosing
+New-HTMLTag -Tag 'meta' -Attributes @{ name = 'viewport'; content = 'width=device-width, initial-scale=1' } -NoClosing
+New-HTMLTag -Tag 'meta' -Attributes @{ 'http-equiv' = "Content-Type"; content = "text/html; charset=utf-8" } -NoClosing
