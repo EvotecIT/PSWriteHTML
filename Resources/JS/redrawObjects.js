f17b0faac7db58f2e3555b4016481d1b9c4c7ee3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-09-06 21:25:55 +0200

    redrawobjects for section/tabs

---
+++ Resources/JS/redrawObjects.js
@@ -0,0 +1,13 @@
+﻿function redrawCalendar(calendar) {
+    //console.log(calendarTracker[calendar.id].view);
+    calendarTracker[calendar.id].changeView(calendarTracker[calendar.id].view.type);
+    console.log('Redrawing view for ' + calendar.id)
+}
+function resizeTable(table) {
+    try {
+        $("#" + table.id).DataTable().columns.adjust().responsive.recalc();
+        console.log('Resized table with id ' + table.id);
+    } catch (e) {
+        console.log('Failed to resize table with ' + table.id);
+    }
+}
