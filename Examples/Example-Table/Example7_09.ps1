7a55fb37498eabe133b3ab04f127d67db92de727
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-08 12:38:34 +0200

    Some example

---
+++ Examples/Example-Table/Example7_09.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Test = Get-Process | Select-Object -First 30
+# New-HTML option
+New-HTML {
+    New-HTMLTable -DataTable $Test -Filtering -FilteringLocation bottom -DefaultSortIndex 0 -DefaultSortOrder Ascending -ScrollX -ScrollY -ScrollSizeY 300
+} -ShowHTML -Online -FilePath $PSScriptRoot\Testing.html
+
+# Out-HTMLView
+Out-HtmlView -DataTable $Test -Filtering -FilteringLocation bottom -DefaultSortIndex 0 -DefaultSortOrder Ascending -ScrollX -ScrollY -ScrollSizeY 300
