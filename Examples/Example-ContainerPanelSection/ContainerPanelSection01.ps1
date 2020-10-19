26256d42724ca316d4c79f9d8430ffead2c7fa92
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-19 20:16:18 +0200

    Update examples

---
+++ Examples/Example-ContainerPanelSection/ContainerPanelSection01.ps1
@@ -0,0 +1,109 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTML {
+    New-HTMLTab -Name 'Health State' {
+        New-HTMLPanel {
+            New-HTMLText -TextBlock {
+                "Following table shows list of all group policies and their status in AD and SYSVOL. Due to different reasons it's "
+                "possible that "
+            } -FontSize 10pt
+            New-HTMLList -Type Unordered {
+                New-HTMLListItem -Text 'Group Policies on SYSVOL, but no details in AD: ', $NotAvailableInAD.Count -FontWeight normal, bold
+                New-HTMLListItem -Text 'Group Policies in AD, but no content on SYSVOL: ', $NotAvailableOnSysvol.Count -FontWeight normal, bold
+                New-HTMLListItem -Text "Group Policies which couldn't be assed due to permissions issue: ", $NotAvailablePermissionIssue.Count -FontWeight normal, bold
+            } -FontSize 10pt
+            New-HTMLText -Text "Follow the steps below table to get Active Directory Group Policies in healthy state." -FontSize 10pt
+        }
+        New-HTMLPanel -Invisible {
+            New-HTMLText -TextBlock {
+                "Following table shows list of all group policies and their status in AD and SYSVOL. Due to different reasons it's "
+                "possible that "
+            } -FontSize 10pt
+            New-HTMLList -Type Unordered {
+                New-HTMLListItem -Text 'Group Policies on SYSVOL, but no details in AD: ', $NotAvailableInAD.Count -FontWeight normal, bold
+                New-HTMLListItem -Text 'Group Policies in AD, but no content on SYSVOL: ', $NotAvailableOnSysvol.Count -FontWeight normal, bold
+                New-HTMLListItem -Text "Group Policies which couldn't be assed due to permissions issue: ", $NotAvailablePermissionIssue.Count -FontWeight normal, bold
+            } -FontSize 10pt
+            New-HTMLText -Text "Follow the steps below table to get Active Directory Group Policies in healthy state." -FontSize 10pt
+        }
+        New-HTMLSection {
+            New-HTMLText -TextBlock {
+                "Following table shows list of all group policies and their status in AD and SYSVOL. Due to different reasons it's "
+                "possible that "
+            } -FontSize 10pt
+            New-HTMLList -Type Unordered {
+                New-HTMLListItem -Text 'Group Policies on SYSVOL, but no details in AD: ', $NotAvailableInAD.Count -FontWeight normal, bold
+                New-HTMLListItem -Text 'Group Policies in AD, but no content on SYSVOL: ', $NotAvailableOnSysvol.Count -FontWeight normal, bold
+                New-HTMLListItem -Text "Group Policies which couldn't be assed due to permissions issue: ", $NotAvailablePermissionIssue.Count -FontWeight normal, bold
+            } -FontSize 10pt
+            New-HTMLText -Text "Follow the steps below table to get Active Directory Group Policies in healthy state." -FontSize 10pt
+        }
+        New-HTMLSection -Invisible {
+            New-HTMLText -TextBlock {
+                "Following table shows list of all group policies and their status in AD and SYSVOL. Due to different reasons it's "
+                "possible that "
+            } -FontSize 10pt
+            New-HTMLList -Type Unordered {
+                New-HTMLListItem -Text 'Group Policies on SYSVOL, but no details in AD: ', $NotAvailableInAD.Count -FontWeight normal, bold
+                New-HTMLListItem -Text 'Group Policies in AD, but no content on SYSVOL: ', $NotAvailableOnSysvol.Count -FontWeight normal, bold
+                New-HTMLListItem -Text "Group Policies which couldn't be assed due to permissions issue: ", $NotAvailablePermissionIssue.Count -FontWeight normal, bold
+            } -FontSize 10pt
+            New-HTMLText -Text "Follow the steps below table to get Active Directory Group Policies in healthy state." -FontSize 10pt
+        }
+        New-HTMLSection {
+            New-HTMLPanel -Invisible {
+                New-HTMLText -TextBlock {
+                    "Following table shows list of all group policies and their status in AD and SYSVOL. Due to different reasons it's "
+                    "possible that "
+                } -FontSize 10pt
+                New-HTMLList -Type Unordered {
+                    New-HTMLListItem -Text 'Group Policies on SYSVOL, but no details in AD: ', $NotAvailableInAD.Count -FontWeight normal, bold
+                    New-HTMLListItem -Text 'Group Policies in AD, but no content on SYSVOL: ', $NotAvailableOnSysvol.Count -FontWeight normal, bold
+                    New-HTMLListItem -Text "Group Policies which couldn't be assed due to permissions issue: ", $NotAvailablePermissionIssue.Count -FontWeight normal, bold
+                } -FontSize 10pt
+                New-HTMLText -Text "Follow the steps below table to get Active Directory Group Policies in healthy state." -FontSize 10pt
+            }
+        }
+        New-HTMLSection -Invisible {
+            New-HTMLPanel -Invisible {
+                New-HTMLText -TextBlock {
+                    "Following table shows list of all group policies and their status in AD and SYSVOL. Due to different reasons it's "
+                    "possible that "
+                } -FontSize 10pt
+                New-HTMLList -Type Unordered {
+                    New-HTMLListItem -Text 'Group Policies on SYSVOL, but no details in AD: ', $NotAvailableInAD.Count -FontWeight normal, bold
+                    New-HTMLListItem -Text 'Group Policies in AD, but no content on SYSVOL: ', $NotAvailableOnSysvol.Count -FontWeight normal, bold
+                    New-HTMLListItem -Text "Group Policies which couldn't be assed due to permissions issue: ", $NotAvailablePermissionIssue.Count -FontWeight normal, bold
+                } -FontSize 10pt
+                New-HTMLText -Text "Follow the steps below table to get Active Directory Group Policies in healthy state." -FontSize 10pt
+            }
+        }
+        New-HTMLContainer {
+            New-HTMLText -TextBlock {
+                "Following table shows list of all group policies and their status in AD and SYSVOL. Due to different reasons it's "
+                "possible that "
+            } -FontSize 10pt
+            New-HTMLList -Type Unordered {
+                New-HTMLListItem -Text 'Group Policies on SYSVOL, but no details in AD: ', $NotAvailableInAD.Count -FontWeight normal, bold
+                New-HTMLListItem -Text 'Group Policies in AD, but no content on SYSVOL: ', $NotAvailableOnSysvol.Count -FontWeight normal, bold
+                New-HTMLListItem -Text "Group Policies which couldn't be assed due to permissions issue: ", $NotAvailablePermissionIssue.Count -FontWeight normal, bold
+            } -FontSize 10pt
+            New-HTMLText -Text "Follow the steps below table to get Active Directory Group Policies in healthy state." -FontSize 10pt
+        }
+
+        New-HTMLSection -Invisible {
+            New-HTMLContainer -Width '400px' -Margin 'auto' {
+                New-HTMLText -TextBlock {
+                    "Following table shows list of all group policies and their status in AD and SYSVOL. Due to different reasons it's "
+                    "possible that "
+                } -FontSize 10pt
+                New-HTMLList -Type Unordered {
+                    New-HTMLListItem -Text 'Group Policies on SYSVOL, but no details in AD: ', $NotAvailableInAD.Count -FontWeight normal, bold
+                    New-HTMLListItem -Text 'Group Policies in AD, but no content on SYSVOL: ', $NotAvailableOnSysvol.Count -FontWeight normal, bold
+                    New-HTMLListItem -Text "Group Policies which couldn't be assed due to permissions issue: ", $NotAvailablePermissionIssue.Count -FontWeight normal, bold
+                } -FontSize 10pt
+                New-HTMLText -Text "Follow the steps below table to get Active Directory Group Policies in healthy state." -FontSize 10pt
+            }
+        }
+    }
+} -Online -FilePath $PSScriptRoot\ContainerPanelSection01.html -TitleText 'Container Panel Section 01' -ShowHTML
