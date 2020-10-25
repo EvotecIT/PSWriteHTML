ae463aced32e02b45e7ca8e0aaf6c5ebfa25f8b2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-10-25 10:38:59 +0100

    Update

---
+++ Examples/Example-Tabs/Example-Tabs08.ps1
@@ -0,0 +1,46 @@
+﻿Import-Module $PSScriptRoot\..\..\PSWriteHTML.psd1 -Force
+
+New-HTML -TitleText 'My title' -Online -FilePath $PSScriptRoot\Example-Tabs08.html -Show {
+    New-HTMLTabStyle -RowElements 4
+    New-HTMLTab -Name 'Tab 1' -IconBrands acquisitions-incorporated {
+        New-HTMLTab -Name '2nd Level Tab - Test 4/1' -IconBrands app-store {
+            New-HTMLTable -DataTable 'string'
+        }
+        New-HTMLTab -Name '2nd Level Tab - Test 4/2' -IconBrands app-store {
+            New-HTMLTable -DataTable 'string'
+        }
+        New-HTMLTab -Name '2nd Level Tab - Test 4/3' -IconBrands app-store {
+            New-HTMLTable -DataTable 'string'
+        }
+        New-HTMLTab -Name '2nd Level Tab - Test 4/4' -IconBrands app-store {
+            New-HTMLTable -DataTable 'string'
+        }
+    }
+    New-HTMLTab -Name 'Tab2' {
+        New-HTMLTable -DataTable 'string'
+    }
+    New-HTMLTab -Name 'Tab3' {
+        New-HTMLTable -DataTable 'string'
+    }
+    New-HTMLTab -Name 'Tab4' {
+        New-HTMLTable -DataTable 'string'
+    }
+    New-HTMLTab -Name 'Tab5' {
+        New-HTMLTable -DataTable 'string'
+    }
+    New-HTMLTab -Name 'Tab6' {
+        New-HTMLTable -DataTable 'string'
+    }
+    New-HTMLTab -Name 'Tab7' {
+        New-HTMLTable -DataTable 'string'
+    }
+    New-HTMLTab -Name 'Tab8' {
+        New-HTMLTable -DataTable 'string'
+    }
+    New-HTMLTab -Name 'Tab9' {
+        New-HTMLTable -DataTable 'string'
+    }
+    New-HTMLTab -Name 'Tab10' {
+        New-HTMLTable -DataTable 'string'
+    }
+}
---
+++ Examples/Example-Tabs/Example-Tabs09.ps1
@@ -0,0 +1,48 @@
+﻿Import-Module $PSScriptRoot\..\..\PSWriteHTML.psd1 -Force
+
+$DataTable = Get-Process | Select-Object -First 30
+
+New-HTML -TitleText 'My title' -Online -FilePath $PSScriptRoot\Example-Tabs09.html -Show {
+    New-HTMLTabStyle -RowElements 4
+    New-HTMLTab -Name 'Tab 1' -IconBrands acquisitions-incorporated {
+        New-HTMLTab -Name '2nd Level Tab - Test 4/1' -IconBrands app-store {
+            New-HTMLTable -DataTable $DataTable
+        }
+        New-HTMLTab -Name '2nd Level Tab - Test 4/2' -IconBrands app-store {
+            New-HTMLTable -DataTable $DataTable
+        }
+        New-HTMLTab -Name '2nd Level Tab - Test 4/3' -IconBrands app-store {
+            New-HTMLTable -DataTable $DataTable
+        }
+        New-HTMLTab -Name '2nd Level Tab - Test 4/4' -IconBrands app-store {
+            New-HTMLTable -DataTable $DataTable
+        }
+    }
+    New-HTMLTab -Name 'Tab2' {
+        New-HTMLTable -DataTable $DataTable
+    }
+    New-HTMLTab -Name 'Tab3' {
+        New-HTMLTable -DataTable $DataTable
+    }
+    New-HTMLTab -Name 'Tab4' {
+        New-HTMLTable -DataTable $DataTable
+    }
+    New-HTMLTab -Name 'Tab5' {
+        New-HTMLTable -DataTable $DataTable
+    }
+    New-HTMLTab -Name 'Tab6' {
+        New-HTMLTable -DataTable $DataTable
+    }
+    New-HTMLTab -Name 'Tab7' {
+        New-HTMLTable -DataTable $DataTable
+    }
+    New-HTMLTab -Name 'Tab8' {
+        New-HTMLTable -DataTable $DataTable
+    }
+    New-HTMLTab -Name 'Tab9' {
+        New-HTMLTable -DataTable $DataTable
+    }
+    New-HTMLTab -Name 'Tab10' {
+        New-HTMLTable -DataTable $DataTable
+    }
+}
