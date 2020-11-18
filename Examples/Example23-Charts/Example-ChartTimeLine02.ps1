c9a01afb61ebb418879babda8dd7337784c237c2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-11-18 09:27:00 +0100

    Improved example

--- Examples/Example23-Charts/Example-ChartTimeLine02.ps1
+++ Examples/Example23-Charts/Example-ChartTimeLine02.ps1
@@ -66,4 +66,27 @@ New-HTML -TitleText 'Charts - TimeLine' -Online -FilePath $PSScriptRoot\Example-
             }
         }
     }
+    New-HTMLSection -Invisible {
+        New-HTMLPanel {
+            New-HTMLChart -Title 'Days' -TitleAlignment center {
+                New-ChartAxisY # this hides AXIS Y
+                New-ChartDataLabel -Enabled -DataLabelsColor Black, Red, Red, Red, Red
+                $From = (Get-Date).AddDays(-1)
+                $To = (Get-Date).AddDays(2)
+                New-ChartTimeLine -DateFrom $From -DateTo $To -Name 'Analysis' -Color Red
+                $From = (Get-Date).AddDays(0)
+                $To = (Get-Date).AddDays(5)
+                New-ChartTimeLine -DateFrom $From -DateTo $To -Name 'Design' -Color Yellow
+                $From = (Get-Date).AddDays(1)
+                $To = (Get-Date).AddDays(8)
+                New-ChartTimeLine -DateFrom $From -DateTo $To -Name 'Coding' -Color Beige
+                $From = (Get-Date).AddDays(3)
+                $To = (Get-Date).AddDays(7)
+                New-ChartTimeLine -DateFrom $From -DateTo $To -Name 'Testing' -Color Beige
+                $From = (Get-Date).AddDays(8)
+                $To = (Get-Date).AddDays(10)
+                New-ChartTimeLine -DateFrom $From -DateTo $To -Name 'Deployment' -Color Beige
+            }
+        }
+    }
 } -ShowHTML
