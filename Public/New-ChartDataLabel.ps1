1042753cd9fcedf2598ad875f59a70f32ce5fff8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-11-18 09:26:38 +0100

    Added New-ChartDataLabel

---
+++ Public/New-ChartDataLabel.ps1
@@ -0,0 +1,30 @@
+﻿function New-ChartDataLabel {
+    [cmdletBinding()]
+    param(
+        [switch] $Enabled,
+        [int] $DataLabelsOffsetX,
+        [string] $DataLabelsFontSize,
+        [string[]] $DataLabelsColor
+    )
+
+    $Object = [PSCustomObject] @{
+        ObjectType = 'DataLabel'
+        DataLabel  = [ordered] @{
+            enabled = $Enabled.IsPresent
+        }
+    }
+    if ($DataLabelsOffsetX) {
+        $Object['DataLabel']['offsetX'] = $DataLabelsOffsetX
+    }
+    $Object.DataLabel.style = [ordered]@{}
+    if ($DataLabelsFontSize) {
+        $Object.DataLabel.style['fontSize'] = $DataLabelsFontSize
+    }
+    if ($DataLabelsColor.Count -gt 0) {
+        $Object.DataLabel.style['colors'] = @(ConvertFrom-Color -Color $DataLabelsColor)
+    }
+    Remove-EmptyValue -Hashtable $Object.DataLabel -Recursive
+    $Object
+}
+
+Register-ArgumentCompleter -CommandName New-ChartDataLabel -ParameterName DataLabelsColor -ScriptBlock $Script:ScriptBlockColors
