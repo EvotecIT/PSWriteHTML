4bc595543df76934f8888cfbf56219f80b3ac9c9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-11-29 23:41:23 +0100

    Update example

--- Examples/Example11-Headings/Example11.ps1
+++ Examples/Example11-Headings/Example11.ps1
@@ -1,18 +1,15 @@
 Import-Module .\PSWriteHTML.psd1 -Force
 $Processes = Get-Process | Select-Object -First 5
 
-New-HTML -TitleText "Title" -Online -FilePath $PSScriptRoot\Example11.html -ShowHTML {
-
+New-HTML -TitleText "Title" -Online:$true -FilePath $PSScriptRoot\Example11.html -ShowHTML {
     New-HTMLLogo
-
-    New-HTMLHeading -Heading h1  -HeadingText 'This is my heading'
-    New-HTMLHeading -Heading h2  -HeadingText 'This is my heading'
-    New-HTMLHeading -Heading h3  -HeadingText 'This is my heading'
-    New-HTMLHeading -Heading h4  -HeadingText 'This is my heading'
-    New-HTMLHeading -Heading h5  -HeadingText 'This is my heading'
-    New-HTMLHeading -Heading h6  -HeadingText 'This is my heading'
-
-    New-HtmlTab -TabName 'Test' {
+    New-HTMLTab -TabName 'Test1' {
+        New-HTMLHeading -Heading h1 -HeadingText 'This is my heading'
+        New-HTMLHeading -Heading h2 -HeadingText 'This is my heading'
+        New-HTMLHeading -Heading h3 -HeadingText 'This is my heading'
+        New-HTMLHeading -Heading h4 -HeadingText 'This is my heading'
+        New-HTMLHeading -Heading h5 -HeadingText 'This is my heading'
+        New-HTMLHeading -Heading h6 -HeadingText 'This is my heading'
         New-HTMLSection -HeaderText '0 section' {
             New-HTMLPanel {
                 New-HTMLTable -DataTable $Processes -HideFooter
