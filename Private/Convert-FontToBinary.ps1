74c9ef8661a1d8a1dd476f36f1204805450e946e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-11-29 14:23:57 +0100

    ConvertTo-FontBinary function

---
+++ Private/Convert-FontToBinary.ps1
@@ -0,0 +1,19 @@
+﻿function Convert-FontToBinary {
+    [CmdLetBinding()]
+    param(
+        [string[]] $Content,
+        [string] $Search,
+        [string] $ReplacePath,
+        [string] $FileType
+    )
+    if ($Content -like "*$Search*") {
+        if ($PSEdition -eq 'Core') {
+            $ImageContent = Get-Content -AsByteStream -LiteralPath $ReplacePath
+        } else {
+            $ImageContent = Get-Content -LiteralPath $ReplacePath -Encoding Byte
+        }
+        $Replace = "data:application/$FileType;charset=utf-8;base64," + [Convert]::ToBase64String($ImageContent)
+        $Content = $Content.Replace($Search, $Replace)
+    }
+    $Content
+}
