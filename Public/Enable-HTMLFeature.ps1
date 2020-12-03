935c0b2c0e91ee77004f6940de934313ce07453e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-03 16:46:48 +0100

    Enable-HTMLFeature

---
+++ Public/Enable-HTMLFeature.ps1
@@ -0,0 +1,14 @@
+﻿function Enable-HTMLFeature {
+    [cmdletBinding()]
+    param(
+        [string[]] $Feature
+    )
+    foreach ($F in $Feature) {
+        $Script:HTMLSchema.Features.$F = $true
+    }
+}
+
+Register-ArgumentCompleter -CommandName Enable-HTMLFeature -ParameterName Feature -ScriptBlock {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    $Script:CurrentConfiguration.Features.Keys | Sort-Object | Where-Object { $_ -like "*$wordToComplete*" }
+}
