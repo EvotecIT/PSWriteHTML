8bd84e40dd7c3906679ff183b15166d88b18eeb0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-06 18:58:34 +0100

    Update examples

---
+++ Examples/Example-TreeView/Example33_01.ps1
@@ -0,0 +1,14 @@
+﻿#Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTML -TitleText 'This is a test' -FilePath "$PSScriptRoot\Example33_01.html" {
+    New-HTMLTree -Checkbox none {
+        New-TreeNode -Title 'Test' -Id 'Test' -Folder {
+            New-TreeNode -Title 'ooh' {
+                New-TreeNode -Title 'Test1'
+                New-TreeNode -Title 'Test2' {
+                    New-TreeNode -Title 'Nope'
+                }
+            }
+        }
+    }
+} -Online -ShowHTML
---
+++ Examples/Example-TreeView/Example34_02.ps1
@@ -0,0 +1,36 @@
+﻿# THIS IS NOT FINISHED
+# I DON'T KNOW HOW TO BUILD THIS :-)
+
+Import-Module .\PSWriteHTML.psd1 -Force
+
+$Content = Get-ChildItem -LiteralPath 'C:\Support\Application-FFMPG' -Recurse
+#$CurrentParent = $_.PSParentPath -replace 'Microsoft.PowerShell.Core\FileSystem::',''
+if ($CurrentParent) {
+
+
+}
+
+New-HTML -TitleText 'This is a test' -FilePath "$PSScriptRoot\Example33_02.html" {
+    New-HTMLTree -Checkbox none {
+
+
+        foreach ($_ in $Content) {
+
+
+
+            New-TreeNode -Title $Parent {
+              #  New-TreeNode -Title $_.Name
+            }
+        }
+        <#
+        New-TreeNode -Title 'Test' -Id 'Test' -Folder {
+            New-TreeNode -Title 'ooh' {
+                New-TreeNode -Title 'Test1'
+                New-TreeNode -Title 'Test2' {
+                    New-TreeNode -Title 'Nope'
+                }
+            }
+        }
+        #>
+    }
+} -Online #-ShowHTML
