60017ed75dc89bd8d829b3b693b79db435604daa
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2020-12-20 10:19:49 +0100

    org chart example

--- Examples/Example-OrganizationalCharts/Example-OrgChart02.ps1
+++ Examples/Example-OrganizationalCharts/Example-OrgChart02.ps1
@@ -16,13 +16,23 @@ New-HTML {
         New-HTMLSection -HeaderText 'Organization Chart 2' {
             New-HTMLOrgChart {
                 New-OrgChartNode -Name 'Test' -Title 'Test2' {
-                    New-OrgChartNode -Name 'Test' -Title 'Test2'
+                    New-OrgChartNode -Name 'Test' -Title 'Test5' -TitleColor Blue -TitleBackgroundColor red -ContentBackgroundColor yellow -ContentColor pink
+
                     New-OrgChartNode -Name 'Test' -Title 'Test2'
                     New-OrgChartNode -Name 'Test' -Title 'Test2' {
-                        New-OrgChartNode -Name 'Test' -Title 'Test2'
+                        New-OrgChartNode -Name 'Test' -Title 'Test2' -TitleColor Blue -TitleBackgroundColor Red
+                    }
+                    New-OrgChartNode -Name 'Test' -Title 'Test2' {
+                        New-OrgChartNode -Name 'Test' -Title 'Test2' -TitleColor Blue -TitleBackgroundColor Red
+                    }
+                    New-OrgChartNode -Name 'Test' -Title 'Test2' {
+                        New-OrgChartNode -Name 'Test' -Title 'Test2' -TitleColor Blue -TitleBackgroundColor Red
+                    }
+                    New-OrgChartNode -Name 'Test' -Title 'Test2' {
+                        New-OrgChartNode -Name 'Test' -Title 'Test2' -TitleColor Blue -TitleBackgroundColor Red
                     }
                 }
-            } -Draggable -Pan
+            } -Draggable -Pan -Zoom
         }
     } -Invisible
 } -FilePath $PSScriptRoot\Example-OrgChart02.html -ShowHTML -Online
