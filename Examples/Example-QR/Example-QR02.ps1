219add3c8212e95bd59af7e2639294d25927368b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-02 13:50:14 +0100

    Update example QR codes

--- Examples/Example-QR/Example-QR02.ps1
+++ Examples/Example-QR/Example-QR02.ps1
@@ -6,7 +6,7 @@ New-HTML -Online -FilePath $PSScriptRoot\Example-QR02.html {
             New-HTMLQRCode -Link 'https://evotec.xyz'
         }
         New-HTMLPanel {
-            New-HTMLQRCode -Link 'https://evotec.xyz'
+            New-HTMLQRCode -Link 'https://evotec.xyz' -Height 250 -Width 250 -Logo 'https://evotec.xyz/wp-content/uploads/2015/05/Logo-evotec-012.png' -LogoWidth 100 -LogoInline
         }
     }
     # Unfortunetly this table when defined outside of New-HTML won't work, not yet - would need to think on new solution
@@ -17,7 +17,7 @@ New-HTML -Online -FilePath $PSScriptRoot\Example-QR02.html {
         }
         [PSCustomObject] @{
             ComputerName = 'EvoMac'
-            QRCode       = New-HTMLQRCode -Link 'https://evotec.xyz/EvoMac' -Height 50 -Width 50
+            QRCode       = New-HTMLQRCode -Link 'https://evotec.xyz/EvoMac' -Height 50 -Width 50 -Logo 'https://evotec.xyz/wp-content/uploads/2015/05/Logo-evotec-012.png'
         }
     )
     New-HTMLTable -DataTable $Table -Filtering -InvokeHTMLTags
