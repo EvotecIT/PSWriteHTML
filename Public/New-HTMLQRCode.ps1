6624bb8bb27de354a7bd05f2e1482a547915e277
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-02 13:49:37 +0100

    Improvement New-HTMLQRCode

--- Public/New-HTMLQRCode.ps1
+++ Public/New-HTMLQRCode.ps1
@@ -2,24 +2,39 @@ function New-HTMLQRCode {
     [cmdletBinding()]
     param(
         [string] $Link,
-        [int] $Width,
-        [int] $Height,
+        [object] $Width,
+        [object] $Height,
         [string] $Title,
-        [string] $TitleColor
+        [string] $TitleColor,
+        [string] $Logo,
+        [object] $LogoWidth,
+        [object] $LogoHeight,
+        [switch] $LogoInline
     )
     $Script:HTMLSchema.Features.QR = $true
 
     if (-not $AnchorName) {
         $AnchorName = "QrCode$(Get-RandomStringName -Size 8)"
     }
+    if ($LogoInline) {
+        # Cache makes sure that file is downloaded once and can be reused over and over until cache is reset
+        # Resetting of cache is done automatically on module reload
+        # This can be very useful when sending 3000 emails with same logo
+        $LogoImage = Convert-Image -Image $Logo -Cache:(-not $DisableCache)
+    } else {
+        $LogoImage = $Logo
+    }
 
     New-HTMLTag -Tag 'div' -Attributes @{ Id = $AnchorName; class = 'qrcode flexElement' }
 
     $Options = @{
         text       = $Link
-        width      = $Width
-        height     = $Height
+        width      = ConvertTo-Size -Size $Width
+        height     = ConvertTo-Size -Size $Height
         title      = $Title
+        logo       = $LogoImage
+        logoWidth  = ConvertTo-Size -Size $LogoWidth
+        logoHeight = ConvertTo-Size -Size $LogoHeight
         titleColor = ConvertFrom-Color -Color $TitleColor
     }
     Remove-EmptyValue -Hashtable $Options -Recursive
