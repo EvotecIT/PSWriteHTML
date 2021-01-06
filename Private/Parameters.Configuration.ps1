7268707dff71e0b540805903804b3c6bf72ba719
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-06 22:24:56 +0100

    Apex downgrade

--- Private/Parameters.Configuration.ps1
+++ Private/Parameters.Configuration.ps1
@@ -460,14 +460,14 @@
             Email   = $false
         }
         ChartsApex              = @{
-            Comment      = 'Apex Charts'
-            Header       = @{
+            Comment = 'Apex Charts'
+            Header  = @{
                 JsLink = @(
                     'https://cdn.jsdelivr.net/npm/promise-polyfill@8/dist/polyfill.min.js' # If you need to make it work with IE11, you need to include these polyfills before including ApexCharts
                     'https://cdn.jsdelivr.net/npm/eligrey-classlist-js-polyfill@1.2.20180112/classList.min.js' # If you need to make it work with IE11, you need to include these polyfills before including ApexCharts
                     'https://cdn.jsdelivr.net/npm/findindex_polyfill_mdn@1.0.0/findIndex.min.js' # You will need this only if you require timeline/rangebar charts
                     #'https://unpkg.com/canvg@3.0.4/lib/umd.js' # You will need this only if you require PNG download of your charts
-                    'https://cdn.jsdelivr.net/npm/apexcharts@3.23.0/dist/apexcharts.min.js'
+                    'https://cdn.jsdelivr.net/npm/apexcharts@3.22.0/dist/apexcharts.min.js'
                 )
                 JS     = @(
                     "$PSScriptRoot\..\Resources\JS\polyfill.min.js"
@@ -477,11 +477,8 @@
                     "$PSScriptRoot\..\Resources\JS\apexcharts.min.js"
                 )
             }
-            HeaderAlways = @{
-                #Css = "$PSScriptRoot\..\Resources\CSS\apexcharts.css"
-            }
-            Default      = $true
-            Email        = $false
+            Default = $true
+            Email   = $false
         }
         ChartsOrg               = [ordered] @{
             Comment      = 'OrgChart'
