9001fae00cd4c4bb20680dd35f0e6cce5285d19c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-16 19:15:02 +0100

    Update New-HTMLTableStyle

--- Public/New-HTMLTableStyle.ps1
+++ Public/New-HTMLTableStyle.ps1
@@ -1,8 +1,8 @@
 ﻿function New-HTMLTableStyle {
     [alias('EmailTableStyle', 'TableStyle', 'New-TableStyle')]
-    [cmdletBinding()]
+    [cmdletBinding(DefaultParameterSetName = 'Manual')]
     param(
-        [ValidateSet('Table', 'Header', 'Row', 'Footer', 'RowOdd', 'RowEven', 'RowSelected', 'RowSelectedEven', 'RowSelectedOdd', 'RowHover', 'RowHoverSelected', 'Button')][string] $Type = 'Table',
+        [Parameter(ParameterSetName = 'Manual')][ValidateSet('Table', 'Header', 'Row', 'Footer', 'RowOdd', 'RowEven', 'RowSelected', 'RowSelectedEven', 'RowSelectedOdd', 'RowHover', 'RowHoverSelected', 'Button')][string] $Type = 'Table',
         [Parameter(ParameterSetName = 'Manual')][alias('TextSize')][string] $FontSize,
         [Parameter(ParameterSetName = 'Manual')][ValidateSet('normal', 'bold', 'bolder', 'lighter', '100', '200', '300', '400', '500', '600', '700', '800', '900')][string] $FontWeight,
         [Parameter(ParameterSetName = 'Manual')][ValidateSet('normal', 'italic', 'oblique')][string] $FontStyle,
@@ -12,7 +12,23 @@
         [Parameter(ParameterSetName = 'Manual')][string] $TextColor,
         [Parameter(ParameterSetName = 'Manual')][ValidateSet('none', 'line-through', 'overline', 'underline')][string] $TextDecoration,
         [Parameter(ParameterSetName = 'Manual')][ValidateSet('uppercase', 'lowercase', 'capitalize')][string] $TextTransform,
-        [alias('FontAlign', 'Align')][Parameter(ParameterSetName = 'Manual')][ValidateSet('left', 'right', 'center', 'justify')][string] $TextAlign
+        [Parameter(ParameterSetName = 'Manual')][alias('FontAlign', 'Align')][ValidateSet('left', 'right', 'center', 'justify')][string] $TextAlign,
+
+        [Parameter(ParameterSetName = 'Manual')][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderTopStyle,
+        [Parameter(ParameterSetName = 'Manual')][string] $BorderTopColor,
+        [Parameter(ParameterSetName = 'Manual')][string] $BorderTopWidthSize,
+
+        [Parameter(ParameterSetName = 'Manual')][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderBottomStyle,
+        [Parameter(ParameterSetName = 'Manual')][string] $BorderBottomColor,
+        [Parameter(ParameterSetName = 'Manual')][string] $BorderBottomWidthSize,
+
+        [Parameter(ParameterSetName = 'Manual')][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderLeftStyle,
+        [Parameter(ParameterSetName = 'Manual')][string] $BorderLeftColor,
+        [Parameter(ParameterSetName = 'Manual')][string] $BorderLeftWidthSize,
+
+        [Parameter(ParameterSetName = 'Manual')][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderRightStyle,
+        [Parameter(ParameterSetName = 'Manual')][string] $BorderRightColor,
+        [Parameter(ParameterSetName = 'Manual')][string] $BorderRightWidthSize
     )
     $CssConfiguration = Get-ConfigurationCss -Feature 'DataTables' -Type 'HeaderAlways'
     #$Row = @(
@@ -61,16 +77,32 @@
     )
 
     $TableStyle = [ordered] @{
-        'text-align'       = $TextAlign
-        'text-transform'   = $TextTransform
-        'color'            = ConvertFrom-Color -Color $TextColor
-        'background-color' = ConvertFrom-Color -Color $BackgroundColor
-        'font-size'        = ConvertFrom-Size -TextSize $FontSize
-        'font-weight'      = $FontWeight
-        'font-style'       = $FontStyle
-        'font-variant'     = $FontVariant
-        'font-family'      = $FontFamily
-        'text-decoration'  = $TextDecoration
+        'text-align'          = $TextAlign
+        'text-transform'      = $TextTransform
+        'color'               = ConvertFrom-Color -Color $TextColor
+        'background-color'    = ConvertFrom-Color -Color $BackgroundColor
+        'font-size'           = ConvertFrom-Size -TextSize $FontSize
+        'font-weight'         = $FontWeight
+        'font-style'          = $FontStyle
+        'font-variant'        = $FontVariant
+        'font-family'         = $FontFamily
+        'text-decoration'     = $TextDecoration
+
+        'border-top-width'    = ConvertFrom-Size -TextSize $BorderTopWidth
+        'border-top-style'    = $BorderTopStyle
+        'border-top-color'    = ConvertFrom-Color -Color $BorderTopColor
+
+        'border-bottom-width' = ConvertFrom-Size -TextSize $BorderBottomWidth
+        'border-bottom-style' = $BorderBottomStyle
+        'border-bottom-color' = "$(ConvertFrom-Color -Color $BorderBottomColor) !important"
+
+        'border-left-width'   = ConvertFrom-Size -TextSize $BorderLeftWidth
+        'border-left-style'   = $BorderLeftStyle
+        'border-left-color'   = ConvertFrom-Color -Color $BorderLeftColor
+
+        'border-right-width'  = ConvertFrom-Size -TextSize $BorderRightWidth
+        'border-right-style'  = $BorderRightStyle
+        'border-right-color'  = ConvertFrom-Color -Color $BorderRightColor
     }
 
     # this will add configuration for all Tables as it already exists
@@ -92,10 +124,10 @@
         }
     }
     if ($Type -in 'Header', 'Table') {
-        Add-ConfigurationCSS -CSS $CssConfiguration -Name 'thead' -Inject $TableStyle
+        Add-ConfigurationCSS -CSS $CssConfiguration -Name 'table.dataTable thead th, table.dataTable thead td' -Inject $TableStyle
     }
     if ($Type -in 'Footer', 'Table') {
-        Add-ConfigurationCSS -CSS $CssConfiguration -Name 'tfoot' -Inject $TableStyle
+        Add-ConfigurationCSS -CSS $CssConfiguration -Name 'table.dataTable tfoot th, table.dataTable tfoot td' -Inject $TableStyle
     }
     if ($Type -in 'RowOdd', 'Row', 'Table') {
         foreach ($Name in $RowOdd) {
@@ -131,3 +163,10 @@
         }
     }
 }
+
+Register-ArgumentCompleter -CommandName New-HTMLTableStyle -ParameterName BorderBottomColor -ScriptBlock $Script:ScriptBlockColors
+Register-ArgumentCompleter -CommandName New-HTMLTableStyle -ParameterName BorderTopColor -ScriptBlock $Script:ScriptBlockColors
+Register-ArgumentCompleter -CommandName New-HTMLTableStyle -ParameterName BorderRightColor -ScriptBlock $Script:ScriptBlockColors
+Register-ArgumentCompleter -CommandName New-HTMLTableStyle -ParameterName BorderLeftColor -ScriptBlock $Script:ScriptBlockColors
+Register-ArgumentCompleter -CommandName New-HTMLTableStyle -ParameterName BackgroundColor -ScriptBlock $Script:ScriptBlockColors
+Register-ArgumentCompleter -CommandName New-HTMLTableStyle -ParameterName TextColor -ScriptBlock $Script:ScriptBlockColors
