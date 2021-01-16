25be21e3a3770695b46a16dd03e1daf2ca65574d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-01-16 19:15:34 +0100

    Formatting example

--- Examples/Example-Formatting/Example-Formatting.ps1
+++ Examples/Example-Formatting/Example-Formatting.ps1
@@ -1,6 +1,18 @@
 ﻿Import-Module PSParseHTML -Force
 
-Format-HTML -File 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML\Examples\Example25-Toasts\Example25.html' -OutputFile $PSScriptRoot\Output\Example.Toasts.html
-Optimize-ResourceFile -File 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML\Resources\CSS\datatables.min.css' -OutputFile $PSScriptRoot\Output\Example.DataTables.css
-Optimize-ResourceFile -File 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML\Resources\JS\HideSection.js' -OutputFile $PSScriptRoot\Output\Example.HideSection.js
-Optimize-ResourceFile -File 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML\Resources\JS\jquery-3.3.1.min.js' -OutputFile $PSScriptRoot\Output\Example.Jquery.js
+New-HTML -Online {
+
+} -FilePath $PSScriptRoot\Output\TestWithoutFormat.html
+
+New-HTML -Online {
+
+} -FilePath $PSScriptRoot\Output\TestWithFormat.html -Format
+
+New-HTML -Online {
+
+} -FilePath $PSScriptRoot\Output\TestMinimize.html -Minify
+
+#Format-HTML -File 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML\Examples\Example25-Toasts\Example25.html' -OutputFile $PSScriptRoot\Output\Example.Toasts.html
+#Optimize-ResourceFile -File 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML\Resources\CSS\datatables.min.css' -OutputFile $PSScriptRoot\Output\Example.DataTables.css
+#Optimize-ResourceFile -File 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML\Resources\JS\HideSection.js' -OutputFile $PSScriptRoot\Output\Example.HideSection.js
+#Optimize-ResourceFile -File 'C:\Users\przemyslaw.klys\OneDrive - Evotec\Support\GitHub\PSWriteHTML\Resources\JS\jquery-3.3.1.min.js' -OutputFile $PSScriptRoot\Output\Example.Jquery.js
