00810aac0606a27389ff2c96c59762eb83460b89
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-07 15:03:44 +0100

    search builder example

---
+++ Examples/Example-TableSearchBuilder/Example-TableSearchBuilder.ps1
@@ -0,0 +1,18 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Table = Get-Process | Select-Object -First 4 -Property Name, Id, PriorityClass, Handle, HandleCount, WorkingSet
+
+New-HTML -TitleText "Testing Search Pane" -FilePath "$PSScriptRoot\Example42-TableSearchPanes.html" {
+    # Hide Buttons
+    New-HTMLSection -HeaderText "SearchBuilder" -Content {
+        New-HTMLTable -DataTable $Table -Filtering {
+
+        } -Buttons @('copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5', 'pageLength', 'searchPanes') -SearchBuilder
+    }
+
+    New-HTMLSection -HeaderText "SearchPane" -Content {
+        New-HTMLTable -DataTable $Table -Filtering {
+
+        } -SearchPane
+    }
+} -ShowHTML -Online -Format
