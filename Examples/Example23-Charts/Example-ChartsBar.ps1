dd5badc3afa1fafb6c69b067bd0f811574ef9bba
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-11 22:18:11 +0100

    Update examples

--- Examples/Example23-Charts/Example-ChartsBar.ps1
+++ Examples/Example23-Charts/Example-ChartsBar.ps1
@@ -14,7 +14,7 @@ New-HTML -TitleText 'My title' -Online -FilePath $PSScriptRoot\Example-ChartsBar
             New-HTMLPanel {
                 New-HTMLChart {
                     New-ChartToolbar -Download
-                    New-ChartLegend -Name 'Time', 'Money', 'Taxes'
+                    New-ChartLegend -Name 'Time', 'Money', 'Taxes' -Color '#024DA1', '#AFD135', Green
                     New-ChartBar -Name 'Test' -Value 1, 2, 3
                     New-ChartBar -Name 'Test1' -Value 2, 5, 7
                     New-ChartBar -Name 'Test2' -Value 3, 1, 2
---
+++ Examples/Example23-Charts/Example-ChartsBarColor.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTML -TitleText 'My title' -Online -FilePath $PSScriptRoot\Example-ChartsBarColor.html {
+    New-HTMLPanel {
+        New-HTMLChart {
+            New-ChartToolbar -Download
+            New-ChartLegend -Name 'Time', 'Money', 'Taxes' -Color '#024DA1', '#AFD135', Green
+            New-ChartBar -Name 'Test' -Value 1, 2, 3
+            New-ChartBar -Name 'Test1' -Value 2, 5, 7
+            New-ChartBar -Name 'Test2' -Value 3, 1, 2
+        } -Title 'Testing Title' -TitleAlignment center
+    }
+} -Show -Format
