fc070df021087ec019689c0e491f4b7bd68edb5d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-12 14:02:18 +0100

    PercentageBar Internal

---
+++ Private/New-TablePercentageBarInternal.ps1
@@ -0,0 +1,19 @@
+﻿function New-TablePercentageBarInternal {
+    [cmdletbinding()]
+    param(
+        [int] $ColumnID,
+        [string] $ColumnName,
+        [ValidateSet('square', 'round')][string] $Type,
+        [string] $TextColor,
+        [string] $BorderColor,
+        [ValidateSet('solid', 'outset', 'groove', 'ridge')][string] $BorderStyle,
+        [string] $BarColor,
+        [string] $BackgroundColor,
+        [int] $RoundValue
+    )
+
+    [ordered]@{
+        targets = $ColumnID
+        render  = "`$.fn.dataTable.render.percentBar('$Type','$TextColor', '$BorderColor', '$BarColor', '$BackgroundColor', $RoundValue, '$BorderStyle')"
+    }
+}
