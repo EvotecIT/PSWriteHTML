c7b3456eace597a8ba1eabe41e92dae97e809e90
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-13 17:47:50 +0100

    Search Examples

--- Examples/Example-Table/Example7_AlphabetSearch.ps1
+++ Examples/Example-Table/Example7_AlphabetSearch.ps1
@@ -4,12 +4,22 @@
 $ProcessSmaller = Get-Process | Select-Object -First 4 -Property Name, Id, PriorityClass, Responding
 
 New-HTML {
-    #New-HTMLSection -Invisible {
-        #New-HTMLSection -HeaderText 'Standard Table with Search Highlight' {
-        #    New-HTMLTable -DataTable $ProcessSmaller -EnableSearchHighlight
-        #}
-       # New-HTMLSection -HeaderText 'Standard Table with Alphabet Search' {
-            New-HTMLTable -DataTable $ProcessSmaller -EnableSearchAlphabet
-        #}
-    #}
+    New-HTMLSection -Invisible {
+        New-HTMLSection -HeaderText 'Standard Table with Search Highlight' {
+            New-HTMLTable -DataTable $ProcessSmaller -SearchHighlight
+        }
+        New-HTMLSection -HeaderText 'Standard Table with Alphabet Search' {
+            New-HTMLTable -DataTable $ProcessSmaller -AlphabetSearch
+        }
+    }
+    New-HTMLSection -Invisible {
+        New-HTMLSection -HeaderText 'Standard Table with Search Builder' {
+            New-HTMLTable -DataTable $ProcessSmaller -SearchBuilder
+        }
+        New-HTMLSection -HeaderText 'Standard Table with Alphabet Search - Advanced' {
+            New-HTMLTable -DataTable $ProcessSmaller -AlphabetSearch {
+                New-TableAlphabetSearch -ColumnName 'Name' -AddNumbers -CaseSensitive
+            }
+        }
+    }
 } -ShowHTML -Online -FilePath $PSScriptRoot\Example7_AlphabetSearch.html -Format
---
+++ Examples/Example-Table/Example7_AlphabetSearch2.ps1
@@ -0,0 +1,10 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+#$Process = Get-Process | Select-Object -First 30
+$ProcessSmaller = Get-ADUser -Filter * | Select-Object -First 15 # -Property Name, Id, PriorityClass, Responding
+
+New-HTML {
+    New-HTMLSection -HeaderText 'Standard Table with Alphabet Search' {
+        New-HTMLTable -DataTable $ProcessSmaller -AlphabetSearch
+    }
+} -ShowHTML -FilePath $PSScriptRoot\Example7_AlphabetSearch2.html -Format -Online
