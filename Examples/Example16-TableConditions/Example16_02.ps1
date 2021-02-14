3d0d0b0378cb2cef558300308b83fbcee2128505
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-14 18:41:07 +0100

    Example update

---
+++ Examples/Example16-TableConditions/Example16_02.ps1
@@ -0,0 +1,26 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Process = @(
+    [PSCustomObject] @{
+        Name = 'Test'
+        Value = 'Something'
+    }
+    [PSCustomObject] @{
+        Name = 'Test'
+        Value = 'Something'
+    }
+    [PSCustomObject] @{
+        Name = 'Test'
+        Value = 'Something'
+    }
+    [PSCustomObject] @{
+        Name = 'Test'
+        Value = 'Something'
+    }
+)
+
+New-HTML -TitleText 'My title' -Online -FilePath $PSScriptRoot\Example16.html {
+    New-HTMLTable -DataTable $Process -PagingOptions @(50, 100, 150, 200) {
+        New-HTMLTableCondition -Name 'Name' -Type string -Operator gt -Value 'Test' -BackgroundColor Yellow
+    }
+} -ShowHTML -Format
