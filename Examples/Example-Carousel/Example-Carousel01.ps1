2363b09fe92db293e8d7672728d52cc1a9dbcfba
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-02-14 16:53:28 +0100

    Not working example

---
+++ Examples/Example-Carousel/Example-Carousel01.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTML -Name 'Test' -FilePath "$PSScriptRoot\Example-Carousel01.html" -Show {
+    New-HTMLCarousel {
+        New-CarouselSlide {
+            New-HTMLText -Text 'Slide 1'
+        }
+        New-CarouselSlide {
+            New-HTMLText -Text 'Slide 1'
+        }
+    }
+
+} -Online -Format
