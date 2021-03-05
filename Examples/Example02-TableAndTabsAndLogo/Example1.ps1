dd968ae79c6001ae85f9cd5f6a6e51d8195381bd
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-05 22:27:42 +0100

    Added example

---
+++ Examples/Example02-TableAndTabsAndLogo/Example1.ps1
@@ -0,0 +1,16 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Allusers = Get-ADUser -Filter * | Select-Object -First 10
+
+New-HTML -TitleText $ReportTitle -Online -FilePath "$PSScriptRoot\Example1.html" {
+    New-HTMLSection -Invisible {
+        New-HTMLSection -HeaderText "Groups 1" {
+            New-HTMLPanel {
+                New-HTMLTable -DataTable $Allusers -Buttons searchPanes
+            }
+        }
+        New-HTMLSection -HeaderText "Groups 2" {
+
+        }
+    }
+} -ShowHTML
