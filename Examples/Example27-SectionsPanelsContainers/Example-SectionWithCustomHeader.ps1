e2a72386515a475207a72cbff3cc3dba4ccedffe
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-06 21:53:19 +0100

    Custom Header idea

---
+++ Examples/Example27-SectionsPanelsContainers/Example-SectionWithCustomHeader.ps1
@@ -0,0 +1,18 @@
+﻿
+Import-Module .\PSWriteHTML.psd1 -Force
+
+$ProcessSmaller = Get-Process | Select-Object -First 1
+
+New-HTML -Name 'Test' -FilePath "$PSScriptRoot\Example27-01.html" -Show {
+    $CustomHeader = New-HTMLText -TextDecoration line-through -FontSize 20px {
+        'Test'
+    }
+    New-HTMLSection -HeaderText $CustomHeader {
+        New-HTMLSection -HeaderText 'Test 1' -CanCollapse -Collapsed {
+            New-HTMLTable -DataTable $ProcessSmaller
+        }
+        New-HTMLSection -HeaderText 'Test 2' -CanCollapse -Collapsed {
+            New-HTMLTable -DataTable $ProcessSmaller
+        }
+    }
+}
