7aaaaede9478a5e8739622172f4cfbf4c461159e
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-06 21:53:05 +0100

    Examples

---
+++ Examples/Example-Maps/Example-Maps.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+Import-Module PSWriteHTML.Maps -Force
+
+New-HTML {
+    New-HTMLSection -Invisible {
+        New-HTMLPanel {
+            New-HTMLMap -Map poland
+        }
+        New-HTMLPanel {
+            New-HTMLMap -Map usa_states
+        }
+    }
+} -ShowHTML -Online -FilePath $PSScriptRoot\Example-Maps.html
---
+++ Examples/Example-Table/Example-TableAdvancedFreeze.ps1
@@ -0,0 +1,11 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$ProcessesAll = Get-Process
+
+New-HTML -TitleText 'Title' -Online -FilePath $PSScriptRoot\Example-TableAdvancedFreeze.html -ShowHTML {
+    New-HTMLSection -HeaderText 'Demo for ScrollY' {
+        New-HTMLPanel {
+            New-HTMLTable -DataTable $ProcessesAll -DisablePaging -FixedHeader -FixedFooter
+        }
+    }
+}
