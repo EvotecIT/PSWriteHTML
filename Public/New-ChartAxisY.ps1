90296626b56514ef1e5ca0ba6f3d745366224a67
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-03-31 21:31:03 +0200

    Improved yAxis

--- Public/New-ChartAxisY.ps1
+++ Public/New-ChartAxisY.ps1
@@ -5,38 +5,119 @@
         [switch] $Show,
         [switch] $ShowAlways,
         [string] $TitleText,
-        [ValidateSet('90', '270')][string] $TitleRotate = '90',
+        [ValidateSet('90', '270')][string] $TitleRotate,
         [int] $TitleOffsetX = 0,
         [int] $TitleOffsetY = 0,
-        [string] $TitleStyleColor,
-        [int] $TitleStyleFontSize = 12,
-        [string] $TitleStylefontFamily = 'Helvetica, Arial, sans-serif',
+        [ValidateSet('normal', 'bold', 'bolder', 'lighter', '100', '200', '300', '400', '500', '600', '700', '800', '900')][string] $TitleFontWeight,
+        [alias('TitleStyleColor')][string] $TitleColor,
+        [alias('TitleStyleFontSize')][int] $TitleFontSize, # = 12,
+        [alias('TitleStyleFontFamily')][string] $TitleFontFamily, # = 'Helvetica, Arial, sans-serif',
         [int] $MinValue,
-        [int] $MaxValue
-        #[ValidateSet('top', 'topRight', 'left', 'right', 'bottom', '')][string] $LegendPosition = '',
-        # [string[]] $Color
+        [int] $MaxValue,
+        [int] $LabelMinWidth = -1,
+        [int] $LabelMaxWidth,
+        [ValidateSet('left', 'center', 'right')][string] $LabelAlign,
+        [object] $LabelFontSize,
+        [string] $LabelFontFamily,
+        [ValidateSet('normal', 'bold', 'bolder', 'lighter', '100', '200', '300', '400', '500', '600', '700', '800', '900')][string] $LabelFontWeight,
+        [string[]] $LabelFontColor,
+        [switch] $Reversed,
+        [switch] $Opposite,
+        [switch] $Logarithmic,
+        [switch] $ForceNiceScale,
+        [switch] $Floating
     )
-    [PSCustomObject] @{
+    $Object = [PSCustomObject] @{
         ObjectType = 'ChartAxisY'
         ChartAxisY = @{
-            Show                 = $Show.IsPresent
-            ShowAlways           = $ShowAlways.IsPresent
-            TitleText            = $TitleText
-            TitleRotate          = $TitleRotate
-            TitleOffsetX         = $TitleOffsetX
-            TitleOffsetY         = $TitleOffsetY
-            TitleStyleColor      = $TitleStyleColor
-            TitleStyleFontSize   = $TitleStyleFontSize
-            TitleStylefontFamily = $TitleStylefontFamily
-            Min                  = $MinValue
-            Max                  = $MaxValue
+            show       = $Show.IsPresent
+            showAlways = $ShowAlways.IsPresent
+        }
+    }
+    if ($Reversed) {
+        $Object.ChartAxisY.reversed = $true
+    }
+    if ($Opposite) {
+        $Object.ChartAxisY.opposite = $true
+    }
+    if ($Logarithmic) {
+        $Object.ChartAxisY.logarithmic = $true
+    }
+    if ($ForceNiceScale) {
+        $Object.ChartAxisY.forceNiceScale = $true
+    }
+    if ($Floating) {
+        $Object.ChartAxisY.floating = $true
+    }
+    if ($MinValue) {
+        $Object.ChartAxisY.min = $MinValue
+    }
+    if ($MaxValue) {
+        $Object.ChartAxisY.max = $MaxValue
+    }
+
+    $Object.ChartAxisY.title = @{}
+    if ($TitleText) {
+        $Object.ChartAxisY.title.text = $TitleText
+    }
+    if ($TitleRotate) {
+        $Object.ChartAxisY.title.rotate = [int] $TitleRotate
+    }
+    if ($TitleOffsetX) {
+        $Object.ChartAxisY.title.offsetX = $TitleOffsetX
+    }
+    if ($TitleOffsetY) {
+        $Object.ChartAxisY.title.offsetY = $TitleOffsetY
+    }
+    if ($TitleColor -or $TitleFontSize -or $TitleFontFamily) {
+        $Object.ChartAxisY.title.style = @{}
+        if ($TitleColor) {
+            $Object.ChartAxisY.title.style.color = ConvertFrom-Color -Color $TitleColor
+        }
+        if ($TitleFontSize) {
+            $Object.ChartAxisY.title.style.fontSize = ConvertFrom-Size -Size $TitleFontSize
+        }
+        if ($TitleFontFamily) {
+            $Object.ChartAxisY.title.style.fontFamily = $TitleFontFamily
+        }
+        if ($TitleFontWeight) {
+            $Object.ChartAxisY.title.style.fontWeight = $TitleFontWeight
         }
     }
 
+    $Object.ChartAxisY.labels = @{}
+    if ($LabelAlign) {
+        $Object.ChartAxisY.labels.align = $LabelAlign
+    }
+    if ($LabelMinWidth -ne -1) {
+        $Object.ChartAxisY.labels.minWidth = $LabelMinWidth
+    }
+    if ($LabelMaxWidth) {
+        $Object.ChartAxisY.labels.maxWidth = $LabelMaxWidth
+    }
+    if ($LabelFontSize -or $LabelFontFamily -or $LabelFontWeight -or $LabelFontColor) {
+        $Object.ChartAxisY.labels.style = @{}
+
+        if ($LabelFontSize) {
+            $Object.ChartAxisY.labels.style.fontSize = ConvertFrom-Size -Size $LabelFontSize
+        }
+        if ($LabelFontFamily) {
+            $Object.ChartAxisY.labels.style.fontFamily = $LabelFontFamily
+        }
+        if ($LabelFontWeight) {
+            $Object.ChartAxisY.labels.style.fontWeight = $LabelFontWeight
+        }
+        if ($LabelFontColor) {
+            $Object.ChartAxisY.labels.style.colors = @($LabelFontColor)
+        }
+    }
+    Remove-EmptyValue -Hashtable $Object.ChartAxisY -Recursive -Rerun 2
+    $Object
+
     # https://apexcharts.com/docs/options/yaxis/
 }
-Register-ArgumentCompleter -CommandName New-ChartAxisY -ParameterName TitleStyleColor -ScriptBlock $Script:ScriptBlockColors
-
+Register-ArgumentCompleter -CommandName New-ChartAxisY -ParameterName TitleColor -ScriptBlock $Script:ScriptBlockColors
+Register-ArgumentCompleter -CommandName New-ChartAxisY -ParameterName LabelFontColor -ScriptBlock $Script:ScriptBlockColors
 <# We can build this
     yaxis: {
         show: true,
