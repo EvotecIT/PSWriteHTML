ed0e84ecdca4461c4ec17782e7d75fb5ac380915
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-01 09:47:52 +0200

    Rename of example

--- Examples/Example-Charts/Example-ChartsBars.ps1
+++ Examples/Example-Charts/Example-ChartsBars.ps1
@@ -10,7 +10,7 @@ New-HTML -TitleText 'My title' -Online -FilePath $PSScriptRoot\Example-ChartsBar
                 New-ChartBar -Name 'Test very long' -Value 1
                 New-ChartBar -Name 'Test1' -Value 20
                 New-ChartBar -Name 'Test2 and even longer title' -Value 30
-            } -Title 'Testing Title' -TitleAlignment center
+            } -Title 'Testing Title' -TitleAlignment center -SubTitle 'Because SubTitles are better' -SubTitleAlignment center -SubTitleFontSize 30 -TitleColor Gold
         }
     }
 } -Show
