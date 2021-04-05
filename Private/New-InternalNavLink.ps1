d91bedafd24944ef66517bdaf52d89df845f745a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-05 21:07:56 +0200

    Removed dead code

--- Private/New-InternalNavLink.ps1
+++ Private/New-InternalNavLink.ps1
@@ -21,35 +21,17 @@
     )
 
     if ($Nested) {
-        #if ($Href) {
         $NavLink = New-HTMLTag -Tag 'span' -Attributes @{ class = 'its-parent' } {
-
             New-InternalNavIcon -IconBrands $IconBrands -IconRegular $IconRegular -IconSolid $IconSolid -IconMaterial $IconMaterial -Spinning:$Spinning.IsPresent -SpinningReverse:$SpinningReverse.IsPresent -IconColor $IconColor -Bordered:$Bordered.IsPresent -BorderedCircle:$BorderedCircle.IsPresent -PullLeft:$PullLeft.IsPresent -PullRight:$PullRight.IsPresent -Rotate $Rotate -FlipVertical:$FlipVertical.IsPresent -FlipHorizontal:$FlipHorizontal.IsPresent
             $Name
-
         }
-        # } else {
-        #     $NavLink = New-HTMLTag -Tag 'span' -Attributes @{ class = 'its-parent' } {
-        #         New-InternalNavIcon -IconBrands $IconBrands -IconRegular $IconRegular -IconSolid $IconSolid -IconMaterial $IconMaterial -Spinning:$Spinning.IsPresent -SpinningReverse:$SpinningReverse.IsPresent -IconColor $IconColor -Bordered:$Bordered.IsPresent -BorderedCircle:$BorderedCircle.IsPresent -PullLeft:$PullLeft.IsPresent -PullRight:$PullRight.IsPresent -Rotate $Rotate -FlipVertical:$FlipVertical.IsPresent -FlipHorizontal:$FlipHorizontal.IsPresent
-        #         $Name
-        #     }
-        # }
     } else {
-        #if ($Href) {
         $NavLink = New-HTMLTag -Tag 'li' {
             New-HTMLTag -Tag 'a' -Attributes @{ href = $Href } {
                 New-InternalNavIcon -IconBrands $IconBrands -IconRegular $IconRegular -IconSolid $IconSolid -IconMaterial $IconMaterial -Spinning:$Spinning.IsPresent -SpinningReverse:$SpinningReverse.IsPresent -IconColor $IconColor -Bordered:$Bordered.IsPresent -BorderedCircle:$BorderedCircle.IsPresent -PullLeft:$PullLeft.IsPresent -PullRight:$PullRight.IsPresent -Rotate $Rotate -FlipVertical:$FlipVertical.IsPresent -FlipHorizontal:$FlipHorizontal.IsPresent
                 $Name
             }
         }
-        # } else {
-        #     $NavLink = New-HTMLTag -Tag 'li' {
-        #         New-InternalNavIcon -IconBrands $IconBrands -IconRegular $IconRegular -IconSolid $IconSolid -IconMaterial $IconMaterial -Spinning:$Spinning.IsPresent -SpinningReverse:$SpinningReverse.IsPresent -IconColor $IconColor -Bordered:$Bordered.IsPresent -BorderedCircle:$BorderedCircle.IsPresent -PullLeft:$PullLeft.IsPresent -PullRight:$PullRight.IsPresent -Rotate $Rotate -FlipVertical:$FlipVertical.IsPresent -FlipHorizontal:$FlipHorizontal.IsPresent
-        #         $Name
-        #     }
-        # }
-
-
     }
     $NavLink
 }
