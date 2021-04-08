857cc6316098df51dc88ed04e967b5729f83b4d3
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-08 23:14:40 +0200

    AnimateToolkit

--- Private/Parameters.Configuration.ps1
+++ Private/Parameters.Configuration.ps1
@@ -42,7 +42,7 @@ $Script:Configuration = [ordered] @{
             Email   = $false
             License = 'SIL OFL 1.1'
         }
-        FontsAnimate                = @{
+        AnimateToolkit              = @{
             Comment = 'Animation Toolkit'
             Header  = @{
                 CssLink = "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css"
