d32e85db709da6bfb8005580e30fdc500314cd5b
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-16 14:07:55 +0200

    Added more properties and help

--- Public/New-CalendarEvent.ps1
+++ Public/New-CalendarEvent.ps1
@@ -1,4 +1,54 @@
 ﻿function New-CalendarEvent {
+    <#
+    .SYNOPSIS
+    Allows adding new calendar events to calendar
+
+    .DESCRIPTION
+    Allows adding new calendar events to calendar
+
+    .PARAMETER Title
+    The text that will appear on an event.
+
+    .PARAMETER Description
+    The text that will appear on an event when hovering over
+
+    .PARAMETER StartDate
+    When your event begins. If your event is explicitly allDay, hour, minutes, seconds and milliseconds will be ignored.
+
+    .PARAMETER EndDate
+    hen your event ends. If your event is explicitly allDay, hour, minutes, seconds and milliseconds will be ignored. If omitted, your events will appear to have the default duration.
+
+    .PARAMETER Constraint
+    A groupId belonging to other events, "businessHours" or "availableForMeeting"
+
+    .PARAMETER Color
+    An alias for specifying the backgroundColor and borderColor at the same time.
+
+    .PARAMETER BackgroundColor
+    Sets backround color. Only works if EndDate is provided.
+
+    .PARAMETER BorderColor
+    Sets border color. Only works if EndDate is provided.
+
+    .PARAMETER TextColor
+    Sets color of Text. Only works if EndDate is provided.
+
+    .PARAMETER Url
+    A URL that will be visited when this event is clicked by the user.
+
+    .PARAMETER AllDayEvent
+    Determines if the event is shown in the “all-day” section of the view, if applicable. Determines if time text is displayed in the event. If this value is not specified, it will be inferred by the start and end properties
+
+    .EXAMPLE
+    New-HTMLCalendar {
+        New-CalendarEvent -Title 'Active Directory Meeting' -Description 'We will talk about stuff' -StartDate (Get-Date)
+        # End date is required for the colors to work...
+        New-CalendarEvent -Title 'Lunch' -StartDate (Get-Date).AddDays(2).AddHours(-3) -EndDate (Get-Date).AddDays(3) -Description 'Very long lunch' -TextColor DeepSkyBlue -BackgroundColor yellow -BorderColor Red
+    }
+
+    .NOTES
+    Keep in mind that colors like background, border, text work only if there's EndDate. If there's no end date the color "color" is used only
+    #>
     [alias('CalendarEvent')]
     [CmdletBinding()]
     param(
@@ -7,18 +57,28 @@
         [DateTime] $StartDate,
         [nullable[DateTime]] $EndDate,
         [string] $Constraint,
-        [string] $Color
+        [string] $Color,
+        [string] $BackgroundColor,
+        [string] $BorderColor,
+        [string] $TextColor,
+        [alias('Uri')][string] $Url,
+        [switch] $AllDayEvent
     )
-
     $Object = [PSCustomObject] @{
         Type     = 'CalendarEvent'
         Settings = [ordered] @{
-            title       = $Title
-            description = $Description
-            constraint  = $Constraint
-            #      url: 'http://google.com/',
-            color       = ConvertFrom-Color -Color $Color
+            title           = $Title
+            description     = $Description
+            constraint      = $Constraint
+            color           = ConvertFrom-Color -Color $Color
+            backgroundColor = ConvertFrom-Color -Color $BackgroundColor
+            borderColor     = ConvertFrom-Color -Color $BorderColor
+            textColor       = ConvertFrom-Color -Color $TextColor
+            url             = $Url
         }
+    }
+    if ($AllDayEvent) {
+        $Object.Settings.allDay = $true
     }
     if ($StartDate) {
         $Object.Settings.start = Get-Date -Date ($StartDate) -Format "yyyy-MM-ddTHH:mm:ss"
@@ -31,67 +91,6 @@
     $Object
 }
 Register-ArgumentCompleter -CommandName New-CalendarEvent -ParameterName Color -ScriptBlock $Script:ScriptBlockColors
-<#
-    events: [
-    {
-        title: 'rrule event',
-        rrule: {
-        dtstart: '2019-08-09T13:00:00',
-        // until: '2019-08-01',
-        freq: 'weekly'
-        },
-        duration: '02:00'
-    }
-    ],
-events: [{
-        title: 'Business Lunch',
-        start: '2019-08-03T13:00:00',
-        constraint: 'businessHours'
-    },
-    {
-        title: 'Meeting',
-        start: '2019-08-13T11:00:00',
-        constraint: 'availableForMeeting', // defined below
-        color: '#257e4a'
-    },
-    {
-        title: 'Conference',
-        start: '2019-08-18',
-        end: '2019-08-20'
-    },
-    {
-        title: 'Party',
-        start: '2019-08-29T20:00:00'
-    },
-
-    // areas where "Meeting" must be dropped
-    {
-        groupId: 'availableForMeeting',
-        start: '2019-08-11T10:00:00',
-        end: '2019-08-11T16:00:00',
-        rendering: 'background'
-    },
-    {
-        groupId: 'availableForMeeting',
-        start: '2019-08-13T10:00:00',
-        end: '2019-08-13T16:00:00',
-        rendering: 'background'
-    },
-
-    // red areas where no events can be dropped
-    {
-        start: '2019-08-24',
-        end: '2019-08-28',
-        overlap: false,
-        rendering: 'background',
-        color: '#ff9f89'
-    },
-    {
-        start: '2019-08-06',
-        end: '2019-08-08',
-        overlap: false,
-        rendering: 'background',
-        color: '#ff9f89'
-    }
-]
-#>
+Register-ArgumentCompleter -CommandName New-CalendarEvent -ParameterName BorderColor -ScriptBlock $Script:ScriptBlockColors
+Register-ArgumentCompleter -CommandName New-CalendarEvent -ParameterName BackgroundColor -ScriptBlock $Script:ScriptBlockColors
+Register-ArgumentCompleter -CommandName New-CalendarEvent -ParameterName TextColor -ScriptBlock $Script:ScriptBlockColors
