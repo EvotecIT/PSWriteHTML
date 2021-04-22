313e60b74803619ed828b07f6eb8406727d175b4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-22 23:07:49 +0200

    Example for #231

--- Examples/Example-Charts/Example-ChartSparks.ps1
+++ Examples/Example-Charts/Example-ChartSparks.ps1
@@ -14,6 +14,20 @@ New-HTML -TitleText 'My charts' -Online -FilePath $PSScriptRoot\Example-ChartSpa
             }
             New-HTMLPanel {
                 New-HTMLChart {
+                    $Categories = @(
+                        'Testing1'
+                        'Testing2'
+                        'Testing3'
+                        'Testing4'
+                        'Testing5'
+                        'Testing6'
+                        'Testing7'
+                        'Testing8'
+                        'Testing9'
+                        'Testing10'
+                        'Testing11'
+                    )
+                    New-ChartAxisX -Type category -Names $Categories
                     New-ChartSpark -Name 'Test1' -Value 12, 14, 2, 47, 42, 15, 47, 75, 65, 19, 14
                 }
             }
