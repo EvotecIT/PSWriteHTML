adb7f95d6c0b5b7bcd9ac8d55205be06c53351b6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-25 18:18:20 +0200

    Slides examples

--- Examples/Example-Carousel/Example-Carousel01.ps1
+++ Examples/Example-Carousel/Example-Carousel01.ps1
@@ -1,12 +1,33 @@
 ﻿Import-Module .\PSWriteHTML.psd1 -Force
 
 New-HTML -Name 'Test' -FilePath "$PSScriptRoot\Example-Carousel01.html" -Show {
-    New-HTMLCarousel {
-        New-CarouselSlide {
-            New-HTMLText -Text 'Slide 1'
+    New-HTMLSection -Invisible {
+        New-HTMLPanel {
+            New-HTMLCarousel -Height auto {
+                New-CarouselSlide -BackgroundColor Gold -Height auto {
+                    New-HTMLText -Text 'Slide 1'
+                }
+                New-CarouselSlide -BackgroundColor '#34495E' -Height 300px {
+
+                    New-HTMLText -Text 'Slide 2' -LineBreak
+                    #New-HTMLText -Text 'Slide 2' -LineBreak
+                    #New-HTMLText -Text 'Slide 2' -LineBreak
+                    #New-HTMLText -Text 'Slide 2'
+
+                }
+                New-CarouselSlide {
+                    New-HTMLChart {
+                        New-ChartLegend -LegendPosition bottom -HorizontalAlign right -Color Red, Blue, Yellow -DisableOnItemHoverHighlightDataSeries -DisableOnItemClickToggleDataSeries
+                        New-ChartAxisY -LabelMaxWidth 200 -LabelAlign left -Show -LabelFontColor Red, Blue, Green -TitleText 'This is title for y Axis' -TitleColor Red
+                        New-ChartBarOptions -Distributed
+                        New-ChartBar -Name 'Test very long' -Value 1
+                        New-ChartBar -Name 'Test1' -Value 20
+                        New-ChartBar -Name 'Test2 and even longer title' -Value 30
+                    } -Title 'Testing Title' -TitleAlignment center -SubTitle 'Because SubTitles are better' -SubTitleAlignment center -SubTitleFontSize 30 -TitleColor Gold
+                }
+            }
         }
-        New-CarouselSlide {
-            New-HTMLText -Text 'Slide 2'
+        New-HTMLPanel {
         }
     }
 } -Online -Format
---
+++ Examples/Example-Carousel/Example-Carousel02.ps1
@@ -0,0 +1,28 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTML -Name 'Test' -FilePath "$PSScriptRoot\Example-Carousel02.html" -Show {
+    New-HTMLSection -Invisible {
+        New-HTMLSection -Invisible {
+            New-HTMLCarousel {
+                New-CarouselSlide {
+                    New-HTMLPanel {
+                        New-HTMLChart {
+                            New-ChartLegend -LegendPosition bottom -HorizontalAlign right -Color Red, Blue, Yellow -DisableOnItemHoverHighlightDataSeries -DisableOnItemClickToggleDataSeries
+                            New-ChartAxisY -LabelMaxWidth 200 -LabelAlign left -Show -LabelFontColor Red, Blue, Green -TitleText 'This is title for y Axis' -TitleColor Red
+                            New-ChartBarOptions -Distributed
+                            New-ChartBar -Name 'Test very long' -Value 1
+                            New-ChartBar -Name 'Test1' -Value 20
+                            New-ChartBar -Name 'Test2 and even longer title' -Value 30
+                        } -Title 'Testing Title' -TitleAlignment center -SubTitle 'Because SubTitles are better' -SubTitleAlignment center -SubTitleFontSize 30 -TitleColor Gold
+                    }
+                }
+                New-CarouselSlide {
+                    New-HTMLText -Text 'Slide 2'
+                }
+            }
+        }
+        New-HTMLSection -Invisible {
+
+        }
+    }
+} -Online -Format
