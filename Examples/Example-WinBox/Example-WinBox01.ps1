37494cce3d4d711720619de25b91f9cea65f5096
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-04-30 23:12:06 +0200

    Winbox examples

---
+++ Examples/Example-WinBox/Example-WinBox01.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Data = Get-Process | Select-Object -First 3
+
+New-HTML -TitleText 'This is a test' -FilePath "$PSScriptRoot\Example-WinBox01.html" {
+    New-HTMLWinBox -Title 'This is a test Window 1' -BackgroundColor Red {
+        New-HTMLText -Text 'This is a text within modal dialog'
+        New-HTMLTable -DataTable $Data
+    } -Width 50% -Height 50% -X center -Y center
+    New-HTMLWinBox -Title 'This is a test Window 2' -BackgroundColor Red {
+        New-HTMLText -Text 'This is a text within modal dialog'
+        New-HTMLTable -DataTable $Data
+    } -Width 50% -Height 50% -X center
+} -Online -ShowHTML
---
+++ Examples/Example-WinBox/Example-WinBox02.ps1
@@ -0,0 +1,5 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTML -TitleText 'This is a test' -FilePath "$PSScriptRoot\Example-WinBox02.html" {
+    New-HTMLWinBox -Title 'This is a test Window' -Url 'https://google.pl'
+} -Online -ShowHTML
