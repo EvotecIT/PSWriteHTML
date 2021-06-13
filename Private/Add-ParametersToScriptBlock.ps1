4953d2a1174096937c53363ec1737ecf98616b56
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-06-13 15:44:20 +0200

    Missing function

---
+++ Private/Add-ParametersToScriptBlock.ps1
@@ -0,0 +1,31 @@
+﻿function Add-ParametersToScriptBlock {
+    [CmdletBinding()]
+    param(
+        [scriptblock] $ScriptBlock,
+        [System.Collections.IDictionary] $Parameter
+    )
+
+    if ($ScriptBlock) {
+        if ($Parameter) {
+            $Count = 0
+            [string] $ScriptBlockParams = @(
+                "param("
+                foreach ($Key in $Parameter.Keys) {
+                    $Count++
+                    if ($Count -eq $Parameter.Keys.Count) {
+                        "`$$($Key)"
+                    } else {
+                        "`$$($Key),"
+                    }
+                }
+                ")"
+                $ScriptBlock.ToString()
+            )
+            $ScriptBlockScript = [scriptblock]::Create($ScriptBlockParams)
+            $ScriptBlockScript
+        } else {
+            $ScriptBlock
+        }
+    }
+
+}
