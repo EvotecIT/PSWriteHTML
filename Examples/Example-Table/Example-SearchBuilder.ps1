0e8d02028ff555be161794e8c1ae4824057f2858
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-06-20 14:39:53 +0200

    Search Builder examplee

---
+++ Examples/Example-Table/Example-SearchBuilder.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$ProcessesAll = Get-Process | Select-Object -First 3
+
+New-HTML -TitleText 'Title' -Online -FilePath $PSScriptRoot\Example-TableAdvancedFreeze.html -ShowHTML {
+    New-HTMLSection -HeaderText 'Search Builder 1' {
+        New-HTMLTable -DataTable $ProcessesAll -SearchBuilder -Buttons excelHtml5, copyHtml5, csvHtml5
+    }
+    New-HTMLSection -HeaderText 'Search Builder as button' {
+        New-HTMLTable -DataTable $ProcessesAll
+    }
+    # This won't really work - button + searchBuilder
+    New-HTMLSection -HeaderText 'Search Builder + button' {
+        # Search Builder will be disabled, button will work
+        New-HTMLTable -DataTable $ProcessesAll -SearchBuilder
+    }
+}
