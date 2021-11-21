d83eb9122b86b04986697de91fa24a42bcf6d60d
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-11-21 13:40:25 +0100

    Update example

---
+++ Examples/Example-Diagrams/Example-Diagram1.ps1
@@ -0,0 +1,19 @@
+﻿New-HTML -TitleText 'My diagram' -Online -FilePath $PSScriptRoot\Example-Basic02.html {
+    New-HTMLTabStyle -SlimTabs
+    New-HTMLTab -Name 'Diagrams' -IconRegular arrow-alt-circle-left {
+        New-HTMLSection -HeaderText 'Diagram - Defaults' -CanCollapse {
+            New-HTMLPanel {
+                New-HTMLDiagram {
+                    New-DiagramNode -Label '1 test' -To '3', '6', '7' -ColorBackground Bisque
+                    New-DiagramNode -Label '2' -To 'Przemyslaw Klys', '1 test'
+                    New-DiagramNode -Label '3' -Title 'my new popup'
+                    New-DiagramNode -Label 'Size based on name' -Shape database -Size 10 -Title "Long Line`nIn another line"
+                    New-DiagramNode -Label 'Przemyslaw Klys'
+                    New-DiagramNode -Label '6' -To 'Przemyslaw Klys' -ColorBorder Aquamarine -ColorBackground Yellow
+                    New-DiagramNode -Label '7' -To 'Przemyslaw Klys' -ColorHighlightBackground Red -Shape diamond
+                    New-DiagramNode -Label '8' -To 'Przemyslaw Klys' -ColorHoverBorder Yellow -Shape diamond
+                }
+            }
+        }
+    }
+} -ShowHTML
