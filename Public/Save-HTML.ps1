2bc1f80ef5bf73bbc5d2d2180f7f3d3e772a60cd
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-11-21 23:01:43 +0100

    Added docs

--- Public/Save-HTML.ps1
+++ Public/Save-HTML.ps1
@@ -1,28 +1,79 @@
 Function Save-HTML {
     <#
     .SYNOPSIS
-    #
+    Saves HTML (text) to a file. Can be used with conjucncton with New-HTML.
 
     .DESCRIPTION
-    Long description
+    Saves HTML (text) to a file. Can be used with conjucncton with New-HTML, although New-HTML has Save-HTML built-in already when providing FilePath parameter.
 
     .PARAMETER FilePath
-    Parameter description
+    Provides the path to the file to be created.
 
     .PARAMETER HTML
-    Parameter description
+    HTML (text) to be saved.
 
     .PARAMETER ShowHTML
-    Parameter description
+    Opens HTML in browser when generating of HTML is done. When no filepath is provided it uses temporary path instead. Good for testing.
 
     .PARAMETER Encoding
-    Parameter description
+    Provides ability to choose encoding of the HTML file. Not really required to use, as UTF8 is the default. Options available: 'Unknown', 'String', 'Unicode', 'Byte', 'BigEndianUnicode', 'UTF8', 'UTF7', 'UTF32', 'Ascii', 'Default', 'Oem', 'BigEndianUTF32'
 
-    .PARAMETER Supress
-    Parameter description
+    .PARAMETER Suppress
+    If Suppress is used then no output is shown. If Suppress is set to false then FilePath is returned.
+
+    .PARAMETER Format
+    Formats HTML output (including CSS/JS). Requires PSParseHTML to be installed and available.
+
+    .PARAMETER Minify
+    Minifies HTML output (including CSS/JS). Requires PSParseHTML to be installed and available.
 
     .EXAMPLE
-    An example
+    $Company = 'Evotec'
+
+    $Text = @"
+    This email is from Evotec. It may contain confidential information. It is intended for the addressee only and may not be copied or disclosed to any third party without our permission. If you are not the intended recipient please contact the sender as soon as possible and delete the material from any computer. If this email has been sent as a personal message to the addressee, the sender is not acting in his/her capacity as an employee or officer of $Company Limited and no liability is accepted for the content of any such email. Outgoing email may be monitored for the purpose of ensuring compliance with our email policy and relevant laws
+    "@
+
+    $HTML = New-HTMLText -FontSize 10 -FontFamily 'Source Sans Pro' -Text $Text
+    Save-HTML -FilePath $PSScriptRoot\Disclaimer.html -HTML $HTML -ShowHTML
+
+    .EXAMPLE
+    $Test = EmailBody {
+        EmailText -LineBreak
+        EmailText -Text 'Z poważaniem' -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray -LineBreak
+        EmailText -Text $DisplayName -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray
+        EmailText -Text $Title -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray
+        EmailText -Text $Department -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray
+        EmailText -LineBreak
+        if ($Mobile) {
+            EmailText -Text "m: $Mobile" -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray
+        }
+        EmailText -Text "e: $Email" -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray
+        EmailText -LineBreak
+        EmailText -Text $CompanyName -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray
+        EmailText -Text $StreetAddress -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray
+        EmailText -Text "$PostalCode $City" -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray
+        EmailText -LineBreak
+        EmailText -Text "[www.aliorleasing.pl](https://www.aliorleasing.pl)" -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray
+        EmailText -LineBreak
+        EmailLayoutRow {
+            EmailLayoutColumn {
+                EmailImage -Source 'https://aliorleasing.pl/images/logoAL2.jpg' -UrlLink '' -AlternativeText 'Alior Leasing Logo' -Width '134' -Inline -Height '90'
+            }
+            EmailLayoutColumn {
+
+            }
+            EmailLayoutColumn {
+                EmailImage -Source 'https://aliorleasing.pl/images/akcia-nnw.jpg' -UrlLink '' -AlternativeText 'Alior Leasing Logo' -Width '300' -Inline -Height '90'
+            }
+        }
+        EmailText -LineBreak
+        EmailText -FontWeight bold, normal -FontSize 8pt -FontFamily 'Verdana,sans-serif' -Color Gray -Text "Alior Leasing", `
+            " Spółka z ograniczoną odpowiedzialnością z siedzibą w Warszawie, ul. Łopuszańska 38D, 02-232 Warszawa, wpisana do rejestru przedsiębiorców Krajowego Rejestru Sądowego pod numerem KRS: 0000554171, której dokumentacja przechowywana jest w Sądzie Rejonowym dla M. St. Warszawy w Warszawie, XIV Wydział Gospodarczy Krajowego Rejestru Sądowego, NIP: 5223027866, REGON: 361335353, o kapitale zakładowym i wpłaconym: 15 009 000,00 PLN."
+        EmailText -LineBreak
+    }
+
+    Save-HTML -FilePath $PSScriptRoot\Output\TestBody2.html -ShowHTML -HTML $Test
 
     .NOTES
     General notes
