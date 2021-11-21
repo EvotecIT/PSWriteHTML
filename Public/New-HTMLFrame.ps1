d58a80b836fe43f844e0794a42fa73a1be1b090c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-11-21 15:53:18 +0100

    Added new New-HTMLFrame

---
+++ Public/New-HTMLFrame.ps1
@@ -0,0 +1,95 @@
+﻿function New-HTMLFrame {
+    <#
+    .SYNOPSIS
+    Allows to inline other HTML files into the current HTML file.
+
+    .DESCRIPTION
+    Allows to inline other HTML files into the current HTML file. This can be useful if we want to display content from another file.
+
+    .PARAMETER Id
+    ID of the HTML element. By default it's auto-generated.
+
+    .PARAMETER SourcePath
+    Path to a file with HTML file to display within iFrame
+
+    .PARAMETER Height
+    Set the height of the iFrame to static value. This should be used when not using iFrameResizer.
+
+    .PARAMETER Scrolling
+    The HTML <iframe> scrolling Attribute is used to specify that whether the scrollbar will be displayed or not in the <Iframe> Element. Basically the scrollbar is used when the content is large than the Iframe Element.
+    Available options are:
+    - auto: It has a default value. The scrollbar appears when needed.
+    - yes: This value shows the scrollbar in the Iframe Element.
+    - no: This value does not show the scrollbar in the Iframe Element.
+
+    .PARAMETER FrameBorder
+    Parameter description
+
+    .PARAMETER UseiFrameResizer
+    Forces HTML inline feature to use iFrameResizer instead of native functionality. For fully functional feature it requires modifying the source HTML file.
+
+    .PARAMETER EnableLogging
+    Enable logging to Console for debugging purposes when using iFrameResizer (requires UseiFrameResizer).
+
+    .EXAMPLE
+    New-HTML {
+        New-HTMLSection {
+            New-HTMLFrame -SourcePath "$PSSCriptRoot\GPOZaurr.html" -Scrolling Auto
+        } -HeaderText 'Test'
+        New-HTMLSection {
+            New-HTMLFrame -SourcePath "$PSSCriptRoot\GPOZaurr.html" -Scrolling Auto -Height 1500px
+        } -HeaderText 'Test'
+        New-HTMLSection {
+            New-HTMLFrame -SourcePath "C:\Support\GitHub\PSWriteHTML\Examples\Example-Maps\Example-Maps.html"
+        } -HeaderText 'Test' -Height 100vh
+    } -Online -TitleText 'Test Inline' -ShowHTML -FilePath "$PSScriptRoot\Example-InlineHTML01.html" -AddComment
+
+    .NOTES
+    General notes
+    #>
+    [CmdletBinding()]
+    param(
+        [string] $Id,
+        [string] $SourcePath,
+        [validateSet('No', 'Yes', 'Auto')][string] $Scrolling = 'auto',
+        [object] $Height,
+        [object] $FrameBorder,
+        [switch] $UseiFrameResizer,
+        [switch] $EnableLogging
+    )
+    $Script:HTMLSchema.Features.iFrame = $true
+    if ($UseiFrameResizer) {
+        $Script:HTMLSchema.Features.iFrameResizer = $true
+    }
+
+    if (-not $ID) {
+        $ID = "Inline-$(Get-RandomStringName -Size 8 -LettersOnly)"
+    }
+
+    $Attributes = [ordered] @{
+        id          = $Id
+        src         = $SourcePath
+        height      = ConvertFrom-Size -Size $Height
+        frameborder = ConvertFrom-Size -Size $FrameBorder
+        scrolling   = $Scrolling.ToLower()
+    }
+    Remove-EmptyValue -Hashtable $Attributes
+
+    New-HTMLTag -Tag 'iframe' -Attributes $Attributes {
+
+    }
+    if ($UseiFrameResizer) {
+        $InlineScript = @{
+            log = $EnableLogging.IsPresent
+        }
+        $InlineScriptBody = $InlineScript | ConvertTo-Json -Depth 100
+        $Script = New-HTMLTag -Tag 'script' -Value {
+            # Convert Dictionary to JSON and return chart within SCRIPT tag
+            # Make sure to return with additional empty string
+
+            "iFrameResize($InlineScriptBody, '#$Id')"
+
+        } -NewLine
+        $Script
+    }
+}
