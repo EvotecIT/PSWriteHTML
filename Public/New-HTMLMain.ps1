7df2deda442f00cc236ecc5b9dd1ca5d028bc081
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-01 19:06:08 +0100

    Added docs for this parameter

--- Public/New-HTMLMain.ps1
+++ Public/New-HTMLMain.ps1
@@ -1,4 +1,95 @@
 ﻿function New-HTMLMain {
+    <#
+    .SYNOPSIS
+    Defines the body HTML content. By default this is not required, but can be useful when header and footer are used to explicitly define the main content.
+
+    .DESCRIPTION
+    Defines the body HTML content. By default this is not required, but can be useful when header and footer are used to explicitly define the main content.
+
+    .PARAMETER HTMLContent
+    Provides ability to specify one or more elements within HTML. Using New-HTMLMain without it, makes no larger sense, as the content will be empty.
+
+    .PARAMETER BackgroundColor
+    Define a background color of the body element. You can choose from 800 defined colors or provide your own hex color
+
+    .PARAMETER Color
+    Choose a color of the body element. You can choose from 800 defined colors or provide your own hex color
+
+    .PARAMETER FontFamily
+    Choose a FontFamily for the body content
+
+    .PARAMETER FontSize
+    Choose a FontSize for the body content
+
+    .EXAMPLE
+    New-HTML -TitleText 'My title' -Online -FilePath $PSScriptRoot\Example40-Body.html -Show {
+        New-HTMLMain {
+            New-HTMLTabStyle -SlimTabs `
+                -BorderBottomStyleActive solid -BorderBottomColorActive LightSkyBlue -BackgroundColorActive none `
+                -TextColorActive Black -Align left -BorderRadius 0px -RemoveShadow -TextColor Grey -TextTransform capitalize #-FontSize 10pt
+
+            New-HTMLSectionStyle -BorderRadius 0px -HeaderBackGroundColor Grey
+            New-HTMLTab -Name 'First Level Tab - Test 1' -IconBrands acquisitions-incorporated {
+                New-HTMLTab -Name '2nd Level Tab - Test 4/1' -IconBrands app-store {
+                    New-HTMLSection -HeaderText 'Default Section Style' {
+                        New-HTMLTableStyle -Type Header -TextAlign right -TextColor Blue
+                        New-HTMLTableStyle -Type Row -TextAlign left -TextColor Grey
+                        New-HTMLTable -DataTable $Test1 {
+                            New-HTMLTableHeader -Names 'ID', 'HandleCount'
+                        } -Filtering #-HideFooter -FilteringLocation Both
+                    } -CanCollapse
+                }
+            }
+            New-HTMLTab -Name 'Next' {
+
+            }
+        } -BackgroundColor Yellow -Color Red -FontSize 12px #-FontFamily 'Arial'
+    }
+    .EXAMPLE
+    New-HTML -TitleText 'This is a test' -FilePath "$PSScriptRoot\Example34_01.html" {
+        New-HTMLHeader {
+            New-HTMLText -Text "Date of this report $(Get-Date)" -Color Blue -Alignment right
+        }
+        New-HTMLMain {
+            New-HTMLTab -TabName 'Test' {
+                New-HTMLSection -HeaderText '0 section' {
+                    New-HTMLPanel {
+                        New-HTMLTable -DataTable $Processes -HideFooter
+                    }
+                    New-HTMLPanel {
+                        New-HTMLTable -DataTable $Processes -HideFooter
+                    }
+                    New-HTMLPanel {
+                        New-HTMLTable -DataTable $Processes -HideFooter -Simplify
+                    }
+                    New-HTMLPanel {
+                        New-HTMLTable -DataTable $Processes -HideFooter
+                    }
+                }
+            }
+            New-HTMLTab -TabName 'Test5' {
+                New-HTMLSection -HeaderText '1 section' {
+                    New-HTMLPanel {
+                        New-HTMLTable -DataTable $Processes -HideFooter
+                    }
+                    New-HTMLPanel {
+                        New-HTMLTable -DataTable $Processes -HideFooter
+                        # New-HTMLTable -DataTable $Processes -HideFooter
+                    }
+                    New-HTMLPanel {
+                        New-HTMLTable -DataTable $Processes -HideFooter
+                    }
+                }
+            }
+        }
+        New-HTMLFooter {
+            New-HTMLText -Text "Date of this report $(Get-Date)" -Color Blue -Alignment right
+        }
+    } -Online -ShowHTML
+
+    .NOTES
+    General notes
+    #>
     [alias('Main')]
     [CmdletBinding()]
     param(
