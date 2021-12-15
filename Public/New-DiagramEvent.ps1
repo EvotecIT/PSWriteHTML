a3cfc45dac15ac54651078f8a8bb585f9519c5e0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2021-12-15 17:17:38 +0100

    Added doc

--- Public/New-DiagramEvent.ps1
+++ Public/New-DiagramEvent.ps1
@@ -1,4 +1,41 @@
 ﻿function New-DiagramEvent {
+    <#
+    .SYNOPSIS
+    Allows to connect Diagrams with Tables using Events.
+
+    .DESCRIPTION
+    Allows to connect Diagrams with Tables using Events.
+
+    .PARAMETER ID
+    ID of the table to connect with the diagram.
+
+    .PARAMETER ColumnID
+    Column Number of the table to connect with the diagram.
+
+    .EXAMPLE
+    $Processes = Get-Process | Select-Object -First 3 -Property Name, ProcessName, Id, FileVersion, WorkingSet
+    $TableID = 'RandomID'
+
+    New-HTML -TitleText 'My Title' -Online -FilePath $PSScriptRoot\Example30-LinkedProcesses.html -ShowHTML {
+        New-HTMLSection -Invisible {
+            New-HTMLPanel {
+                New-HTMLTable -DataTable $Processes -DataTableID $TableID
+            }
+            New-HTMLPanel {
+                New-HTMLDiagram {
+                    New-DiagramEvent -ID $TableID -ColumnID 2
+                    New-DiagramNode -Label 'Processes' -IconBrands delicious
+                    foreach ($_ in $Processes) {
+                        New-DiagramNode -Label $_.ProcessName -Id $_.Id -To 'Processes'
+                    }
+                }
+            }
+        }
+    }
+
+    .NOTES
+    General notes
+    #>
     [CmdletBinding()]
     param(
         #[switch] $FadeSearch,
