a0a1dc5d507c9b346f63d5912d1ef32f23b22afb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-01-21 18:54:28 +0100

    Added css/js

---
+++ Resources/JS/accordionSummary.js
@@ -0,0 +1,3 @@
+﻿//hint: hold shift while opening to keep more than 1 open
+let ds = [...document.querySelectorAll('details')];
+ds.forEach(d => d.addEventListener('click', e => e.shiftKey || ds.filter(i => i != d).forEach(i => i.removeAttribute('open'))))
---
+++ Resources/JS/sectionScrolling.js
@@ -0,0 +1,17 @@
+﻿window.addEventListener('DOMContentLoaded', () => {
+    const observer = new IntersectionObserver(entries => {
+        entries.forEach(entry => {
+            const id = entry.target.getAttribute('id');
+            if (entry.intersectionRatio > 0) {
+                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.add('active');
+            } else {
+                document.querySelector(`nav li a[href="#${id}"]`).parentElement.classList.remove('active');
+            }
+        });
+    });
+
+    // Track all sections that have an `id` applied
+    document.querySelectorAll('section[id]').forEach((section) => {
+        observer.observe(section);
+    });
+});
