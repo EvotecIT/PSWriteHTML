8f14b5550e12e974593782949cb02ded83c215f7
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-01-21 18:59:34 +0100

    Example for icons added

---
+++ Examples/Example44-FontIcon/Example44-FontIcon.ps1
@@ -0,0 +1,9 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTMLFontIcon -IconSolid ad
+
+New-HTMLFontIcon -IconSolid ad -AsCSS -Name '.className'
+
+New-HTMLFontIcon -IconSolid ad -AsCSS
+
+New-HTMLFontIcon -IconSolid ad -AsCSS -AsHashTable
