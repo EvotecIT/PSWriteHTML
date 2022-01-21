eefbe72a45ac5763ac80683fac7824b97d8452ab
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-01-21 18:59:20 +0100

    work in progress

---
+++ Public/New-HTMLSectionScrolling.ps1
@@ -0,0 +1,30 @@
+﻿function New-HTMLSectionScrolling {
+    [cmdletBinding()]
+    param(
+        [scriptblock]$Content
+    )
+    #Enable-HTMLFeature -Feature MainFlex, AccordionSummary
+    Enable-HTMLFeature -Feature SectionScrolling
+
+    if ($Content) {
+        $ContentExecuted = & $Content
+
+        $HTMLList = [System.Collections.Generic.List[string]]::new()
+        $HTMLData = foreach ($C in $ContentExecuted) {
+            if ($C -is [PSCustomObject] -and $C.Type -eq 'SectionScrollingItem') {
+                $C.HTMLOutput
+                $HTMLList.Add($C.ListEntry)
+            }
+        }
+        New-HTMLTag -Tag 'div' {
+            New-HTMLTag -Tag 'div' {
+                $HTMLData
+            }
+            New-HTMLTag -Tag 'nav' {
+                New-HTMLTag -Tag 'ol' {
+                    $HTMLList
+                }
+            } -Attributes @{ class = 'section-nav' }
+        } -Attributes @{ class = 'sectionScrolling' }
+    }
+}
---
+++ Public/New-HTMLSectionScrollingItem.ps1
@@ -0,0 +1,59 @@
+﻿function New-HTMLSectionScrollingItem {
+    [cmdletBinding()]
+    param(
+        [scriptblock] $Content,
+        [string] $SectionTitle
+    )
+
+    $Name = "scrolling-$(Get-RandomStringName -Size 8 -LettersOnly)"
+
+
+
+    if (-not $Script:HTMLSectionScrollingItem) {
+        $HTMLOutput = New-HTMLTag -Tag 'section' {
+            New-HTMLTag -Tag 'h2' {
+                $SectionTitle
+            }
+            New-HTMLTag -Tag 'div' {
+                $Script:HTMLSectionScrollingItem = $true
+                if ($Content) {
+                    & $Content
+                }
+                $Script:HTMLSectionScrollingItem = $false
+            }
+        } -Attributes @{ 'id' = $Name }
+        $Level = 0
+
+        $ListEntry = New-HTMLTag -Tag 'li' {
+            New-HTMLTag -Tag 'a' {
+                $SectionTitle
+            } -Attributes @{ href = "#$Name" }
+        }
+    } else {
+        $Link
+        New-HTMLTag -Tag 'section' {
+            if ($Content) {
+                & $Content
+            }
+            $Script:HTMLSectionScrollingItem = $false
+        } -Attributes @{ 'id' = $Name }
+        $Level = 1
+
+        $ListEntry = New-HTMLTag -Tag 'li' {
+            New-HTMLTag -Tag 'a' {
+                $SectionTitle
+            } -Attributes @{ href = "#$Name" }
+            New-HTMLTag -Tag 'ul' {
+
+            }
+        }
+
+    }
+    [PSCustomObject] @{
+        Type       = 'SectionScrollingItem'
+        HTMLOutput = $HTMLOutput
+        ListEntry  = $ListEntry
+        Name       = $Name
+        Level      = $Level
+    }
+}
