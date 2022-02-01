366cea818863c5469530f3202a0bcfbbc392addb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-02-01 17:36:23 +0100

    Example

--- Examples/Example-DashimoStyle/Run-SimplestTable01-DefaultSort.ps1
+++ Examples/Example-DashimoStyle/Run-SimplestTable01-DefaultSort.ps1
@@ -1,11 +1,23 @@
 Import-Module .\PSWriteHTML.psd1 -Force
 
-$Process = Get-Process | Select-Object -First 30
+$PRocess = @(
+    [PSCustomObject] @{ PRI = 1; ID =	7; Text = "Placeholder Text" }
+    [PSCustomObject] @{ PRI = 1; ID =	5; Text = "Placeholder Text" }
+    [PSCustomObject] @{ PRI = 2; ID =	4; Text = "Placeholder Text" }
+    [PSCustomObject] @{ PRI = 2; ID =	2; Text = "Placeholder Text" }
+    [PSCustomObject] @{ PRI = 3; ID =	1; Text = "Placeholder Text" }
+    [PSCustomObject] @{ PRI = 3; ID =	6; Text = "Placeholder Text" }
+    [PSCustomObject] @{ PRI = 3; ID =	3; Text = "Placeholder Text" }
+)
 
-Dashboard -Name 'Dashimo Test' -FilePath $PSScriptRoot\Output\DashboardEasy01.html -AutoRefresh 15 -Show {
-    Table -DataTable $Process -DefaultSortIndex 4 -ScrollCollapse -HideFooter -Buttons @()
+Dashboard -Name 'Dashimo Test' -FilePath $PSScriptRoot\DashboardEasy01.html -Show {
+    #Table -DataTable $Process -DefaultSortIndex 2 -ScrollCollapse -HideFooter -Buttons @()
 
-    Table -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -HideFooter -Buttons @()
+    #Table -DataTable $Process -DefaultSortColumn 'PRI' -ScrollCollapse -HideFooter -Buttons @()
 
-    Table -DataTable $Process -DefaultSortColumn 'WorkingSet' -DefaultSortOrder Descending -ScrollCollapse -HideFooter -Buttons @()
-}
+    #Table -DataTable $Process -DefaultSortColumn 'ID' -DefaultSortOrder Descending -ScrollCollapse -HideFooter -Buttons @()
+
+    Table -DataTable $Process -DefaultSortColumn 'PRI', 'Id' -DefaultSortOrder Descending, Ascending -ScrollCollapse -HideFooter -Buttons @()
+
+    Table -DataTable $Process -DefaultSortColumn 'PRI', 'Id' -DefaultSortOrder Descending, Descending -ScrollCollapse -HideFooter -Buttons @()
+} -Online
