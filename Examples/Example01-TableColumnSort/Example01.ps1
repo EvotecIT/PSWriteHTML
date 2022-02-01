fb63dd169c49b69cf1937618377d2e3a9fc36b0c
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-02-01 17:37:38 +0100

    examples

--- Examples/Example01-TableColumnSort/Example01.ps1
+++ Examples/Example01-TableColumnSort/Example01.ps1
@@ -1,12 +1,18 @@
 ﻿Import-Module .\PSWriteHTML.psd1 -Force
 
-$Process = Get-Process | Select-Object -First 30
-$ProcessSmaller = Get-Process | Select-Object -First 15
+$Process = Get-Process | Select-Object -First 3
+$ProcessSmaller = Get-Process | Select-Object -First 5
 
 New-HTML -Name 'Dashimo Test' -FilePath $PSScriptRoot\Example01.html -AutoRefresh 15 -Show {
     New-HTMLTable -DataTable $ProcessSmaller -DefaultSortIndex 4 -ScrollCollapse -HideFooter -Buttons @()
 
-    New-HTMLTable -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -HideFooter -Buttons @()
+   # New-HTMLTable -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -HideFooter -Buttons @()
 
-    New-HTMLTable -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -DefaultSortOrder Descending -HideFooter -Buttons @()
+  #  New-HTMLTable -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -DefaultSortOrder Descending -HideFooter -Buttons @()
+
+    New-HTMLTable -DataTable $ProcessSmaller -DefaultSortIndex 4 -ScrollCollapse -HideFooter -Buttons @() -hideHeader
+
+#    New-HTMLTable -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -HideFooter -Buttons @() -hideHeader
+
+ #   New-HTMLTable -DataTable $Process -DefaultSortColumn 'WorkingSet' -ScrollCollapse -DefaultSortOrder Descending -HideFooter -Buttons @() -hideHeader
 }
--- Examples/Example29-Logo/Example29-01.ps1
+++ Examples/Example29-Logo/Example29-01.ps1
@@ -1,8 +1,15 @@
 ﻿Import-Module .\PSWriteHTML.psd1 -Force
 
 New-HTML -TitleText 'This is a test' -FilePath "$PSScriptRoot\Example29-01.html" {
-    New-HTMLLogo
+    #New-HTMLHeader {
+    #    New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Class 'otehr' -Width '100%'
+    #}
+    New-HTMLHeader {
+        New-HTMLPanel -Invisible {
+            New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Width '50%'
+        } -AlignContentText right
+    }
     New-HTMLTab -TabName 'Dashboard' {
         New-HTMLTable -DataTable (Get-Process | Select-Object -First 2)
     }
-} -ShowHTML #-Online
+} -ShowHTML -Online
