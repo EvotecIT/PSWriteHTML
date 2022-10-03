ac093bdcb912a8c7d54b58f7e1a9584d18431793
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-10-03 22:10:09 +0200

    Improvements to layout for mails

--- Public/EmailLayoutColumn.ps1
+++ Public/EmailLayoutColumn.ps1
@@ -3,13 +3,51 @@
     param(
         [ScriptBlock] $ColumnLayout,
         [string] $Width,
-        [string] $Alignment
+        [string] $Alignment,
+        [string] $Padding,
+        [string] $PaddingTop,
+        [string] $PaddingRight,
+        [string] $PaddingBottom,
+        [string] $PaddingLeft,
+        [Parameter()][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderTopStyle,
+        [Parameter()][string] $BorderTopColor,
+        [Parameter()][string] $BorderTopWidthSize = 0,
+        [Parameter()][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderBottomStyle,
+        [Parameter()][string] $BorderBottomColor,
+        [Parameter()][string] $BorderBottomWidthSize = 0,
+        [Parameter()][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderLeftStyle,
+        [Parameter()][string] $BorderLeftColor,
+        [Parameter()][string] $BorderLeftWidthSize = 0,
+        [Parameter()][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderRightStyle,
+        [Parameter()][string] $BorderRightColor,
+        [Parameter()][string] $BorderRightWidthSize = 0
     )
 
     if ($ColumnLayout) {
         $Output = & $ColumnLayout
-        $HTMLWidth = ConvertFrom-Size -Size $Width
-        New-HTMLTag -Tag 'td' -Attributes @{ class = 'layoutTableColumn'; width = $HTMLWidth; align = $Alignment; style = @{ 'max-width' = $HTMLWidth; "text-align" = $Alignment } } {
+
+        $Style = @{
+            'padding'             = ConvertFrom-Size -Size $Padding
+            'padding-top'         = ConvertFrom-Size -Size $PaddingTop
+            'padding-right'       = ConvertFrom-Size -Size $PaddingRight
+            'padding-bottom'      = ConvertFrom-Size -Size $PaddingBottom
+            'padding-left'        = ConvertFrom-Size -Size $PaddingLeft
+            'max-width'           = ConvertFrom-Size -Size $Width
+            "text-align"          = $Alignment
+            'border-top-width'    = $BorderTopWidthSize
+            'border-bottom-width' = $BorderBottomWidthSize
+            'border-left-width'   = $BorderLeftWidthSize
+            'border-right-width'  = $BorderRightWidthSize
+            'border-top-color'    = $BorderTopColor
+            'border-bottom-color' = $BorderBottomColor
+            'border-left-color'   = $BorderLeftColor
+            'border-right-color'  = $BorderRightColor
+            'border-top-style'    = $BorderTopStyle
+            'border-bottom-style' = $BorderBottomStyle
+            'border-left-style'   = $BorderLeftStyle
+            'border-right-style'  = $BorderRightStyle
+        }
+        New-HTMLTag -Tag 'td' -Attributes @{ class = 'layoutTableColumn'; width = $HTMLWidth; align = $Alignment; style = $Style } {
             $Output
         }
     }
--- Public/EmailLayoutRow.ps1
+++ Public/EmailLayoutRow.ps1
@@ -2,15 +2,42 @@
     [cmdletBinding()]
     param(
         [ScriptBlock] $RowLayout,
-        [string] $Height
+        [string] $Height,
+        [Parameter()][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderTopStyle,
+        [Parameter()][string] $BorderTopColor,
+        [Parameter()][string] $BorderTopWidthSize,
+        [Parameter()][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderBottomStyle,
+        [Parameter()][string] $BorderBottomColor,
+        [Parameter()][string] $BorderBottomWidthSize,
+        [Parameter()][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderLeftStyle,
+        [Parameter()][string] $BorderLeftColor,
+        [Parameter()][string] $BorderLeftWidthSize,
+        [Parameter()][ValidateSet('none', 'hidden', 'dotted', 'dashed', 'solid', 'double', 'groove', 'ridge', 'inset', 'outset')] $BorderRightStyle,
+        [Parameter()][string] $BorderRightColor,
+        [Parameter()][string] $BorderRightWidthSize
     )
     if ($RowLayout) {
         $Output = & $RowLayout
 
         $HTMLHeight = ConvertFrom-Size -Size $Height
+        $Style = @{
+            'height'              = $HTMLHeight
+            'border-top-width'    = $BorderTopWidthSize
+            'border-bottom-width' = $BorderBottomWidthSize
+            'border-left-width'   = $BorderLeftWidthSize
+            'border-right-width'  = $BorderRightWidthSize
+            'border-top-color'    = $BorderTopColor
+            'border-bottom-color' = $BorderBottomColor
+            'border-left-color'   = $BorderLeftColor
+            'border-right-color'  = $BorderRightColor
+            'border-top-style'    = $BorderTopStyle
+            'border-bottom-style' = $BorderBottomStyle
+            'border-left-style'   = $BorderLeftStyle
+            'border-right-style'  = $BorderRightStyle
+        }
         New-HTMLTag -Tag 'table' -Attributes @{ class = 'layoutTable' } {
             New-HTMLTag -Tag 'tbody' {
-                New-HTMLTag -Tag 'tr' -Attributes @{class = 'layoutTableRow'; style = @{ 'height' = $HTMLHeight } } {
+                New-HTMLTag -Tag 'tr' -Attributes @{class = 'layoutTableRow'; style = $Style } {
                     $Output
                 }
             }
