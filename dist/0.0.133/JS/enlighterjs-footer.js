66ff5c2c5648c460e4d60aa4520469e72fbcd141
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2022-10-03 18:32:20 +0200

    Removed unused code (I think)

--- dist/0.0.133/JS/enlighterjs-footer.js
+++
@@ -1,5 +0,0 @@
-EnlighterJS.init("pre", "code", {
-    language: "powershell", // - use javascript as default language
-    theme: "enlighter", // - use theme "enlighter" as default theme
-    indent: 2 // - replace tabs with 2 spaces
-});
