db99e5ea424858cbf980a5c5563c4f28128e88e0
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-24 21:18:54 +0100

    Smalll example

---
+++ Examples/Example-Charts/Example-ChartsPie.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+# Define HTML
+New-HTML {
+    New-HTMLSection {
+        New-HTMLChart {
+            New-ChartPie -Name 'Pie Part 1' -Value 20
+            New-ChartPie -Name 'Pie Part 2' -Value 30
+            New-ChartPie -Name 'Pie Part 3' -Value 40
+        }
+    }
+} -ShowHTML -FilePath $PSScriptRoot\Example-ChartsPie.html -Online
