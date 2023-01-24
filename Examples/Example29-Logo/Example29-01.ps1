710b9f18885374f48750daffde8d44f86a6cc5c2
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-01-24 21:20:26 +0100

    Imrpove example

--- Examples/Example29-Logo/Example29-01.ps1
+++ Examples/Example29-Logo/Example29-01.ps1
@@ -1,15 +1,27 @@
 ﻿Import-Module .\PSWriteHTML.psd1 -Force
 
 New-HTML -TitleText 'This is a test' -FilePath "$PSScriptRoot\Example29-01.html" {
-    #New-HTMLHeader {
-    #    New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Class 'otehr' -Width '100%'
-    #}
     New-HTMLHeader {
-        New-HTMLPanel -Invisible {
-            New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Width '50%'
-        } -AlignContentText right
+        New-HTMLSection -Invisible {
+            New-HTMLPanel -Invisible {
+                New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Class 'otehr' -Width '50%'
+            }
+            New-HTMLPanel -Invisible {
+                New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Width '20%'
+            } -AlignContentText right
+        }
     }
     New-HTMLTab -TabName 'Dashboard' {
         New-HTMLTable -DataTable (Get-Process | Select-Object -First 2)
+    }
+    New-HTMLFooter {
+        New-HTMLSection -Invisible {
+            New-HTMLPanel -Invisible {
+                New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Class 'otehr' -Width '50%'
+            }
+            New-HTMLPanel -Invisible {
+                New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Width '20%'
+            } -AlignContentText right
+        }
     }
 } -ShowHTML -Online
