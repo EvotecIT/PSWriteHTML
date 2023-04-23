ba258532d07f4b41068fc8777aa9d67badcac6c4
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-04-23 09:32:37 +0200

    Add new function for Maps

---
+++ Public/New-MapPlot.ps1
@@ -0,0 +1,33 @@
+﻿function New-MapPlot {
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][string] $Plot,
+        [Parameter(Mandatory)][double] $Latitude,
+        [Parameter(Mandatory)][double] $Longitude,
+        [string] $Href,
+        [Parameter(Mandatory)][string] $Value,
+        [alias('Tooltip')][scriptblock] $TooltipContent
+    )
+
+    $OutputObject = [ordered] @{
+        Type          = 'MapPlot'
+        Configuration = [ordered] @{
+            $Plot = [ordered] @{
+                latitude  = $Latitude
+                longitude = $Longitude
+                href      = $Href
+                value     = $Value
+                tooltip   = @{
+                    content = if ($TooltipContent) {
+                        $ValueExecuted = & $TooltipContent
+                        $ValueExecuted -join ""
+                    } else {
+                        $null
+                    }
+                }
+            }
+        }
+    }
+    Remove-EmptyValue -Hashtable $OutputObject.Configuration -Recursive -Rerun 2
+    $OutputObject
+}
