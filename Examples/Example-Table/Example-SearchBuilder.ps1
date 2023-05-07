3cd404ca5f5c9a04ec99552992b55b4f7e45fb73
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-05-07 23:37:41 +0200

    Update searchbuilder

--- Examples/Example-Table/Example-SearchBuilder.ps1
+++ Examples/Example-Table/Example-SearchBuilder.ps1
@@ -1,10 +1,16 @@
 ﻿Import-Module .\PSWriteHTML.psd1 -Force
 
-$ProcessesAll = Get-Process | Select-Object -First 3
+$ProcessesAll = Get-Process | Select-Object -First 5 #-Property Name, Id, StartTime
 
-New-HTML -TitleText 'Title' -Online -FilePath $PSScriptRoot\Example-TableAdvancedFreeze1.html -ShowHTML {
+New-HTML -TitleText 'Title' -Online -FilePath $PSScriptRoot\Example-SearchBuilder.html -ShowHTML {
+    New-HTMLTableStyle -BackgroundColor Blue -Type RowSelected
+    New-HTMLTableStyle -BackgroundColor Yellow -Type RowHover
+    New-HTMLTableStyle -BackgroundColor Yellow -Type RowHoverSelected
+
     New-HTMLSection -HeaderText 'Search Builder 1' {
-        New-HTMLTable -DataTable $ProcessesAll -SearchBuilder -Buttons excelHtml5, copyHtml5, csvHtml5
+        New-HTMLTable -DataTable $ProcessesAll -SearchBuilder -Buttons excelHtml5, copyHtml5, csvHtml5 {
+
+        }
     }
     New-HTMLSection -HeaderText 'Search Builder as button' {
         New-HTMLTable -DataTable $ProcessesAll
@@ -16,9 +22,9 @@ New-HTML -TitleText 'Title' -Online -FilePath $PSScriptRoot\Example-TableAdvance
     }
 }
 
-New-HTML -TitleText 'Title' -FilePath $PSScriptRoot\Example-TableAdvancedFreeze2.html -ShowHTML {
+New-HTML -TitleText 'Title' -FilePath $PSScriptRoot\Example-SearchBuilder2.html {
     New-HTMLSection -HeaderText 'Search Builder 1' {
-        New-HTMLTable -DataTable $ProcessesAll -SearchBuilder -Buttons excelHtml5, copyHtml5, csvHtml5
+        New-HTMLTable -DataTable $ProcessesAll -Filtering -ScrollX -SearchBuilder -Buttons excelHtml5, copyHtml5, csvHtml5
     }
     New-HTMLSection -HeaderText 'Search Builder as button' {
         New-HTMLTable -DataTable $ProcessesAll
@@ -28,4 +34,4 @@ New-HTML -TitleText 'Title' -FilePath $PSScriptRoot\Example-TableAdvancedFreeze2
         # Search Builder will be disabled, button will work
         New-HTMLTable -DataTable $ProcessesAll -SearchBuilder
     }
-}
+} -ShowHTML
