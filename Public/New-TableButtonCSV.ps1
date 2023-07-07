dfa60b8aebffffe3248c4f181d89eff3cbcaf2a8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-07 22:18:57 +0200

    Add csv features

--- Public/New-TableButtonCSV.ps1
+++ Public/New-TableButtonCSV.ps1
@@ -2,14 +2,29 @@
     [alias('TableButtonCSV', 'EmailTableButtonCSV', 'New-HTMLTableButtonCSV')]
     [CmdletBinding()]
     param(
-        [string] $Title
+        [string] $Title,
+        [string] $ButtonName = 'CSV',
+        [string] $Extension = '.csv',
+        [string] $FileName,
+        [switch] $DisableBOM,
+        [string] $FieldSeparator = ';'
     )
     if (-not $Script:HTMLSchema['TableSimplify']) {
         $Script:HTMLSchema.Features.DataTablesButtons = $true
         $Script:HTMLSchema.Features.DataTablesButtonsHTML5 = $true
     }
-    $Output = [ordered]@{}
-    $Output['extend'] = 'csvHtml5'
+    $Output = [ordered]@{
+        extend         = 'csvHtml5'
+        text           = $ButtonName
+        charset        = 'utf-8'
+        extension      = $Extension
+        fieldSeparator = $FieldSeparator
+        fieldBoundary  = ''
+        bom            = -not $DisableBOM.IsPresent
+    }
+    if ($FileName) {
+        $Output['filename'] = $FileName
+    }
     if ($Title) {
         $Output['title'] = $title
     }
