11a70469496d47c805e5baf431156ea002c92114
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-07 22:18:29 +0200

    Add column visibility

---
+++ Examples/Example-Table/Example-TableMultiColumnLayout.ps1
@@ -0,0 +1,14 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$DataTable = @(
+    [PSCustomObject] @{ 'Test.1' = 'Name'; 'Test 2 Another' = 'Name2'; 'Test..3' = 'Name3'; "Da't'es" = $Date; 'Numbers' = $Int; Enum = $Enum; EnumFruit = $EnumFruit; Bool = $false }
+    [PSCustomObject] @{ 'Test.1' = 'Name'; 'Test 2 Another' = 'Name2'; 'Test..3' = 'Name3'; "Da't'es" = $Date2; 'Numbers' = $Int; Enum = $Enum; EnumFruit = $EnumFruit; Bool = $false }
+    [PSCustomObject] @{ 'Test.1' = 'Przemysłąw'; 'Test 2 Another' = 'Name2'; 'Test..3' = 'Name3'; "Da't'es" = $Date3; 'Numbers' = $Int; Enum = $Enum; EnumFruit = $EnumFruit; Bool = $false }
+    [PSCustomObject] @{ 'Test.1' = 'Kłys'; 'Test 2 Another' = 'Name2'; 'Test..3' = 'Name3'; "Da't'es" = $Date2; 'Numbers' = $Int; Enum = $Enum; EnumFruit = $EnumFruit; Bool = $false }
+)
+
+#$DataTable = Get-Process | Select-Object -First 30
+
+New-HTML -TitleText 'Title' -Online -FilePath $PSScriptRoot\Example-TableMultiColumnLayout.html -ShowHTML {
+    New-HTMLTable -DataTable $DataTable -Filtering -Buttons columnVisibility, csvHtml5, excelHtml5, pageLength, searchBuilder -Fla
+}
---
+++ Public/New-TableButtonColumnVisibility.ps1
@@ -0,0 +1,21 @@
+﻿function New-TableButtonColumnVisibility {
+    [alias('TableButtonColumnVisibility', 'New-HTMLTableButtonColumnVisibility')]
+    [CmdletBinding()]
+    param(
+        [string] $Title
+    )
+    $Script:HTMLSchema.Features.DataTablesButtons = $true
+    $Script:HTMLSchema.Features.DataTablesButtonsColVis = $true
+    $Output = @{}
+    $Output['extend'] = 'colvis'
+    if ($Title) {
+        $Output['title'] = $title
+    }
+    $Output['collectionLayout'] = 'fixed columns'
+    $Output['collectionTitle'] = 'Column visibility control'
+    [PSCustomObject] @{
+        Type   = 'TableButtonColumnVisibility'
+        Output = $Output
+
+    }
+}
