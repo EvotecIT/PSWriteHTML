fb86a772633f20839a138581431ced2bf71ce751
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-08 17:40:17 +0200

    add options

--- Public/New-TableButtonExcel.ps1
+++ Public/New-TableButtonExcel.ps1
@@ -2,13 +2,17 @@
     [alias('TableButtonExcel', 'EmailTableButtonExcel', 'New-HTMLTableButtonExcel')]
     [CmdletBinding()]
     param(
-        [string] $Title
+        [string] $Title,
+        [string] $ButtonName
     )
     $Script:HTMLSchema.Features.DataTablesButtons = $true
     $Script:HTMLSchema.Features.DataTablesButtonsHTML5 = $true
     $Script:HTMLSchema.Features.DataTablesButtonsExcel = $true
     $Output = @{}
     $Output['extend'] = 'excelHtml5'
+    if ($ButtonName) {
+        $Output['text'] = $ButtonName
+    }
     if ($Title) {
         $Output['title'] = $title
     }
