1759a06028c6b76851f19979f3354857c1a95570
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-08 17:40:32 +0200

    add options

--- Public/New-TableButtonPrint.ps1
+++ Public/New-TableButtonPrint.ps1
@@ -2,13 +2,17 @@
     [alias('TableButtonPrint', 'EmailTableButtonPrint', 'New-HTMLTableButtonPrint')]
     [CmdletBinding()]
     param(
-        [string] $Title
+        [string] $Title,
+        [string] $ButtonName
     )
     if (-not $Script:HTMLSchema['TableSimplify']) {
         $Script:HTMLSchema.Features.DataTablesButtons = $true
     }
     $Output = @{}
     $Output['extend'] = 'print'
+    if ($ButtonName) {
+        $Output['text'] = $ButtonName
+    }
     if ($Title) {
         $Output['title'] = $title
     }
