d54077ef67ae6f8ec82b5e6847442b0574470159
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-09 19:57:28 +0200

    update example

--- Examples/Example-Table/Example-TableColumnVisibility.ps1
+++ Examples/Example-Table/Example-TableColumnVisibility.ps1
@@ -7,7 +7,7 @@ $DataTable = @(
     [PSCustomObject] @{ 'Test.1' = 'Kłys'; 'Test 2 Another' = 'Name2'; 'Test..3' = 'Name3'; "Da't'es" = $Date2; 'Numbers' = $Int; Enum = $Enum; EnumFruit = $EnumFruit; Bool = $false }
 )
 
-#$DataTable = Get-Process | Select-Object -First 30
+$DataTable = Get-Process | Select-Object -First 5
 
 New-HTML -TitleText 'Title' -Online -FilePath $PSScriptRoot\Example-TableColumnVisibility.html -ShowHTML {
     New-HTMLTable -DataTable $DataTable -Filtering -Buttons columnVisibility, csvHtml5, excelHtml5, pageLength, searchBuilder
