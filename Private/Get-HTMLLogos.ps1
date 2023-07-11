6e5047268fa1a71b19e25aba9c48344f336e4e83
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-11 07:46:37 +0200

    Removed dead code

--- Private/Get-HTMLLogos.ps1
+++ Private/Get-HTMLLogos.ps1
@@ -20,31 +20,11 @@ Function Get-HTMLLogos {
         } else {
             "$PSScriptRoot\..\Resources\Images\Other"
         }
-        #"$PSScriptRoot\..\Resources\Images\DataTables"
     )
     $ImageFiles = Get-ChildItem -Path (Join-Path $LogoPath '\*') -Include *.jpg, *.png, *.bmp -Recurse
     foreach ($ImageFile in $ImageFiles) {
-        <#
-        if ($ImageFile.Extension -eq '.jpg') {
-            $FileType = 'jpeg'
-        } else {
-            $FileType = $ImageFile.Extension.Replace('.', '')
-        }
-        Write-Verbose "Converting $($ImageFile.FullName) to base64 ($FileType)"
-
-        if ($PSEdition -eq 'Core') {
-            $ImageContent = Get-Content -AsByteStream -LiteralPath $ImageFile.FullName
-        } else {
-            $ImageContent = Get-Content -LiteralPath $ImageFile.FullName -Encoding Byte
-        }
-        #>
         $ImageBinary = Convert-ImageToBinary -ImageFile $ImageFile
-        #$LogoSources.Add($ImageFile.BaseName, "data:image/$FileType;base64," + [Convert]::ToBase64String(($ImageContent)))
         $LogoSources.Add($ImageFile.BaseName, $ImageBinary)
     }
     $LogoSources
 }
-
-
-#$t = 'C:\Support\GitHub\PSWriteHTML\Private\Get-HTMLLogos.ps1'
-#$t -as [System.IO.FileInfo]
