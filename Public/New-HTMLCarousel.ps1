68f67f4ba17df26306ebe250d747b391ab1e7f75
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-07-17 21:23:46 +0200

    add new options

--- Public/New-HTMLCarousel.ps1
+++ Public/New-HTMLCarousel.ps1
@@ -2,14 +2,28 @@
     [cmdletBinding()]
     param(
         [scriptblock] $Slide,
-        [validateSet('Horizontal', 'Vertical')][string] $Mode,
+        [validateSet('Horizontal', 'Vertical')][string] $Mode = 'Horizontal',
         [validateSet('center', 'start', 'end', 'justify')][string] $Align,
         [object] $PerView,
         [validateSet('adaptive', 'equal', 'auto')][string] $Height,
         [string] $Margin,
         [switch] $Loop,
         [switch] $Pagination,
-        [nullable[int]] $StartAt
+        [nullable[int]] $Speed,
+        [nullable[int]] $MoveBy,
+        [nullable[int]] $StartAt,
+        [switch] $MoveOnClick,
+        [alias('Stream')][switch] $AutoPlay,
+        [alias('StreamEvery')][nullable[int]] $AutoPlayEvery,
+        [alias('StreamRewind')][switch] $AutoPlayRewind,
+        [alias('StreamPauseOnFocus')][switch] $AutoPlayPauseOnFocus,
+        [alias('StreamPauseOnHover')][switch] $AutoPlayPauseOnHover,
+        [alias('StreamSyncID')][string] $AutoPlaySyncID,
+        [switch] $DisableArrows,
+        [string[]] $ArrowTemplate,
+        [string] $PaginationTemplate,
+        [switch] $Count,
+        [string] $CountTemplate
     )
     $Script:HTMLSchema.Features.MainFlex = $true
     $Script:HTMLSchema.Features.CarouselKineto = $true
@@ -27,17 +41,82 @@
         height  = $Height
         margin  = $Margin
         startAt = $StartAt
+        moveBy  = $moveBy
+        speed   = $Speed
     }
     if ($Pagination) {
         $Options['pagination'] = $true
     }
+    if ($PaginationTemplate) {
+        if ($PaginationTemplate -eq 'default') {
+            $Options['paginationTemplate'] = $null
+        } elseif ($PaginationTemplate -eq 'index') {
+            $Options['paginationTemplate'] = '{{index}}'
+        } else {
+            $Options['paginationTemplate'] = $PaginationTemplate
+        }
+    }
     if ($Loop) {
         $Options['loop'] = $true
     }
+    if ($MoveOnClick) {
+        $Options['moveOnClick'] = $true
+    }
+    if ($AutoPlay) {
+        $Options['stream'] = $true
+        if ($AutoPlayEvery) {
+            $Options['streamEvery'] = $AutoPlayEvery
+        }
+        if ($AutoPlayRewind) {
+            $Options['streamRewind'] = $true
+        }
+        if ($AutoPlayPauseOnFocus) {
+            $Options['pauseOnFocus'] = $true
+        }
+        if ($AutoPlayPauseOnHover) {
+            $Options['pauseOnHover'] = $true
+        }
+        if ($AutoPlaySyncID) {
+            $Options['syncId'] = $AutoPlaySyncID
+        }
+    }
+    if ($DisableArrows) {
+        $Options['arrows'] = $false
+    }
 
+    if ($ArrowTemplate) {
+        $Options['arrows'] = $true
+        if ($ArrowTemplate.Count -eq 1) {
+            if ($ArrowTemplate -eq 'default') {
+                $ArrowTemplate = $null
+            } elseif ($ArrowTemplate -eq 'arrows') {
+                $Options['arrowTemplate'] = @('←', '→')
+            } else {
+                $Options['arrowTemplate'] = $ArrowTemplate
+            }
+        } else {
+            $Options['arrowTemplate'] = $ArrowTemplate
+        }
+    }
+    if ($Count) {
+        $Options['count'] = $true
+
+        if ($CountTemplate) {
+            if ($CountTemplate -eq 'default') {
+                $CountTemplate = $null
+            } elseif ($CountTemplate -eq 'count') {
+                $CountTemplate = '{current}/{total}'
+            } elseif ($CountTemplate -eq 'countBold') {
+                $CountTemplate = '<em class="current">{{current}}</em> / {{total}}'
+            } else {
+                $CountTemplate = $CountTemplate
+            }
+            $Options['countTemplate'] = $CountTemplate
+        }
+    }
 
     Remove-EmptyValue -Hashtable $Options
-    $Carousel = $Options | ConvertTo-JsonLiteral -Depth 0 -AdvancedReplace @{ '.' = '\.'; '$' = '\$' }
+    $Carousel = $Options | ConvertTo-JsonLiteral -Depth 1 -AdvancedReplace @{ '.' = '\.'; '$' = '\$' }
 
     New-HTMLTag -Tag 'div' -Attributes @{ ID = $CarouselID; class = "carousel" } {
         if ($Slide) {
@@ -50,6 +129,19 @@
 
     $Script:HTMLSchema.Carousel.Add($ScriptOutput)
 }
+
+
+[scriptblock] $AutoCompleterCountTemplate = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    'default', 'count', 'countBold' | Sort-Object | Where-Object { $_ -like "*$wordToComplete*" }
+}
+[scriptblock] $AutoCompleterArrowTemplate = {
+    param($commandName, $parameterName, $wordToComplete, $commandAst, $fakeBoundParameters)
+    'default', 'arrows' | Sort-Object | Where-Object { $_ -like "*$wordToComplete*" }
+}
+
+Register-ArgumentCompleter -CommandName New-HTMLCarousel -ParameterName CountTemplate -ScriptBlock $AutoCompleterCountTemplate
+Register-ArgumentCompleter -CommandName New-HTMLCarousel -ParameterName ArrowTemplate -ScriptBlock $AutoCompleterArrowTemplate
 
 <#
 {
