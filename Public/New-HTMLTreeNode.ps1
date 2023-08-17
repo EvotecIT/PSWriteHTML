1cea5521c5ea200bb6755d40ac9ca7536002806f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-08-17 14:01:02 +0200

    Rename function Improve function

---
+++ Public/New-HTMLTreeNode.ps1
@@ -0,0 +1,61 @@
+﻿function New-HTMLTreeNode {
+    [CmdletBinding()]
+    param(
+        [scriptblock] $Children,
+        [string] $Title,
+        [string] $Id,
+        [switch] $Folder,
+        [string] $Tooltip,
+        [string] $IconTooltip,
+        [switch] $IsSelected,
+        [alias('Expanded')][switch] $IsExpanded,
+        [switch] $Unselectable,
+        [ValidateSet('none', 'checkbox', 'radio')][string] $Checkbox,
+        [alias('Url', 'Link', 'UrlLink', 'Href')][string] $HrefLink,
+        [string] $Target # "_blank|_self|_parent|_top|framename"
+    )
+
+    if ($Children) {
+        [Array] $SourceChildren = & $Children
+    }
+
+    $Node = [ordered] @{
+        title       = $Title
+        key         = $Id
+        tooltip     = $Tooltip
+        iconTooltip = $IconTooltip
+    }
+    if ($Checkbox -eq 'radio') {
+        $Node['checkbox'] = 'radio'
+    } elseif ($Checkbox -eq 'checkbox') {
+        $Node['checkbox'] = $true
+    }
+    if ($Folder) {
+        $Node['folder'] = $true
+    }
+    if ($IsSelected.IsPresent) {
+        $Node['selected'] = $true
+    }
+    if ($IsExpanded.IsPresent) {
+        $Node['expanded'] = $true
+    }
+    if ($SourceChildren.Count) {
+        $Node['children'] = $SourceChildren
+    }
+    if ($Unselectable.IsPresent) {
+        $Node['unselectable'] = $true
+    }
+    if ($HrefLink -and $Target) {
+        $Node['data'] = [ordered] @{
+            href   = $HrefLink
+            target = $Target
+        }
+    } elseif ($Href) {
+        $Node['data'] = [ordered] @{
+            href   = $HrefLink
+            target = "_blank"
+        }
+    }
+    Remove-EmptyValue -Hashtable $Node
+    $Node
+}
