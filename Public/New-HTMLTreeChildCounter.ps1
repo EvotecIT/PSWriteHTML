aa3d6b7589ec19a1b133755c28278d94c9b82d8a
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-08-17 14:02:59 +0200

    Add configuration for child counter

---
+++ Public/New-HTMLTreeChildCounter.ps1
@@ -0,0 +1,22 @@
+﻿function New-HTMLTreeChildCounter {
+    [CmdletBinding()]
+    param(
+        [switch] $Deep,
+        [switch] $HideZero,
+        [switch] $HideExpanded
+    )
+
+
+    $Counter = [ordered] @{
+        Type      = 'ChildCounter'
+        Settings  = [ordered] @{
+            childcounter = @{
+                deep         = $Deep.IsPresent
+                hideZeros    = $HideZero.IsPresent
+                hideExpanded = $HideExpanded.IsPresent
+            }
+        }
+        Extension = 'childcounter'
+    }
+    $Counter
+}
