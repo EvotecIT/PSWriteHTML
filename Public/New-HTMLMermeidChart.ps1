156614b996698f5f559c76d029f51db4e492cbd6
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-20 09:35:31 +0200

    add basic mermeid

---
+++ Public/New-HTMLMermeidChart.ps1
@@ -0,0 +1,20 @@
+﻿function New-HTMLMermeidChart {
+    [alias('New-HTMLMermeid')]
+    [CmdletBinding()]
+    param(
+        [scriptblock] $Markdown
+    )
+
+    $Script:HTMLSchema.Features.Mermaid = $true
+
+    $Data = & $Markdown
+    if ($Data -is [string]) {
+        $Data = $Data
+    } else {
+        $Data = $Data -join "`r`n"
+    }
+
+    New-HTMLTag -Tag 'div' -Attributes @{ class = 'mermaid' } {
+        $Data
+    }
+}
