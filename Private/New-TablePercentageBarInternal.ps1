5dc47fd11d28c2e5b6dd145cdf34e3074ca9dfa9
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2023-09-20 09:33:50 +0200

    Add new feature

--- Private/New-TablePercentageBarInternal.ps1
+++ Private/New-TablePercentageBarInternal.ps1
@@ -9,11 +9,20 @@
         [ValidateSet('solid', 'outset', 'groove', 'ridge')][string] $BorderStyle,
         [string] $BarColor,
         [string] $BackgroundColor,
-        [int] $RoundValue
+        [int] $RoundValue,
+        [Array] $ConditionalFormatting
     )
 
-    [ordered]@{
-        targets = $ColumnID
-        render  = "`$.fn.dataTable.render.percentBar('$Type','$TextColor', '$BorderColor', '$BarColor', '$BackgroundColor', $RoundValue, '$BorderStyle')"
+    if ($ConditionalFormatting) {
+        $JsonConditions = $ConditionalFormatting | ConvertTo-Json
+        [ordered]@{
+            targets = $ColumnID
+            render  = "`$.fn.dataTable.render.percentBar('$Type','$TextColor', '$BorderColor', '$BarColor', '$BackgroundColor', $RoundValue, '$BorderStyle', $JsonConditions)"
+        }
+    } else {
+        [ordered]@{
+            targets = $ColumnID
+            render  = "`$.fn.dataTable.render.percentBar('$Type','$TextColor', '$BorderColor', '$BarColor', '$BackgroundColor', $RoundValue, '$BorderStyle')"
+        }
     }
 }
