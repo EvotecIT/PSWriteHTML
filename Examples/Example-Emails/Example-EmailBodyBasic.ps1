f0cb081efdd3fc0f26ec142401b9ed7e8ae6ca24
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-02-07 19:55:11 +0100

    Basic example

---
+++ Examples/Example-Emails/Example-EmailBodyBasic.ps1
@@ -0,0 +1,10 @@
+﻿$Table = (Get-Process | Select-Object -First 2 -Property Name, BasePriority, Company, CompanyName)
+
+$Body = EmailBody -FontSize 12px -FontFamily 'Tahoma' {
+    EmailText -Text 'This should be font 8pt, table should also be font 8pt' -FontSize 8pt
+    EmailTable -DataTable $Table -HideFooter {
+        EmailTableCondition -Name 'Name' -Operator eq -Value 'svchost' -BackgroundColor TeaGreen -FailBackgroundColor Salmon -Inline
+    }
+}
+
+Send-MailMessage -Body $Body
