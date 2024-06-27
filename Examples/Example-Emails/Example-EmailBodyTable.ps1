dcbd8e26ccd228d5ad2ad15758183ec2e6dc549f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-27 18:56:44 +0200

    Additional example

---
+++ Examples/Example-Emails/Example-EmailBodyTable.ps1
@@ -0,0 +1,35 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Table = @(
+    [PSCustomObject] @{
+        Name         = 'Process1'
+        BasePriority = 1
+        Company      = 'Company1'
+        CompanyName  = 'Company Name 1', "Teest"
+    }
+    [PSCustomObject] @{
+        Name         = 'Process1'
+        BasePriority = 1
+        Company      = 'Company1'
+        CompanyName  = @('Company Name 1', "Teest")
+    }
+    [PSCustomObject] @{
+        Name         = 'Process1'
+        BasePriority = 1
+        Company      = 'Company1'
+        CompanyName  = "Teest"
+    }
+)
+
+$Output = EmailBody -FontSize 15px -FontFamily 'Tahoma' {
+    EmailText -Text 'This should be font 8pt, table should also be font 8pt'
+    EmailTable -Table $Table -PrettifyObject #-PrettifyObjectSeparator " - "
+    EmailText -LineBreak
+
+    EmailTextBox -FontFamily 'Calibri' -Size 17 -TextDecoration underline -Color DarkSalmon -Alignment center {
+        'Demonstration'
+    }
+    EmailText -LineBreak
+}
+
+Save-HTML -ShowHTML -HTML $Output
