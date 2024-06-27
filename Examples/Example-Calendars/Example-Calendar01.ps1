006c76e3dc39da08b18d45cc125a351101abaf10
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-06-27 19:20:17 +0200

    Update example

--- Examples/Example-Calendars/Example-Calendar01.ps1
+++ Examples/Example-Calendars/Example-Calendar01.ps1
@@ -2,7 +2,7 @@
 
 $ProcessSmaller = Get-Process | Select-Object -First 1
 
-New-HTML -Name 'Test' -FilePath "$PSScriptRoot\Example-Calendar01.html" -Show {
+New-HTML -Name 'Test' -FilePath "$PSScriptRoot\Example-Calendar01.html" -Show -Online {
     New-HTMLSection {
         New-HTMLSection -HeaderText 'Test 1' {
             New-HTMLTable -DataTable $ProcessSmaller
@@ -10,7 +10,7 @@ New-HTML -Name 'Test' -FilePath "$PSScriptRoot\Example-Calendar01.html" -Show {
         New-HTMLSection -HeaderText 'Test 2' {
             New-HTMLCalendar {
                 New-CalendarEvent -Title 'Active Directory Meeting' -Description 'We will talk about stuff' -StartDate (Get-Date)
-                New-CalendarEvent -Title 'Lunch' -StartDate (Get-Date).AddDays(2).AddHours(-3) -EndDate  (Get-Date).AddDays(3) -Description 'Very long lunch'
+                New-CalendarEvent -Title 'Lunch' -StartDate (Get-Date).AddDays(2).AddHours(-3) -EndDate (Get-Date).AddDays(3) -Description 'Very long lunch'
             }
         }
     }
