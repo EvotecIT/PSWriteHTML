3f519879ed98970f1fb5d3569f0c236d28d7ec79
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-01 09:37:07 +0200

    JS/CSS code to search in diagrams

---
+++ Resources/JS/vis-network.find.js
@@ -0,0 +1,45 @@
+﻿// Example usage:
+// setupSearch(nodes, edges, 'searchInput', 'searchButton'); // Enables typing search and button click
+// setupSearch(nodes, edges, 'searchInput', null, true, 5); // Enables typing search only, with minimum 5 characters
+
+function updateVisibility(searchQuery, nodes, edges) {
+    var filteredNodes = nodes.get({
+        filter: function (item) {
+            return item.label.toLowerCase().includes(searchQuery);
+        }
+    });
+    var filteredEdges = edges.get({
+        filter: function (item) {
+            return filteredNodes.some(node => node.id === item.from || node.id === item.to);
+        }
+    });
+
+    nodes.get().forEach(node => {
+        nodes.update({ id: node.id, hidden: !filteredNodes.some(fNode => fNode.id === node.id) });
+    });
+    edges.get().forEach(edge => {
+        edges.update({ id: edge.id, hidden: !filteredEdges.some(fEdge => fEdge.id === edge.id) });
+    });
+}
+
+function setupSearch(nodes, edges, inputId, buttonId = null, enableTypingSearch = true, minChars = 3) {
+    if (enableTypingSearch) {
+        document.getElementById(inputId).addEventListener('input', function () {
+            if (this.value.length >= minChars) {
+                updateVisibility(this.value.toLowerCase(), nodes, edges);
+            } else {
+                // Optionally, reset visibility when below minChars
+                updateVisibility('', nodes, edges);
+            }
+        });
+    }
+
+    if (buttonId) {
+        document.getElementById(buttonId).addEventListener('click', function () {
+            var searchQuery = document.getElementById(inputId).value;
+            if (searchQuery.length >= minChars) {
+                updateVisibility(searchQuery.toLowerCase(), nodes, edges);
+            }
+        });
+    }
+}
