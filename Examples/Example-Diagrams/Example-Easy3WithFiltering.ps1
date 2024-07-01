57815a3b29ef78056b8b8bd4423cbcac236de254
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-01 11:54:36 +0200

    Rename example file

--- Examples/Example-Diagrams/Example-Easy3WithFiltering.ps1
+++ Examples/Example-Diagrams/Example-Easy3WithFiltering.ps1
@@ -9,7 +9,7 @@ New-HTML -TitleText 'My diagram' -FilePath $PSScriptRoot\Example-Easy4.html {
         New-DiagramNode -Label 'Fourth node' -IconRegular building -IconColor Brown #-IconAsImage
         New-DiagramNode -Label 'Fifth node' -IconSolid align-left -IconColor Brown #-IconAsImage
         New-DiagramNode -Label 'Bart Simson' -Image 'https://cdn.imgbin.com/5/2/1/imgbin-bart-simpson-homer-simpson-supreme-drawing-bart-simpson-bard-simpsons-jAfrtPtYNPTK5rQrcKULn5XBn.jpg' -To 'Second node'
-    } -EnableSearch -MinimumSearchChars 1
+    } -EnableFiltering -MinimumFilteringChars 3
 
     # }
     # New-HTMLTab -Name 'Test2' {
@@ -20,6 +20,6 @@ New-HTML -TitleText 'My diagram' -FilePath $PSScriptRoot\Example-Easy4.html {
         New-DiagramNode -Label 'Fourth node' -IconRegular building -IconColor Brown #-IconAsImage
         New-DiagramNode -Label 'Fifth node' -IconSolid align-left -IconColor Brown #-IconAsImage
         New-DiagramNode -Label 'Bart Simson' -Image 'https://cdn.imgbin.com/5/2/1/imgbin-bart-simpson-homer-simpson-supreme-drawing-bart-simpson-bard-simpsons-jAfrtPtYNPTK5rQrcKULn5XBn.jpg' -To 'Second node'
-    } -EnableSearch -MinimumSearchChars 1
+    } -EnableFiltering -MinimumFilteringChars 1
     #  }
 } -ShowHTML -Online
