28a20ffab2883c1cedac6f2bfbef9abe3d3f95cf
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-07-01 22:27:44 +0200

    Update filtering example

--- Examples/Example-Diagrams/Example-Easy3WithFiltering.ps1
+++ Examples/Example-Diagrams/Example-Easy3WithFiltering.ps1
@@ -1,7 +1,6 @@
 ﻿Import-Module .\PSWriteHTML.psd1 -Force
 
 New-HTML -TitleText 'My diagram' -FilePath $PSScriptRoot\Example-Easy4.html {
-    # New-HTMLTab -Name "Test" {
     New-HTMLDiagram -Height '300px' {
         New-DiagramNode -Label 'One node' -To 'Second node', 'Third node' -ColorBackground Bisque
         New-DiagramNode -Label 'Second node'
@@ -11,8 +10,6 @@ New-HTML -TitleText 'My diagram' -FilePath $PSScriptRoot\Example-Easy4.html {
         New-DiagramNode -Label 'Bart Simson' -Image 'https://cdn.imgbin.com/5/2/1/imgbin-bart-simpson-homer-simpson-supreme-drawing-bart-simpson-bard-simpsons-jAfrtPtYNPTK5rQrcKULn5XBn.jpg' -To 'Second node'
     } -EnableFiltering -MinimumFilteringChars 3
 
-    # }
-    # New-HTMLTab -Name 'Test2' {
     New-HTMLDiagram -Height '300px' {
         New-DiagramNode -Label 'One node' -To 'Second node', 'Third node' -ColorBackground Bisque
         New-DiagramNode -Label 'Second node'
@@ -20,6 +17,5 @@ New-HTML -TitleText 'My diagram' -FilePath $PSScriptRoot\Example-Easy4.html {
         New-DiagramNode -Label 'Fourth node' -IconRegular building -IconColor Brown #-IconAsImage
         New-DiagramNode -Label 'Fifth node' -IconSolid align-left -IconColor Brown #-IconAsImage
         New-DiagramNode -Label 'Bart Simson' -Image 'https://cdn.imgbin.com/5/2/1/imgbin-bart-simpson-homer-simpson-supreme-drawing-bart-simpson-bard-simpsons-jAfrtPtYNPTK5rQrcKULn5XBn.jpg' -To 'Second node'
-    } -EnableFiltering -MinimumFilteringChars 1
-    #  }
+    } -EnableFiltering -MinimumFilteringChars 1 -EnableFilteringButton
 } -ShowHTML -Online
