e6bc619adadb3d02d44322f0ea3267c444c9a9ad
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-08-02 20:42:44 +0200

    Improve example

--- Examples/Example45-ScrollingText/Example45-Scrolling.ps1
+++ Examples/Example45-ScrollingText/Example45-Scrolling.ps1
@@ -1,34 +1,44 @@
 ﻿Import-Module .\PSWriteHTML.psd1 -Force
 
 New-HTML {
-
     New-HTMLSectionScrolling {
-        New-HTMLSectionScrollingItem {
-            New-HTMLPanel {
+        New-HTMLSectionScrollingItem -SectionTitle 'Test 1' {
+            "<p>Test 1 Text</p>"
+        }
+        New-HTMLSectionScrollingItem -SectionTitle 'Test 2' {
 
-            }
-        } -SectionTitle 'Test 1'
-        New-HTMLSectionScrollingItem {
-
-        } -SectionTitle 'Test 2'
-        New-HTMLSectionScrollingItem {
+        }
+        New-HTMLSectionScrollingItem -SectionTitle 'Test 3' {
             New-HTMLTable -DataTable 'Test'
-        } -SectionTitle 'Test 3'
-        New-HTMLSectionScrollingItem {
+        }
+        New-HTMLSectionScrollingItem -SectionTitle 'Test 4' {
 
-        } -SectionTitle 'Test 4'
-        New-HTMLSectionScrollingItem {
+        }
+        New-HTMLSectionScrollingItem -SectionTitle 'Test 5' {
+            "<p>Test 5 Text</p>"
 
-        } -SectionTitle 'Test 5'
-        New-HTMLSectionScrollingItem {
-
-        } -SectionTitle 'Test 6'
-        New-HTMLSectionScrollingItem {
-            New-HTMLSection {
-
+            New-HTMLSectionScrollingItem -SectionTitle 'Test 5.1' {
+                "<p>Test 5.1 Text</p>"
             }
 
-        } -SectionTitle 'Test 7'
+            New-HTMLSectionScrollingItem -SectionTitle 'Test 5.2' {
+                "<p>Test 5.2 Text</p>"
+            }
+        }
+        New-HTMLSectionScrollingItem -SectionTitle 'Test 6' {
+
+        }
+        New-HTMLSectionScrollingItem -SectionTitle 'Test 7' {
+            New-HTMLSectionScrollingItem -SectionTitle 'Test 7.1' {
+                "<p>Test 7.1 Text</p>"
+            }
+            New-HTMLSectionScrollingItem -SectionTitle 'Test 7.2' {
+                "<p>Test 7.2 Text</p>"
+            }
+            New-HTMLSectionScrollingItem -SectionTitle 'Test 7.3' {
+                "<p>Test 7.3 Text</p>"
+            }
+        }
     }
 
-} -ShowHTML -FilePath $PSScriptRoot\Example45-ScrolingText.html -Online #-Format
+} -FilePath $PSScriptRoot\Example45-ScrolingText.html -Online -Format # -ShowHTML
