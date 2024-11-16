0c188f44db146f8b8a2fc95043f8cd2ce95eb212
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-16 09:03:48 +0100

    Add example script for generating HTML tables from CSV data

---
+++ Examples/Example-Table/Example-CSV.ps1
@@ -0,0 +1,12 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$data = Get-Process | Select-Object Name, WorkingSet, Id, VirtualMemorySize, CPU -First 10
+New-HTML -HtmlData {
+    New-HTMLTable -DataTable $data -HideFooter -PagingLength 7 -PagingOptions 5, 7, 10, 15, 30, 50, 100 -HTML {
+        #New-TableButtonCopy
+        #New-TableButtonColumnVisibility
+        New-TableButtonCSV
+        New-TableCondition -Name Name -ComparisonType string -Operator eq -Value 1password -Color BlueViolet -BackgroundColor MoonYellow -Row
+        New-TableCondition -Name Name -ComparisonType string -Operator like -Value svc -Color White -BackgroundColor Green
+    } -DataStore JavaScript
+} -ShowHTML -FilePath $PSScriptRoot\Example-CVS.html -Online
