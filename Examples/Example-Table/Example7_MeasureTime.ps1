71a3df980d7a19e8005b0dc54b2b5ec8587c17b8
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-16 09:16:56 +0100

    Add example script for measuring execution time of HTML table generation

---
+++ Examples/Example-Table/Example7_MeasureTime.ps1
@@ -0,0 +1,30 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Processes = Get-Process | Select-Object -First 50
+$Title = 'My title'
+
+Measure-Command {
+    New-HTML -TitleText $Title -Online -FilePath $PSScriptRoot\Example7_MeasureTime01.html {
+        New-HTMLSection -HeaderText '1 section' {
+            New-HTMLTable -DataTable $Processes -HideFooter {
+                New-TableRowGrouping -Name 'PriorityClass' -Color Red -BackgroundColor GreenYellow
+            } -DisableSearch
+        }
+        New-HTMLSection -HeaderText '2 section' {
+            New-HTMLTable -DataTable $Processes -HideFooter -ImmediatelyShowHiddenDetails -HideShowButton
+        }
+    } -ShowHTML
+}
+
+Measure-Command {
+    New-HTML -TitleText $Title -Online -FilePath $PSScriptRoot\Example7_MeasureTime02.html {
+        New-HTMLSection -HeaderText '1 section' {
+            New-HTMLTable -DataTable $Processes -HideFooter {
+                New-TableRowGrouping -Name 'PriorityClass' -Color Red -BackgroundColor GreenYellow
+            } -DisableSearch -DataStore JavaScript
+        }
+        New-HTMLSection -HeaderText '2 section' {
+            New-HTMLTable -DataTable $Processes -HideFooter -ImmediatelyShowHiddenDetails -HideShowButton -DataStore JavaScript
+        }
+    } -ShowHTML
+}
