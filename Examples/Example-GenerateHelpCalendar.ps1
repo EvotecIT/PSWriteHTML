9c6bee82805d8a4af901e96c9d5ecc2a1b309841
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-16 09:19:27 +0100

    Add example scripts for generating HTML tree views and calendars

---
+++ Examples/Example-GenerateHelpCalendar.ps1
@@ -0,0 +1,32 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Files = Get-ChildItem -Path 'C:\Support\GitHub\PSWriteHTML\Examples' -Filter *.html -Recurse
+
+New-HTML -TitleText 'This is a test' -FilePath "$PSScriptRoot\ShowMeCalendar.html" {
+    New-HTMLSection -Invisible {
+        New-HTMLSection -HeaderText 'Introduction' {
+
+        }
+        New-HTMLSection -HeaderText 'Calendar' {
+            New-HTMLCalendar {
+                foreach ($File in $Files) {
+                    $CalendarEntry = @{
+                        Date     = $File.CreationTime
+                        FileName = $File.FullName.Replace("$PSScriptRoot\", '')
+                        Name     = $File.BaseName
+                    }
+                    if ($($CalendarEntry.Date).Day -eq $($($CalendarEntry.Date).AddMinutes(30)).Day) {
+                        New-CalendarEvent -Title $CalendarEntry.Name -StartDate $CalendarEntry.Date -EndDate $($CalendarEntry.Date).AddMinutes(30) -Url $CalendarEntry.FileName #-TargetName '_blank'
+                    } else {
+                        New-CalendarEvent -Title $CalendarEntry.Name -StartDate $CalendarEntry.Date.AddMinutes(-30) -EndDate $($CalendarEntry.Date) -Url $CalendarEntry.FileName #-TargetName '_blank'
+                    }
+                }
+            } -HeaderRight @('dayGridMonth', 'timeGridWeek', 'timeGridDay', 'listMonth', 'listYear') #-UrlTarget 'contentFrame'
+
+        }
+
+    }
+    New-HTMLSection -Invisible {
+        New-HTMLFrame -Name 'contentFrame' -Scrolling Auto -Height 1500px
+    }
+} -Online -ShowHTML
