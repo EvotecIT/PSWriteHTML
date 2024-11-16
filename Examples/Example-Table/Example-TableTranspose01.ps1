0b6cb9eb6c93c4c658ab361ffc124c5a459ffe85
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-16 09:01:58 +0100

    Add example script for transposing HTML tables

---
+++ Examples/Example-Table/Example-TableTranspose01.ps1
@@ -0,0 +1,43 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Title = 'My title'
+
+$Object = @(
+    [PSCustomObject] @{
+        Test  = 5
+        Ello  = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test  = 5
+        Ello  = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test  = 5
+        Ello  = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test  = 5
+        Ello  = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test  = 5
+        Ello  = 'Motto'
+        Hello = 'Totto'
+    }
+    [PSCustomObject] @{
+        Test  = 3
+        Ello  = 'Lotto'
+        Hello = 'Totto'
+    }
+)
+
+
+$Object | Out-HtmlView -Transpose -TransposeName 'Test' -Filtering
+
+New-HTML -TitleText $Title -Online -FilePath $PSScriptRoot\Example-TableTranspose01.html {
+    New-HTMLTable -DataTable $Object -Filtering -Transpose -TransposeName 'Test'
+} -ShowHTML
