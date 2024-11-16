6300d8344752b00ec7e1c2b7d75e68c61ceccb17
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-11-16 09:21:44 +0100

    Refactor example script for generating HTML table output to improve readability and update file path

--- Examples/Example-Table/Example7_02_DataStoreJava.ps1
+++ Examples/Example-Table/Example7_02_DataStoreJava.ps1
@@ -2,23 +2,23 @@
 
 $DataTable3 = @(
     [PSCustomObject] @{
-        'Test1'        = 'Test' + [System.Environment]::NewLine + 'test3';
-        'Test2'        = 'Test' + [System.Environment]::NewLine + 'test3' + "`n test"
-        'Test3'        = 'Test' + [System.Environment]::NewLine + 'test3' + "`r`n test"
-        'Test4'        = 'Test' + [System.Environment]::NewLine + 'test3' + "`r test"
-        'Test5'        = 'Test' + "`r`n" + 'test3' + "test"
-        'Test6'        = @"
+        'Test1'                                     = 'Test' + [System.Environment]::NewLine + 'test3'
+        'Test2'                                     = 'Test' + [System.Environment]::NewLine + 'test3' + "`n test"
+        'Test3'                                     = 'Test' + [System.Environment]::NewLine + 'test3' + "`r`n test"
+        'Test4'                                     = 'Test' + [System.Environment]::NewLine + 'test3' + "`r test"
+        'Test5'                                     = 'Test' + "`r`n" + 'test3' + "test"
+        "Test6 $([System.Environment]::NewLine) ok" = @"
         Test1
         Test2
         Test3
 
         Test4
 "@
-        'Test7'        = 'Test' + "`n`n" + "Oops"
-        'Test9'        = 'Test' + "<Br>" + "Oops"
-        'Test8"Oopps"' = 'MyTest "Ofcourse"'
-        "Test9'Ooops'" = "MyTest 'Ofcourse'"
+        'Test7'                                     = 'Test' + "`n`n" + "Oops"
+        'Test9'                                     = 'Test' + "<Br>" + "Oops"
+        'Test8"Oopps"'                              = 'MyTest "Ofcourse"'
+        "Test9'Ooops'"                              = "MyTest 'Ofcourse'"
 
     }
 )
-$DataTable3 | Out-HtmlView -DataStore JavaScript -FilePath $PSScriptRoot\Example7_02_DataStoreJava.html -Online
+$DataTable3 | Out-HtmlView -FilePath C:\Support\GitHub\PSWriteHTML\Examples\Example-Table\Example7_02_DataStoreJava.html -Online -DataStore JavaScript
