0c6313b5c1426cf36c1d7988e1e6fd9886b213ff
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-17 20:33:48 +0100

    Add HTML table for international users and devices with export options

--- Examples/Example-Table/Example7_01.ps1
+++ Examples/Example-Table/Example7_01.ps1
@@ -22,7 +22,53 @@ foreach ($Key in $RootDSE.PSObject.Properties.Name) {
     $TranslatedRootDSE["$Key"] = $RootDSE.$Key -join ', '
 }
 
+
+New-HTML -Online -TitleText "Users With Devices" {
+    Add-HTMLStyle -Placement Header -Css @{
+        ".dt-button" = @{
+            'background-color' = '#f5f5f5;'
+            'font-size'        = '30px !important;'
+        }
+    }
+    New-HTMLHeader {
+        New-HTMLSpanStyle -Alignment center {
+            New-HTMLHeading -Heading h1 -HeadingText "International Users and Devices"
+        }
+    }
+    # New-HTMLTableOption -DataStore JavaScript -DateTimeFormat 'MMM-dd-yyyy'
+    $newHTMLTableSplat = @{
+        DataTable               = $DataTable1
+        Title                   = "Int Users And Devices"
+        PagingLength            = 50
+        WordBreak               = 'break-word'
+        AutoSize                = $true
+        Style                   = 'display'
+        Buttons                 = 'copyHtml5', 'excelHtml5', 'csvHtml5', 'pdfHtml5', 'columnVisibility', 'searchBuilder'
+        EnableColumnReorder     = $true
+        SearchRegularExpression = $true
+        PreContent              = {
+            New-HTMLSpanStyle -Alignment center {
+                New-HTMLHeading -Heading h2 -HeadingText "Graph Data Export"
+            }
+        }
+        Filtering               = $true
+        HTML                    = {
+            New-HTMLTableStyle -FontSize 14
+            New-HTMLTableButtonExcel
+            New-HTMLTableColumnOption -ColumnIndex 2 -Hidden $true
+            New-HTMLTableColumnOption -AllColumns -Width 100
+        }
+    }
+
+    New-HTMLTable @newHTMLTableSplat
+} -FilePath $PSScriptRoot\IntAzureReportHTML.html -ShowHTML
+
+
+
+return
 New-HTML {
+
+
     New-HTMLSection -Invisible {
         New-HTMLSection {
             New-HTMLTable -DataTable $DataTable1 -Title 'Test1' -PagingLength 2 # Title will be used for filename when using export
