ae856005ac709e9940bcc4cc502be43092dddcea
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2024-12-17 20:34:02 +0100

    Add example script for generating HTML table of Active Directory users

---
+++ Examples/Example-TableShareData/Example-TableShareData.ps1
@@ -0,0 +1,13 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+$Users = Get-ADUser -Filter * -Properties LastLogonDate, PasswordLastSet | Select-Object -First 3
+New-HTML {
+    New-HTMLTableOption -DataStore JavaScript
+    New-HTMLTable -DataTable $Users -Title 'Table with Users' -HideFooter -PagingLength 10 {
+
+    }
+
+    New-HTMLTable -DataTable @() -Title 'Table with Users' -HideFooter -PagingLength 10 {
+
+    }
+} -ShowHTML -FilePath "$PSScriptRoot\Example-TableShareData.html" -Online
