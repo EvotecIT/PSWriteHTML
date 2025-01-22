8e0a2ee5f7d23aec60b9c5bcfb42933b703f21cb
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-22 18:41:12 +0100

    Add Example-Texts02.ps1 for HTML report generation
    - Import PSWriteHTML module
    - Create HTML structure with header, footer, and images
    - Include dynamic date generation in footer

---
+++ Examples/Example-Texts/Example-Texts02.ps1
@@ -0,0 +1,19 @@
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
+New-HTML -TitleText 'This is a test' -FilePath "$PSScriptRoot\Example-Texts02.html" {
+    New-HTMLHeader {
+        New-HTMLSection -Invisible {
+            New-HTMLPanel -Invisible {
+                New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Class 'otehr' -Width '50%'
+            }
+            New-HTMLPanel -Invisible {
+                New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Width '20%'
+            } -AlignContentText right
+        }
+    }
+    New-HTMLFooter {
+        New-HTMLPanel {
+            New-HTMLText -Text "Report generated on ", (New-HTMLDate -InputDate (Get-Date) -DisplayFormat "dd MMM yyyy" -IncludeFromNow) -Color None, Blue -FontSize 10, 10
+        } -Invisible -AlignContentText right
+    }
+} -ShowHTML -Online
