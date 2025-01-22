8a349f264226a1d005b1079dd462b8a4c2863110
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-22 18:21:09 +0100

    Add momentDateText.js for dynamic report date display
    - Introduced a new JavaScript file to append relative date information to report dates.
    - Utilizes moment.js to format and display dates in a user-friendly manner.

---
+++ Resources/JS/momentDateText.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+    $('.reportDate').each(function () {
+        var dateValue = $(this).data('reportdate');
+        if (dateValue) {
+            var fromNow = moment(dateValue, 'YYYY-MM-DD HH:mm:ss').fromNow();
+            $(this).append(' (' + fromNow + ')');
+        }
+    });
+});
