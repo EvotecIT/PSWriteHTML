0a9834cbf225755fbce7e246289a26c58e7daf29
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-01-22 18:31:00 +0100

    Add New-HTMLDate function for date output with live "time ago" calculation
    - Introduced New-HTMLDate function to generate a span element with date and data attributes.
    - Supports customizable display format and optional relative time display.

---
+++ Public/New-HTMLDate.ps1
@@ -0,0 +1,24 @@
+﻿function New-HTMLDate {
+    <#
+    .SYNOPSIS
+    Outputs a date with a data attribute for live "time ago" calculation.
+
+    .DESCRIPTION
+    Generates a span element containing the provided date and data attributes
+    defining formatting and whether to display relative time.
+    #>
+    [CmdletBinding()]
+    param(
+        [Parameter(Mandatory)][DateTime]$InputDate,
+        [string]$DisplayFormat = 'yyyy-MM-dd HH:mm:ss',
+        [switch]$IncludeFromNow
+    )
+
+    $Script:HTMLSchema.Features.Moment = $true
+    $Script:HTMLSchema.Features.MomentDateText = $true
+    $Script:HTMLSchema.Features.Jquery = $true
+
+    $dateString = $InputDate.ToString($DisplayFormat)
+
+    "<span class='reportDate' data-reportdate='$dateString' data-format='$DisplayFormat' data-fromnow=$($IncludeFromNow.IsPresent)>$($InputDate.ToString())</span>"
+}
