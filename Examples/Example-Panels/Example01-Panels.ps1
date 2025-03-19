aec22c54f055a0f2c497faf63f6b42c700e372ce
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-03-19 17:54:49 +0100

    - Adjust alignment of HTML panels to center for better presentation - Ensure height is set for all HTML panels for consistent layout - Remove unnecessary commented-out code for clarity

--- Examples/Example-Panels/Example01-Panels.ps1
+++ Examples/Example-Panels/Example01-Panels.ps1
@@ -1,9 +1,10 @@
-﻿
+﻿Import-Module .\PSWriteHTML.psd1 -Force
+
 New-HTML {
 
     New-HTMLSection {
         New-HTMLSectionStyle -BorderRadius 0px -HeaderBackGroundColor Grey -RemoveShadow
-        New-HTMLPanel -BackgroundColor lightpink -AlignContentText right -BorderRadius 10px {
+        New-HTMLPanel -BackgroundColor lightpink -AlignContentText center -BorderRadius 10px {
             New-HTMLText -TextBlock {
                 New-HTMLText -Text "Test" -Alignment left -FontSize 25 -FontWeight bold
                 New-HTMLText -Text 'Users Never Loged' -Alignment left -FontSize 15
@@ -13,13 +14,13 @@ New-HTML {
 
         New-HTMLPanel -Margin 10 {
 
-            New-HTMLPanel -BackgroundColor lightpink -AlignContentText right -BorderRadius 0px {
+            New-HTMLPanel -BackgroundColor lightpink -AlignContentText center -BorderRadius 0px {
                 New-HTMLText -TextBlock {
                     New-HTMLText -Text $neverlogedenabled -Alignment left -FontSize 25 -FontWeight bold
                     New-HTMLText -Text 'Users Never Loged' -Alignment left -FontSize 15
                     New-HTMLFontIcon -IconRegular address-book -IconSize 20
                 }
-            }
+            } -Height 150px
         }
         New-HTMLPanel -Margin 10 {
 
@@ -27,17 +28,18 @@ New-HTML {
                 New-HTMLText -Text 30 -Alignment left -FontSize 40 -FontWeight bold
                 New-HTMLText -Text "Computers deleted" -Alignment left -FontSize 20
                 New-HTMLFontIcon -IconSolid desktop -IconSize 20
-            }
+            } -Height 250px
         }
         New-HTMLPanel -Margin 10 {
 
-            New-HTMLPanel -BackgroundColor lightgreen -AlignContentText right -BorderRadius 10px {
-                New-HTMLText -TextBlock {
-                    New-HTMLText -Text 15 -Alignment justify -FontSize 25 -FontWeight bold
-                    New-HTMLText -Text 'Disabled Users' -Alignment justify -FontSize 15
-                    New-HTMLFontIcon -IconRegular address-book -IconSize 20
-                }
-            }
+            New-HTMLPanel -BackgroundColor lightgreen -AlignContentText center -BorderRadius 10px {
+                # New-HTMLText -TextBlock {
+                #     New-HTMLText -Text 15 -Alignment justify -FontSize 25 -FontWeight bold
+                #     New-HTMLText -Text 'Disabled Users' -Alignment justify -FontSize 15
+                #     New-HTMLFontIcon -IconRegular address-book -IconSize 20
+                # }
+                New-HTMLText -Text "15" -FontSize 20px -FontWeight bold
+            } -Height 250px
         }
     }
 
