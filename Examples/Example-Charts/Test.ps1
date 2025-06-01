be643e9599a2d0943170f2b01e2104dee72c4151
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-01 16:39:44 +0200

    Add Test.ps1 for dashboard chart examples
    - Created a new PowerShell script to demonstrate various chart configurations for test reports.
    - Included multiple sections with different chart types and options for visualizing test results.
    - Enhanced the dashboard's capability to display passed, failed, and awaiting test statuses with color coding.

---
+++ Examples/Example-Charts/Test.ps1
@@ -0,0 +1,36 @@
+﻿
+
+Dashboard -Name "Test report" -FilePath ".\testreport.html" {
+    Section -Name "Tests" {
+        Chart {
+            ChartToolbar
+            ChartBarOptions -Vertical
+            ChartBar -Name "Passed" -Value 1 -Color Green
+            ChartBar -Name "Failed" -Value 5 -Color Red
+            ChartBar -Name "Awaiting" -Value 2 -Color Blue
+        }
+    }
+    Section -Name "Tests2" {
+        Chart {
+            ChartBar -Name "Status" -Value 1, 5, 2
+            ChartLegend -Name 'Passed', 'Failing', 'Awaiting' -Color Green, Red, Blue
+        }
+    }
+    Section -Name "Tests2" {
+        Chart {
+            ChartBar -Name "C# Tests" -Value 1, 5, 2
+            ChartBar -Name "PowerShell Tests" -Value 1, 5, 2
+            ChartBar -Name "F# Tests" -Value 1, 15, 2
+            ChartLegend -Name 'Passed', 'Failing', 'Awaiting' -Color Green, Red, Blue
+        }
+    }
+    Section -Name "Tests2" {
+        Chart {
+            ChartBarOptions -Vertical
+            ChartBar -Name "C# Tests" -Value 1, 5, 2
+            ChartBar -Name "PowerShell Tests" -Value 1, 5, 2
+            ChartBar -Name "F# Tests" -Value 1, 15, 2
+            ChartLegend -Name 'Passed', 'Failing', 'Awaiting' -Color Green, Red, Blue
+        }
+    }
+} #-ShowHTML
