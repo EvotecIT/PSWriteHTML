d948bdcef45f2df99795312505a9290c5ed63d7f
Przemyslaw Klys <przemyslaw.klys@evotec.pl>
2025-06-02 19:07:20 +0200

    Enhance Example-MicrosoftEntra.ps1 with additional HTML elements for improved report presentation
    - Added new HTML header and sections to include logos and report generation dates.
    - Implemented invisible panels for better layout control and visual consistency.
    - Enhanced the overall structure to improve the readability and presentation of the report.

--- Examples/Example-InfoCard/Example-MicrosoftEntra.ps1
+++ Examples/Example-InfoCard/Example-MicrosoftEntra.ps1
@@ -1,6 +1,22 @@
 Import-Module "$PSScriptRoot\..\..\PSWriteHTML.psd1" -Force
 
 New-HTML {
+    New-HTMLHeader {
+        New-HTMLSection -Invisible {
+            New-HTMLPanel -Invisible {
+                New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Class 'otehr' -Width '50%'
+            }
+            New-HTMLPanel -Invisible {
+                New-HTMLImage -Source 'https://evotec.pl/wp-content/uploads/2015/05/Logo-evotec-012.png' -UrlLink 'https://evotec.pl/' -AlternativeText 'My other text' -Width '20%'
+            } -AlignContentText right
+        }
+        New-HTMLPanel {
+            New-HTMLText -Text "Report generated on ", (New-HTMLDate -InputDate (Get-Date)) -Color None, Blue -FontSize 10, 10
+            New-HTMLText -Text "Report generated on ", (New-HTMLDate -InputDate (Get-Date -Year 2022)) -Color None, Blue -FontSize 10, 10
+            New-HTMLText -Text "Report generated on ", (New-HTMLDate -InputDate (Get-Date -Year 2022) -DoNotIncludeFromNow) -Color None, Blue -FontSize 10, 10
+            New-HTMLText -Text "Report generated on ", (New-HTMLDate -InputDate (Get-Date -Year 2024 -Month 11)) -Color None, Blue -FontSize 10, 10
+        } -Invisible -AlignContentText right
+    }
     New-HTMLSectionStyle -BorderRadius 0px -HeaderBackGroundColor '#0078d4'
 
     # Feature highlights section - now with ResponsiveWrap
